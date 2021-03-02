abf7f7d32c1a7e780d49799ef392b72e127b76f1
Mateus Martins - Turma 10 - Tribo A <mateusmartinsmck@gmail.com>
2021-03-02 17:16:20 -0300

    Requisito 5 concluído

--- src/challenges.js
+++ src/challenges.js
@@ -25,9 +25,10 @@ function concatName(array) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return (wins*3)+(ties);
 }
+console.log(footballPoints(0,0));
 
 // Desafio 6
 function highestCount() {
