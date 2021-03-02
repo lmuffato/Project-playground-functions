865b012c249b69b77a630c69700a2d07c8f0bdbe
Eduardo Seije <eduardoseije@gmail.com>
2021-03-02 19:06:27 -0300

    Desafio 08 - function fizzBuzz

--- src/challenges.js
+++ src/challenges.js
@@ -63,9 +63,25 @@ function catAndMouse(mouse, cat1, cat2) {
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(array) {
+  let arrayResult = [];
+  for (let index = 0; index <= array.length; index += 1) {
+    if (array[index] % 3 === 0 && array[index] % 5 === 0) {
+      arrayResult.push('fizzbuzz');
+    }
+      else if (array[index] % 3 === 0) {
+        arrayResult.push('fizz');    
+      }
+      else if (array[index] % 5 === 0) {
+        arrayResult.push('buzz');
+      }
+      else {
+        arrayResult.push('bug');
+      }
+  }
+  return arrayResult;
 }
+console.log(fizzBuzz([2, 15, 7, 9, 45]))
 
 // Desafio 9
 function encode() {
