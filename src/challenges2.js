d0c260bf1438d05ec0b167f7cf5d0f127020ae52
Guilherme Dugaich de Oliveira <guilherme.dugaich@gmail.com>
2021-03-02 17:19:51 -0300

    Exercicio 10 - tech list

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,12 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(techArray, name) {
+  techArray = techArray.sort();
+  let objectArray = [];
+  for (let index = 0; index < techArray.length; index += 1) {
+    let object = { tech: techArray[index], name: name }
+    objectArray.push(object);
+  }
+  return objectArray;
 }
 
 // Desafio 11
