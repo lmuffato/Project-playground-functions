929bba03e58d054be670fbce4060ea199901ee36
Diogo Augusto <diogotrescastro@gmail.com>
2021-03-02 17:12:49 -0300

    Resoluçao do requisito 5:Crie uma função que calcule a quantidade de pontos no futebol

--- src/challenges.js
+++ src/challenges.js
@@ -2,16 +2,14 @@
 function compareTrue(value1, value2) {
   if (value1 && value2 === true) {
     return true;
-  } else {
-    return false;
   }
+  return false;
 }
 
 // Desafio 2
 function calcArea(base, height) {
   return ((base * height) / 2);
 }
-
 
 // Desafio 3
 // Para o exercicio foi utilizada a documentação da função SPLIT disponível em: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/split
@@ -28,14 +26,17 @@ function concatName(phraseArray) {
   let firstItem = phraseArray[0];
   let lastItem = phraseArray[phraseArray.length - 1];
   let phraseString = lastItem + ', ' + firstItem;
-  return phraseString;
+  return (phraseString);
 }
 
 console.log(concatName(['foguete', 'não', 'tem', 'ré']));
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+
+function footballPoints(wins, ties) {
+  let sumWins = wins * 3;
+  let resultPoints = sumWins + ties;
+  return resultPoints;
 }
 
 // Desafio 6
