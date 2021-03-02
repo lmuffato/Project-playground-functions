ea4a217725f80292450ecd82f173707714117267
Gabriel Ferreira <glucas709@gmail.com>
2021-03-02 15:04:30 -0300

    Requisito 01 - compareTrue()

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,10 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(valueOne, valueTwo) {
+  if (valueOne === true && valueTwo === true) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Desafio 2
