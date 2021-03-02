7c12e0e65d79967f4d0c38c5c1f85ab10a30be83
AndreBarroso <andre_barroso17@hotmail.com>
2021-03-02 20:07:30 -0300

    Requisito 9

--- src/challenges.js
+++ src/challenges.js
@@ -86,16 +86,124 @@ function catAndMouse(mouse, cat1, cat2) {
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(array) {
+  let retorno = [];
+  for(let index=0; index<array.length; index+=1){
+    if(array[index]%3===0&&array[index]%5!==0){
+      retorno[index] = 'fizz';
+    }
+    else if(array[index]%5===0&&array[index]%3!==0){
+      retorno[index] = 'buzz';
+    }
+    else if(array[index]%5===0&&array[index]%3===0){
+      retorno[index] = 'fizzBuzz';
+    }
+    else{
+      retorno[index]='bug!';
+    }
+  }
+  return retorno;
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
-}
-function decode() {
-  // seu código aqui
+function encode(string) {
+  let codigo;
+  for(let index=0; index<string.length; index+=1){
+    if(codigo===undefined){
+        switch(string[index]){
+          case 'a':
+           codigo = '1';
+          break;
+          case 'e':
+            codigo = '2';
+          break;
+          case 'i':
+            codigo = '3';
+          break;
+          case 'o':
+            codigo = '4';
+          break;
+          case 'u':
+            codigo = '5';
+          break;
+          default:
+            codigo = string[index];
+        } //fecha swich if
+
+    } //fecha if
+    else {
+        switch(string[index]){
+          case 'a':
+           codigo += '1';
+          break;
+          case 'e':
+            codigo += '2';
+          break;
+          case 'i':
+            codigo += '3';
+          break;
+          case 'o':
+            codigo += '4';
+          break;
+          case 'u':
+            codigo += '5';
+          break;
+          default:
+            codigo += string[index];
+        }//fecha swich els
+   } //fecha else
+  } //fecha for
+  return codigo;
+} //fecha funcao
+
+function decode(string) {
+  let codigo;
+  for(let index=0; index<string.length; index+=1){
+    if(codigo===undefined){
+        switch(string[index]){
+          case 'a':
+           codigo = '1';
+          break;
+          case 'e':
+            codigo = '2';
+          break;
+          case 'i':
+            codigo = '3';
+          break;
+          case 'o':
+            codigo = '4';
+          break;
+          case 'u':
+            codigo = '5';
+          break;
+          default:
+            codigo = string[index];
+        } //fecha swich if
+
+    } //fecha if
+    else {
+        switch(string[index]){
+          case '1':
+           codigo += 'a';
+          break;
+          case '2':
+            codigo += 'e';
+          break;
+          case '3':
+            codigo += 'i';
+          break;
+          case '4':
+            codigo += 'o';
+          break;
+          case '5':
+            codigo += 'u';
+          break;
+          default:
+            codigo += string[index];
+        }//fecha swich els
+   } //fecha else
+  } //fecha for
+  return codigo;
 }
 
 module.exports = {
@@ -115,8 +223,10 @@ module.exports = {
 //console.log(concatName(['captain', 'my', 'captain']));
 //console.log(footballPoints(0,0));
 //console.log(highestCount([0,0,0]));
-console.log(catAndMouse(1,3,2));
-
+//console.log(catAndMouse(1,3,2));
+//console.log(fizzBuzz([9, 25]));
+console.log(encode('hi there!'));
+console.log(decode('h3 th2r2!'));
 
 
 
