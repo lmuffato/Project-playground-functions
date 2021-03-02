cdef25e2d7bb7b4bfd8e801408efd5d1cc2d0453
guilhermemd <guilherme.md@gmail.com>
2021-03-02 15:02:28 -0300

    corrige - desafio 1 - true or false

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,6 @@
 // Desafio 1
 function compareTrue(value1, value2) {
-  if(value1 === true && value2 === true){
+  if (value1 === true && value2 === true) {
     return true;
   } else {
     return false;
