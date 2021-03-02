e81cb173ad4802c5b7e0df88de76d13d96c16a6d
Carlos Campo <carlosgabrielcampo@gmail.com>
2021-03-02 20:45:42 -0300

    desafio 10 teste

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,26 @@
+
+
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(tech, name) {
+
+  let result = [];
+
+if (tech.length === 5){
+  tech = tech.sort()
+} else if (tech.lenght > 0){
+  tech 
+} else {
+  return 'Vazio!'
+} 
+
+for (let indexTech = 0; indexTech < tech.length; indexTech += 1){
+    result.push("tech: " + tech[indexTech] + " name: " + name)
+  }
+  return result
 }
+
+console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"],'carlos'))
+
 
 // Desafio 11
 function generatePhoneNumber() {
