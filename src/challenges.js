4f488a1acbf6163ddc58b9d36e0817e6fa1151b3
Lucas Felipe Ribeiro <luc.felipe42@gmail.com>
2021-03-02 16:07:29 -0300

    Requisito 3

--- src/challenges.js
+++ src/challenges.js
@@ -15,22 +15,28 @@ console.log(compareTrue(false, false));
 // Desafio 2
 function calcArea(base, height) {
   // seu código aqui
-  result = (base* height)/ 2;
-  return result
+  let result = (base * height) / 2;
+  return result;
 }
 console.log(calcArea(10, 50));
 console.log(calcArea(5, 2));
 console.log(calcArea(51, 1));
 
-
 // Desafio 3
-function splitSentence() {
+function splitSentence(separatee, limiter) {
   // seu código aqui
+  var result = separatee.split(limiter);
+  return result;
 }
+
+console.log(splitSentence("go Trybe", " "));
+console.log(splitSentence("vamo que vamo", " "));
+console.log(splitSentence("foguete", " "));
 
 // Desafio 4
 function concatName() {
   // seu código aqui
+
 }
 
 // Desafio 5
