fbd4585359ff28e6eda83422264988318fc3ec80
Mateus Alencar <mtauws@gmail.com>
2021-03-02 19:23:10 -0300

    lint: exercise 8

--- src/challenges.js
+++ src/challenges.js
@@ -39,15 +39,26 @@ function footballPoints(wins, ties) {
 }
 
 // Desafio 6
-function highestCount(array) {
+/**
+ * Com base no código de outros colegas vi que o Patrick Moroes criou outra função para deixar
+ * o código mais organizado, achei interessante fazer mesma coisa, ou seja, delegar uma função
+ * para achar o maior numero e outra função para contar quantas vezes esse numero se repete,
+ * link do github: https://github.com/tryber/sd-010-a-project-playground-functions/blob/patrickmoraisn-project-playground-functions/src/challenges.js
+ */
+function highestNumber(array) {
   let highestValue = 0;
-  let highestValueCount = 0;
 
   for (let index = 0; index < array.length; index += 1) {
     if (highestValue < array[index]) {
       highestValue = array[index];
     }
   }
+  return highestValue;
+}
+
+function highestCount(array) {
+  let highestValueCount = 0;
+  let highestValue = highestNumber(array);
 
   for (let index = 0; index < array.length; index += 1) {
     if (highestValue === array[index]) {
@@ -101,31 +112,28 @@ function encode(string) {
    * Usei como base o código do Voicu, segue abaixo o link do StackOveFlow:
    * https://stackoverflow.com/questions/16576983/replace-multiple-characters-in-one-replace-call
    */
-  let encode = {
+  let encodeString = {
     a: 1,
     e: 2,
     i: 3,
     o: 4,
-    u: 5
+    u: 5,
   };
-  string.toLowerCase();
-  string = string.replace(/[aeiou]/g, m => encode[m]);
 
+  string = string.replace(/[aeiou]/g, ((m) => encodeString[m]));
   return string;
-
 }
 
 function decode(string) {
-  let decode = {
-    '1': 'a',
-    '2': 'e',
-    '3': 'i',
-    '4': 'o',
-    '5': 'u'
+  let decodeString = {
+    1: 'a',
+    2: 'e',
+    3: 'i',
+    4: 'o',
+    5: 'u',
   };
 
-  string = string.replace(/[12345]/g, m => decode[m]);
-
+  string = string.replace(/[12345]/g, ((m) => decodeString[m]));
   return string;
 }
 
