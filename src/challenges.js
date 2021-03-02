b371ef6875dbba36744e797fcfe4e1eca24e152c
Max Rudim <max.hermsdorff@gmail.com>
2021-03-02 18:07:55 -0300

    nova correção desafio 04

--- src/challenges.js
+++ src/challenges.js
@@ -18,7 +18,7 @@ function splitSentence(string) {
 
 // Desafio 4
 function concatName(array) {
-  let newArray = array[array.length - 1] + ',' + array[0];
+  let newArray = array[array.length - 1] + ', ' + array[0];
   return newArray;
 }
 
