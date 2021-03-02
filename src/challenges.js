6ae3ab8704221b562c69185bfefdd1cfc7789ee3
Alan <alan.alb.flopes@gmail.com>
2021-03-02 14:05:26 -0400

    primeiro requisito completo.

--- src/challenges.js
+++ src/challenges.js
@@ -1,8 +1,12 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(x,y) {
+  if(x==true&&y==true){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
-
 // Desafio 2
 function calcArea() {
   // seu código aqui
