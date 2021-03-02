462b1b830fa720c4acd958b4016fd28c885defc4
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-03-02 18:57:49 -0300

    correções console.log

--- src/challenges.js
+++ src/challenges.js
@@ -7,24 +7,24 @@ function compareTrue(valor1, valor2) {
   }
 }
 
-console.log(compareTrue(true, false));
+//console.log(compareTrue(true, false));
 
 // Desafio 2
 function calcArea(base, height) {
   return (base * height) / 2;
 }
 
-console.log(calcArea(10,50));
-console.log(calcArea(5,2));
-console.log(calcArea(51,1));
+//console.log(calcArea(10,50));
+//console.log(calcArea(5,2));
+//console.log(calcArea(51,1));
 
 // Desafio 3
 function splitSentence(string) {
   return string.split(' ');
 }
 
-let teste = "go Trybe"
-console.log(splitSentence(teste));
+//let teste = "go Trybe"
+//console.log(splitSentence(teste));
 
 
 // Desafio 4
@@ -33,7 +33,7 @@ function concatName(stringArray) {
 }
 
 let arrayTeste = ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo'];
-console.log(concatName(arrayTeste));
+//console.log(concatName(arrayTeste));
 
 // Desafio 5
 function footballPoints(wins, ties) {
@@ -41,9 +41,9 @@ function footballPoints(wins, ties) {
   return pontos;
 }
 
-console.log(footballPoints(14,8));
-console.log(footballPoints(1,2));
-console.log(footballPoints(0,0));
+//console.log(footballPoints(14,8));
+//console.log(footballPoints(1,2));
+//console.log(footballPoints(0,0));
 
 // Desafio 6
 // usei esse link para me ajudar a decobrir qual o maior valor de um array https://pt.stackoverflow.com/questions/35202/como-saber-o-maior-valor-de-um-array
@@ -73,11 +73,11 @@ function catAndMouse(mouse, cat1, cat2) {
   } else if (posição2 > posição1){
     return 'cat1';
   } else {
-    console.log('os gatos trombam e o rato foge')
+    return 'os gatos trombam e o rato foge'
   }
 }
 
-console.log(catAndMouse(1,8,5));
+//console.log(catAndMouse(1,8,5));
 
 
 // Desafio 8
@@ -98,7 +98,7 @@ function fizzBuzz(arrayNumbers) {
 }
 
 let testeArray = [2, 15, 7, 9, 45];
-console.log(fizzBuzz(testeArray));
+//console.log(fizzBuzz(testeArray));
 
 // Desafio 9
 function encode(string) {
