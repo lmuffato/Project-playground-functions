ef8d9da349aef48c4af942874800d936bef25854
Marcelo Maurício <mmauricio.design@gmail.com>
2021-03-02 17:35:08 -0300

    Requisito 10 - foi alterado para teste e funcionamento

--- src/challenges2.js
+++ src/challenges2.js
@@ -8,16 +8,20 @@ function techList(stringArray, name) {
     }
     arrayObjects[index] = objects
   }
-  arrayObjects.sort(function (a, b) {  //Entrei no MDN para entender sobre o comando sort de array, e lá tem essa explicação de como ordenar objetos.
-    if (a.tech > b.tech) {
-      return 1;
-    }
-    if (a.tech < b.tech) {
-      return -1;
-    } 
-    return 0;
-  });
-  return arrayObjects;
+  if (arrayObjects.length >= 5){
+    arrayObjects.sort(function (a, b) {  //Entrei no MDN para entender sobre o comando sort de array, e lá tem essa explicação de como ordenar objetos.
+      if (a.tech > b.tech) {
+        return 1;
+      }
+      if (a.tech < b.tech) {
+        return -1;
+      } 
+      return 0;
+      });
+    return arrayObjects;
+  } else if (arrayObjects.tech === undefined){
+    return 'Vazio!';
+  }
 }
 
 // Desafio 11
