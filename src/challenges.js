11c451628bf59825f07d36e1ee0452a111f01b41
ThalitaC <thalitaceci@gmail.com>
2021-03-02 16:13:40 -0300

    Requisito 3: Crie uma função que divida a frase

--- src/challenges.js
+++ src/challenges.js
@@ -13,8 +13,9 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+  let stringArrays = string.split(' ');
+  return stringArrays;
 }
 
 // Desafio 4
