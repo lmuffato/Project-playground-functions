fddec8fc9809ca502dda1fd90d50dc0de6a84f96
Guilherme Sucupira Oliveira <guilherme.so23@gmail.com>
2021-03-02 18:43:47 -0300

    7. Concluido funcao catAndMouse

--- src/challenges.js
+++ src/challenges.js
@@ -57,8 +57,16 @@ function highestCount(numbers) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  if (cat1 > cat2) {
+    return 'cat2';
+  }
+  if (cat1 < cat2) {
+    return 'cat1';
+  }
+  if (cat1 === cat2) {
+    return 'os gatos trombam e o rato foge';
+  }
 }
 
 // Desafio 8
