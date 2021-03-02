17c52b520944ac66afeec97d022b1c8c50908653
Gabriel Ferreira <glucas709@gmail.com>
2021-03-02 15:17:46 -0300

    Requisito 02 - calcArea()

--- src/challenges.js
+++ src/challenges.js
@@ -11,11 +11,10 @@ function compareTrue(valueOne, valueTwo) {
   return comparedValue;
 }
 
-console.log(compareTrue(true, true));
-
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let result = (base * height) / 2;
+  return result;
 }
 
 // Desafio 3
