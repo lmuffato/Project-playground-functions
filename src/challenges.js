2809f987eacc10e3399bc7c265a3959924b2c49e
Luiz Felipe Araujo <thelastnailmusic@gmail.com>
2021-03-02 15:01:27 -0300

    Desafio 1 Ok

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,11 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(val1, val2) {
+  if ((val1===true) && (val2===true)){
+    return true; 
+  } else {
+    return false;
+  }
+
 }
 
 // Desafio 2
