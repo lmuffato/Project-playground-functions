23400248d0c2b74c48a69f1a02b48b8c0baa40a8
jonathan <jonathanfernandesjf98@gmail.com>
2021-03-02 15:17:04 -0300

    Desafio 01 crie uma função usando o operador &&

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,11 @@
 // Desafio 1
-function compareTrue() {
+function compareTrue(value1, value2) {
   // seu código aqui
+  if (value1 == true && value2 == true){
+  return true;
+  }else {
+  return false;
+  }
 }
 
 // Desafio 2
