dddd55c3c127a5fa3e90d557e0eb418f1e6fec81
Sérgio Martins <sergiomos2010@gmail.com>
2021-03-02 15:04:40 -0300

    Terceiro desafio: splitSentence

--- src/challenges.js
+++ src/challenges.js
@@ -13,8 +13,8 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+  return string.split(' ');
 }
 
 // Desafio 4
