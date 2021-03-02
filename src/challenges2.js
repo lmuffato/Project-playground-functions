8cc52f1a845e6287a7f0217a10dfbd266b4576b9
Gui-lira <gui.lira.martins@gmail.com>
2021-03-02 17:47:21 -0300

    10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,12 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(array, name) {
+  // esse codigo foi baseado por essa discussão no stack overflow https://stackoverflow.com/questions/5868850/creating-list-of-objects-in-javascript
+  let list = [];
+  for (let index in array)
+  {
+    list.push({ tech: array[index], name: name })
+  }
+  return list
 }
 
 // Desafio 11
