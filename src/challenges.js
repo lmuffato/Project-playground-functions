9d1049d84a8c97273d3b5328355d9e3b030c42ab
Patrick Morais <ppternunes@gmail.com>
2021-03-02 12:04:18 -0300

    adiciona exercicio 2 e exercicio 3

--- src/challenges.js
+++ src/challenges.js
@@ -4,13 +4,13 @@ function compareTrue(param1, param2) {
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, altura) {
+  return (base * altura)/2;
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(frase) {
+  return frase.split(' ');
 }
 
 // Desafio 4
