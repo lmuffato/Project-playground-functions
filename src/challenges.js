8c30de4c66da8e8c9e6dec1226476606aa5ee977
Southier <patrick.southier@hotmail.com>
2021-03-02 16:30:47 -0300

    Adiciona a resolução dos requisitos obrigatórios 1 e 2.

--- src/challenges.js
+++ src/challenges.js
@@ -1,12 +1,20 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(value1, value2) {
+  if (value1 === true && value2 === true) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
+console.log(compareTrue(true, false));
+
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return (base * height) / 2;
 }
+
+console.log(calcArea(51, 1));
 
 // Desafio 3
 function splitSentence() {
