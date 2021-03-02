a54790e61b8e1dfa8dd65a806f75d91701b46b44
Tiago Santos <tiago881@gmail.com>
2021-03-02 18:38:19 -0300

    requisito 03- dividir uma string com separador

--- src/challenges.js
+++ src/challenges.js
@@ -14,8 +14,10 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(phrase, separator) {
+  let message = phrase.split(separator);
+
+  return message;
 }
 
 // Desafio 4
