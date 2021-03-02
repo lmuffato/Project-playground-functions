c45d882a7130d2c3736d66ad64cd8ac9ee3c48cd
PollyanaOliveira <pollyana.deoliveir@gmail.com>
2021-03-02 17:09:18 -0400

    ajustando exercicio 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,4 +1,6 @@
 // Desafio 1
+function compareTrue() {
+    //seu código aqui
 function compareTrue(param1, param2) {
   if (param1 === true && param2 === true) {
     return true;
@@ -12,14 +14,14 @@ function calcArea(base, height) {
   let area = (base * height) / 2;//objeto
   return area;
 }
-//console.log(calcArea(51, 1));
+console.log(calcArea(51, 1));
 
 // Desafio 3
 function splitSentence(string) {
   let phraseArray = string.split (' ');
   return phraseArray;
 }
-//console.log(splitSentence('Go Trybe'));
+console.log(splitSentence('Go Trybe'));
 //***Como usar o split: https://www.guj.com.br/t/o-metodo-split/100456
 
 // Desafio 4
@@ -28,14 +30,14 @@ function concatName(string) {
   name = string[(string.length - 1)] + ", " + string[0];
     return name;
 }
-  //console.log(concatName(['foguete', 'não', 'tem', 'ré']));
+console.log(concatName(['foguete', 'não', 'tem', 'ré']));
 
 // Desafio 5
 function footballPoints(wins, ties) {
   let points = wins * 3 + ties;    
   return points;
 }
-//console.log(footballPoints(14, 8));
+console.log(footballPoints(14, 8));
 
 // Desafio 6
 function highestCount(numbers) {
