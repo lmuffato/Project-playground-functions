6529713bfd3696b4d6459f931aa434cdd331d1e5
Gui-lira <gui.lira.martins@gmail.com>
2021-03-02 15:02:04 -0300

    exercicio 2

--- src/challenges.js
+++ src/challenges.js
@@ -1,14 +1,13 @@
 // Desafio 1
-function compareTrue(a,b) {
+function compareTrue(a, b) {
   if (a === true && b === true)
-      {return true}
-  else {return false}
+      { return true }
+  else { return false }
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
-}
+function calcArea(base, height) {
+ return (base*height)/2}
 
 // Desafio 3
 function splitSentence() {
