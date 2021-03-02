a07556169573b7fefd14b57582215572711accaf
AdelinoJnr <adelinojunior96@gmail.com>
2021-03-02 14:55:53 -0300

    Resoluçao dos requisitos 1 e 2

--- src/challenges.js
+++ src/challenges.js
@@ -1,17 +1,17 @@
 // Desafio 1
-function compareTrue(valueBoolOne, valueBoolTwo) {
-  if (valueBoolOne == true && valueBoolTwo == true) {
+function compareTrue(valueOne, valueTwo) {
+  if (valueOne === true && valueTwo === true) {
     return true
   } else {
     return false
   }
 }
 
-console.log(compareTrue(true, false))
-
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let area = (base * height) / 2;
+
+  return area
 }
 
 // Desafio 3
