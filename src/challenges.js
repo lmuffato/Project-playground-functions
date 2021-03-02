0d5973d8bb8c55a2ff8781e59d484040091e04ff
Jackson Pires <jackson.pires@gmail.com>
2021-03-02 14:45:56 -0300

    Requisito 01 - compareTrue

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,10 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(value1, value2) {
+  if(value1 == true && value2 == true) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Desafio 2
@@ -45,6 +49,9 @@ function encode() {
 function decode() {
   // seu código aqui
 }
+
+console.log(compareTrue(true, false));
+
 
 module.exports = {
   calcArea,
