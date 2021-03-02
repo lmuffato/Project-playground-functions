86023ca68cf38400453311f7241a3f9e886833ec
Priscila <prihscapin@gmail.com>
2021-03-02 19:14:30 -0300

    Requisito 3 - Separando string

--- src/challenges.js
+++ src/challenges.js
@@ -14,10 +14,12 @@ function calcArea(base, height) {
   }
 
 // Desafio 3
-function splitSentence() {
+function splitSentence(sentence) {
   // seu código aqui
+     return sentence.split(" ")
 }
 
+console.log(splitSentence("go trybe"))
 // Desafio 4
 function concatName() {
   // seu código aqui
