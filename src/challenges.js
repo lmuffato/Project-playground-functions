6a1c4bc10e7f4d97ca89be3c8971cef6d8241db7
Lucas André <lucas.cancado89@gmail.com>
2021-03-02 14:54:09 -0300

    Requisitos 1 e 2

--- src/challenges.js
+++ src/challenges.js
@@ -8,13 +8,13 @@ function compareTrue(bool1, bool2) {
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return (base * height)/2
 }
 
 // Desafio 3
 function splitSentence() {
-  // seu código aqui
+  
 }
 
 // Desafio 4
