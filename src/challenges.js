eb1145f3d95114919938012f8a4b0dbc1b077666
Gustavo Sousa <gustavohl98@gmail.com>
2021-03-02 16:02:08 -0300

    Requisito 07 - catAndMouse

--- src/challenges.js
+++ src/challenges.js
@@ -49,8 +49,16 @@ function highestCount(numbersArr) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  if (cat1 - mouse < cat2 - mouse) {
+    return 'cat1';
+  }
+
+  if (cat2 - mouse < cat1 - mouse) {
+    return 'cat2';
+  }
+
+  return 'os gatos trombam e o rato foge';
 }
 
 // Desafio 8
