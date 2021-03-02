27e783fc846515492d7f00e2fbf33d2cd210e898
Alexandre Oliveira <alexandremolive@gmail.com>
2021-03-02 16:28:01 -0300

    ajuste na identacao

--- src/challenges.js
+++ src/challenges.js
@@ -4,52 +4,44 @@ function compareTrue(boolOne, boolTwo) {
   if (boolOne && boolTwo === true) {
     return true;
   } return false;
-} console.log(compareTrue(true, true));
-
+}
 // Desafio 2
 function calcArea(base, height) {
   // seu código aqui
   let result = 0;
   result = (base * height) / 2;
   return console.log(result);
-} calcArea(51, 1);
-
+}
 // Desafio 3
 function splitSentence(string) {
   // seu código aqui
   let stringToSplit = '';
   stringToSplit = string.split(' ');
   return console.log(stringToSplit);
-} splitSentence('foguete');
-
+}
 // Desafio 4
 function concatName(name) {
   // seu código aqui
   return console.log(name[name.length - 1], name[0]);
-} concatName(['captain', 'my', 'captain']);
-
+}
 // Desafio 5
 function footballPoints(wins, ties) {
   // seu código aqui
   let resultEndGame = (wins * 3) + (ties * 1);
   return console.log(resultEndGame);
-} footballPoints(5, 5);
-
+}
 // Desafio 6
 function highestCount() {
   // seu código aqui
 }
-
 // Desafio 7
 function catAndMouse() {
   // seu código aqui
 }
-
 // Desafio 8
 function fizzBuzz() {
   // seu código aqui
 }
-
 // Desafio 9
 function encode() {
   // seu código aqui
@@ -57,6 +49,11 @@ function encode() {
 function decode() {
   // seu código aqui
 }
+console.log(compareTrue(true, true));
+calcArea(51, 1);
+splitSentence('foguete');
+concatName(['captain', 'my', 'captain']);
+footballPoints(5, 5);
 
 module.exports = {
   calcArea,
