15ec64d0fd6f9be552d4b9342f5b3b212e6f4dd7
Rafael <rafittu@outlook.com>
2021-03-02 16:52:31 -0300

    finalizado desafio 5

--- src/challenges.js
+++ src/challenges.js
@@ -23,8 +23,8 @@ function concatName(info) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return (wins * 3) + ties;
 }
 
 // Desafio 6
