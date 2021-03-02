e091f2388493e702b9fb0242b18b1d29b1bb5c9e
Renzo Sevilha <sevilharenzo@gmail.com>
2021-03-02 15:16:05 -0300

    [RESOLVIDO LINT] npm install realizado novamente

--- src/challenges.js
+++ src/challenges.js
@@ -14,8 +14,9 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+  let stringArray = string.split(' ');
+  return stringArray;
 }
 
 // Desafio 4
