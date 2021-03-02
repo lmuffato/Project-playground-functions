a6e3ee7b2410c43bdeb032b3a7f6501c68ab8ef7
Orlando Flores <orlando_flores@id.uff.br>
2021-03-02 15:05:25 -0300

    Requisito 2

--- src/challenges.js
+++ src/challenges.js
@@ -5,12 +5,13 @@ function compareTrue(boolean1, boolean2) {
   }
   return false;
 }
-console.log(compareTrue(true,true));
 
 // Desafio 2
-function calcArea() {
+function calcArea(base, height) {
+  let area = (base * height) / 2;
+  return area;
 }
-
+console.log(calcArea(51,1))
 // Desafio 3
 function splitSentence() {
   // seu código aqui
