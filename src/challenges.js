a51d66c54bb14fb4f0687df3ba020e5a38370704
Gui-lira <gui.lira.martins@gmail.com>
2021-03-02 15:26:56 -0300

    4

--- src/challenges.js
+++ src/challenges.js
@@ -14,8 +14,8 @@ function splitSentence(str) {
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(array) {
+  return array[array.length - 1] + ', ' + array[0]
 }
 
 // Desafio 5
