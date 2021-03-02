c408807e2652e224684bc61cab6519ff3b8185f4
Robertson <robertson.nunes@gmail.com>
2021-03-02 15:40:55 -0300

    Desafio 2 OK

--- src/challenges.js
+++ src/challenges.js
@@ -8,8 +8,9 @@ function compareTrue(value1, value2) {
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  calcArea = base * height / 2
+ return(calcArea);
 }
 
 // Desafio 3
