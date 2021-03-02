9133273da1e259a66f90c1faeeb08459c14ff761
Rodrigo Queiroz <rodrigoandradequeiroz@gmail.com>
2021-03-02 18:40:35 -0300

    reenviando o décimo primeiro requisito

--- src/challenges2.js
+++ src/challenges2.js
@@ -13,6 +13,7 @@ function techList(techArray, name) {
 
 // Desafio 11
 function generatePhoneNumber(array) {
+
   let phoneNumber = '';
   let countRepeatedNumbers = 0;
   let lastNumber = array[0]
@@ -24,16 +25,18 @@ function generatePhoneNumber(array) {
     return 'Array com tamanho incorreto.'
   }
   for (let index = 0; index < array.length; index += 1) {
-    if (array[index] < 0 || array[index] > 9 || countRepeatedNumbers > 3) {
-      return 'não é possível gerar um número de telefone com esses valores'
-    } else if (array[index] == lastNumber) {
-      countRepeatedNumbers += 1
-      phoneNumber += lastNumber
-    } else {
-      phoneNumber += array[index]
-    }
-  }
+    for (indexRepeated = index; indexRepeated < array.length; indexRepeated += 1){
+      if (array[index] < 0 || array[index] > 9 || countRepeatedNumbers >= 2) {
+        return 'não é possível gerar um número de telefone com esses valores'
+      } else if (array[index] == array[indexRepeated]) {
+        countRepeatedNumbers += 1
 
+      }
+    } 
+    countRepeatedNumbers = 0
+    phoneNumber += array[index]     
+  }
+  
   for (let index = 0; index < phoneNumber.length; index += 1) {
     if (index < 2) {
       firstPart += phoneNumber[index]
@@ -43,7 +46,11 @@ function generatePhoneNumber(array) {
       thirdPart += phoneNumber[index]
     }
   }
-  return '(' + firstPart + ')' + secondPart + '-' + thirdPart
+  
+  phoneNumber = '(' + firstPart + ')' + secondPart + '-' + thirdPart
+
+  return phoneNumber
+  
 }
 
 // Desafio 12
@@ -63,12 +70,13 @@ module.exports = {
   triangleCheck,
 };
 
-// console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]))
-// console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, -8, 9, 0, 1]))
-// console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 13, 0, 1]))
-// console.log(generatePhoneNumber([1, 2, 3, 3, 3, 3, 7, -8, 13, 0, 1]))
-// console.log(generatePhoneNumber([1, 0, 1, 5, 9,8,9,9,6,2,0,2,3,7,6,7,6]))
-
-
+// console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1])) // normal
+// console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, -8, 9, 0, 1])) // valor negativo
+// console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 13, 0, 1])) // valor acima de 9
+// console.log(generatePhoneNumber([1, 2, 3, 3, 3, 7, 5, 6, 0, 1, 3])) // valor repetido 4x
+// console.log(generatePhoneNumber([1, 0, 1, 5, 9,8,9,9,6,2,0,2,3,7,6,7,6])) // mais numeros que o esperado
+// console.log(generatePhoneNumber([1, 0, 1, 5, 6])) // menos numeros que o esperado
+// console.log(generatePhoneNumber([8, 3, 3, 3, 9, 4, 7, 8, 5, 0, 1])) // valor repetido 3x
+// console.log(generatePhoneNumber([0, 2, 3, 4, 4, 2, 7, 8, 9, 9, 4])) // teste do git - número repetido 3x
 
 
