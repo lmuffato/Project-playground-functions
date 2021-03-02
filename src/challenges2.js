345b931f4bcd29ff32a15da78cae22320b4da690
Gui-lira <gui.lira.martins@gmail.com>
2021-03-02 18:49:56 -0300

    tentativa 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -3,10 +3,12 @@ function techList(array, name) {
   // esse codigo foi baseado por essa discussão no stack overflow https://stackoverflow.com/questions/5868850/creating-list-of-objects-in-javascript
   let list = [];
   if (array.length === 0)
-  {return 'Vazio!'}
-  for (let index in array)
-  {
-    list.push({ tech: array[index], name: name })
+  { 
+    return 'Vazio!';
+  }
+else {for (let index = 0;index < array.length;index += 1)
+     {list.push({tech: array[index], name: name})
+    }
   }
   return list
 }
