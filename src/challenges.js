77d625098527684c23cd019a1e68e7913cd4b47b
Mateus Martins - Turma 10 - Tribo A <mateusmartinsmck@gmail.com>
2021-03-02 17:38:54 -0300

    Ajustados erros do Linter - desafios 1 ao 6

--- src/challenges.js
+++ src/challenges.js
@@ -1,52 +1,47 @@
 // Desafio 1
 function compareTrue(firstBoolean, secondBoolean) {
-  if (firstBoolean && secondBoolean){
+  if (firstBoolean && secondBoolean) {
     return true;
-  } else {
-    return false;
   }
+  return false;
 }
 
 // Desafio 2
 function calcArea(base, height) {
-  return (base*height)/2;
+  return (base * height) / 2;
 }
-
 
 // Desafio 3
 function splitSentence(frase) {
   return frase.split(' ');
 }
 
-
 // Desafio 4
 function concatName(arrayName) {
-  return (arrayName[arrayName.length-1]+ ', ' + arrayName[0]);
+  return (arrayName[arrayName.length - 1] + ', ' + arrayName[0]);
 }
 
 // Desafio 5
 function footballPoints(wins, ties) {
-  return (wins*3)+(ties);
+  return (wins * 3)+(ties);
 }
-
 
 // Desafio 6
 function highestCount(arrayNumbers) {
   let count = 0;
   let highestNumber = 0;
   for (let index in arrayNumbers) {
-    if (arrayNumbers[index]>highestNumber){
+    if (arrayNumbers[index] > highestNumber) {
       highestNumber = arrayNumbers[index];
     }
   }
   for (let index in arrayNumbers) {
-    if (arrayNumbers[index]===highestNumber){
+    if (arrayNumbers[index] === highestNumber) {
       count += 1;
     }
   }
   return count;
 }
-console.log(highestCount([0, 0, 0]));
 
 // Desafio 7
 function catAndMouse() {
