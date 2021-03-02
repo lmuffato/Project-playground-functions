d6ba8092ca9b3ebb12a753bfee90d58aa9d36ba4
Stanrley Barbosa <3011stan@gmail.com>
2021-03-02 20:04:20 -0300

    Desafio 10 do projeto

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,20 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(arrayTechs, name) {
+  let arrayNamesSort = arrayTechs.sort(); //ordenando arrayTechs.
+  let arrayObjectInsert = []; //criando array vazio que será lotado de objetos e retornado.
+  for(let i = 0; i < arrayNamesSort.length; i += 1){
+    let objectInsert = {}; //criando objeto vazio que será preenchido e colocado no arrayObjectInsert
+    objectInsert['tech'] = arrayNamesSort[i];
+    objectInsert['name'] = name;
+    // objectInsert[i].tech = arrayNamesSort[i];
+    // objectInsert[i].name = name;
+    arrayObjectInsert.push(objectInsert);
+  }
+  return arrayObjectInsert;
 }
+// array = ['React', 'Jest', 'HTML', 'CSS', 'JavaScript'];
+// console.log(techList(array, 'stanrley'));
+
 
 // Desafio 11
 function generatePhoneNumber() {
