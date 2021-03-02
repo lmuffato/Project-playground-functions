b5c95c0e09bdc38a23c6a53332c0941380298a2b
André Jaques Batista <andrejaques.b@gmail.com>
2021-03-02 20:16:13 -0300

    refatored the function 08 - fizzBuzz and made part of the function 09 - encode n decode

--- src/challenges.js
+++ src/challenges.js
@@ -61,74 +61,69 @@ function catAndMouse(mouse, cat1, cat2) {
 function fizzBuzz(arrayN8) {
   let arrayResultN8 = arrayN8;
   for (let index in arrayN8) {
-    if ((arrayN8[index] % 3) === 0 && (arrayN8[index] % 5) === 0) {
-      arrayResultN8[index] = 'fizzBuzz';
-    } else if ((arrayN8[index] % 3) === 0 && (arrayN8[index] % 5) !== 0) {
-      arrayResultN8[index] = 'fizz';
-    } else if ((arrayN8[index] % 3) !== 0 && (arrayN8[index] % 5) === 0) {
-      arrayResultN8[index] = 'buzz';
-    } else {
-      arrayResultN8[index] = 'bug!';
-    }
+    arrayResultN8[index] = phrase(arrayN8[index]);
   }
   return arrayResultN8;
 }
-console.log(fizzBuzz([9, 25]))
+
+function phrase(number) {
+  if ((number % 3) === 0 && (number % 5) === 0) {
+    return 'fizzBuzz';
+  } else if ((number % 3) === 0 && (number % 5) !== 0) {
+    return 'fizz';
+  } else if ((number % 3) !== 0 && (number % 5) === 0) {
+    return 'buzz';
+  } else {
+    return 'bug!';
+  }
+}
 
 // Desafio 9
 function encode(stringIn) {
   let encodedString = stringIn;
-  for (let indexEnc in encodedString) {
-    switch (encodedString[indexEnc]) {
-      case 'a': 
-        encodedString[indexEnc] = '1';
-        break;
-      case 'e':
-        encodedString[indexEnc] = '2';
-        break;
-      case 'i':
-        encodedString[indexEnc] = '3';
-        break;
-      case 'o':
-        encodedString[indexEnc] = '4';
-        break
-      case 'u':
-        encodedString[indexEnc] = '5';
-        break;
-      default:
-        encodedString[indexEnc] = encodedString[indexEnc];
-    }
+  for (let indexIn in stringIn) {
+    encodedString[indexIn] = convert(stringIn[indexIn]);
   }
   return encodedString;
+}
+
+console.log(encodedString[indexIn]);
+
+function convert(stringX) {
+  switch (stringX) {
+    case 'a': 
+      return '1';
+    case 'e':
+      return '2';
+    case 'i':
+      return '3';
+    case 'o':
+      return '4';
+    case 'u':
+      return '5';
+    case '1': 
+      return 'a';
+    case '2':
+      return 'e';
+    case '3':
+      return 'i';
+    case '4':
+      return 'o';
+    case '5':
+      return 'u';
+    default:
+      return stringX;
+  }
 }
 
 function decode(stringOut) {
   let decodedString = stringOut;
 
   for (let indexDec in decodedString) {
-    switch (decodedString[indexDec]) {
-      case '1': 
-        decodedString[indexDec] = 'a';
-        break;
-      case '2':
-        decodedString[indexDec] = 'e';
-        break;
-      case '3':
-        decodedString[indexDec] = 'i';
-        break;
-      case '4':
-        decodedString[indexDec] = 'o';
-        break;
-      case '5':
-        decodedString[indexDec] = 'u';
-        break;
-      default:
-        decodedString[indexDec] = decodedString[indexDec];
-    }
+    decodedString[indexDec] = convert(stringIn[indexDec])
   }
   return decodedString;
 }
-
 
 
 
