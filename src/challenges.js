01f01a0d9abfaf783b3c7b83e1e406195ca73705
Lucas André <lucas.cancado89@gmail.com>
2021-03-02 14:32:36 -0300

    Commit inicial - iniciando o projeto

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,10 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(bool1, bool2) {
+  if (bool1 === true && bool2 === true) {
+    return true
+  } else {
+    return false
+  }
 }
 
 // Desafio 2
