5a1496140b4f889e4874cb9d218ad89c62c5a2ab
diogomarin <diogoamarin@gmail.com>
2021-03-02 16:24:08 -0300

    Fifth chanllenge, ok.

--- src/challenges.js
+++ src/challenges.js
@@ -20,12 +20,11 @@ function splitSentence(string) {
 
 // Desafio 4
 function concatName(array) {
-  return array[array.length - 1] + ", " + array[0];
+  return (array[array.length - 1] + ', ' + array[0]);
 }
 
-// Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return (wins * 3) + ties;
 }
 
 // Desafio 6
