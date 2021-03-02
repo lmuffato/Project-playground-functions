f5d76f3573fb72edd8431de84f4902425159b230
Lucas André <lucas.cancado89@gmail.com>
2021-03-02 16:08:30 -0300

    Requisito 7

--- src/challenges.js
+++ src/challenges.js
@@ -47,8 +47,14 @@ function highestCount(arr) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  if (Math.abs(cat1 - mouse) < Math.abs(cat2 - mouse)) {
+    return 'cat1'
+  } else if (Math.abs(cat1 - mouse) > Math.abs(cat2 - mouse)) {
+    return 'cat2'
+  } else {
+    return 'os gatos trombam e o rato foge'
+  }
 }
 
 // Desafio 8
