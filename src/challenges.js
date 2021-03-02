98214a06bbae61d72f77f1aad766926305fc9457
johnpligeiro <johnpligeiro@gmail.com>
2021-03-02 13:13:43 -0300

    Requisito 3.

--- src/challenges.js
+++ src/challenges.js
@@ -16,8 +16,8 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(sentence) {
+  return sentence.split(" ");
 }
 
 // Desafio 4
