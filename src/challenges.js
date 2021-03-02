9004f1a94e6337d1030e9f888c871f50adc82c47
ThalitaC <thalitaceci@gmail.com>
2021-03-02 15:52:09 -0300

    Requisito 2: Crie uma função que calcule a área do triângulo

--- src/challenges.js
+++ src/challenges.js
@@ -7,8 +7,9 @@ function compareTrue(value1, value2) {
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let area = (base * height) / 2;
+  return area;
 }
 
 // Desafio 3
