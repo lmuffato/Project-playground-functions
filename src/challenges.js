e5f10ea7ac4f8f2e2a3b1457f40ed7bb3e9b4105
Lucas André <lucas.cancado89@gmail.com>
2021-03-02 15:35:36 -0300

    Requisito 5

--- src/challenges.js
+++ src/challenges.js
@@ -25,8 +25,8 @@ function concatName(arr) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return (wins * 3 + ties * 1)
 }
 
 // Desafio 6
