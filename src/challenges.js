6642bc7cd3048931f8fafc3d2d3144df285ab112
Felipe Muller <felipeavmuller@gmail.com>
2021-03-02 15:11:59 -0300

    Iniciando o projeto

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,11 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(num1, num2) {
+  if (num1 === true && num2 === true) {
+    return true;
+    else {
+      return false;
+    }
+  }
 }
 
 // Desafio 2
