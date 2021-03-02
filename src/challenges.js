b3c66d5a3640613697984d0397a4cb96a9b5a0ee
Mateus Alencar <mtauws@gmail.com>
2021-03-02 15:54:52 -0300

    chore: exercise 5

--- src/challenges.js
+++ src/challenges.js
@@ -34,8 +34,8 @@ function concatName(array) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return (wins * 3) + (ties * 1);
 }
 
 // Desafio 6
