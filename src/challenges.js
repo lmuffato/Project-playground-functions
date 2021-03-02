27d9fba5e4367e493c7124fd499777dc182984a9
Thiago <thiagodesouza.dev@gmail.com>
2021-03-02 18:33:17 -0300

    atividade 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,15 +1,16 @@
 // Desafio 1
 function compareTrue(value1,value2) {
   if (value1 === true && value2 === true) {
-    return(true);
+    return true;
   }
-  return(false);
+  return false;
 }
-console.log(compareTrue([valie1,value2]))
+
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base,height) {
+  let  area = (base * height) / 2;
+  return area;
 }
 
 // Desafio 3
