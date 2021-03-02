11b06774ca2cf6b6093f7f528e46214d8b513aa3
Mateus Alencar <mtauws@gmail.com>
2021-03-02 15:46:50 -0300

    chore: exercise 4

--- src/challenges.js
+++ src/challenges.js
@@ -13,14 +13,24 @@ function calcArea(base, height) {
 
 // Desafio 3
 function splitSentence(string) {
+  /**
+   * Me basei um pouco no código do Rich Rhinoceros, link do repositório: https://www.codegrepper.com/code-examples/javascript/javascript+split+string+by+space
+   */
   const array = string.split(' ');
 
   return array;
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(array) {
+  let firstItem = array[0];
+  /**
+   * Para pegar o último elemento de um array segui de acordo com o código do LINQ,
+   * segue o link do StackOverFlow: https://pt.stackoverflow.com/questions/87416/como-retornar-o-%C3%BAltimo-registro-de-um-array-com-javascript-ou-jquery
+   */
+  let lastItem = array[array.length - 1];
+
+  return `${lastItem}, ${firstItem}`;
 }
 
 // Desafio 5
