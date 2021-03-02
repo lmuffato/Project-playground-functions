fef2858b56f49457717d37d55636bc1f3b3dcd92
Jodiel Briesemeister <jodielll29@gmail.com>
2021-03-02 16:57:49 -0300

    adicionado exercícios 5, 6, 7, 8, 9 e 10

--- src/challenges.js
+++ src/challenges.js
@@ -5,7 +5,7 @@ function compareTrue(parameter1, parameter2) {
   if (parameter1 && parameter2 === true){
     compara = true;
   }
-  else{
+  else {
     compara = false;
   }
   return compara;
@@ -39,10 +39,10 @@ function concatName(list) {
   let firstName;
   let concat;
   for(let index = 0; index < list.length; index += 1){
-    if (index == list.length - 1){
+    if (index === list.length - 1){
       lastName = list[index];
     }
-    else if(index == 0){
+    else if (index === 0){
       firstName = list[index];
     }
   }
@@ -55,32 +55,123 @@ let arrayNames = ['captain', 'my', 'captain']
 console.log(concatName(arrayNames));
 
 // Desafio 5
-function footballPoints() {
+function footballPoints(wins, ties) {
   // seu código aqui
+  let calc = (wins * 3) + (ties * 1)
+  return calc
 }
+
+let win = 0;
+let tie = 0;
+
+console.log(footballPoints(win, tie))
 
 // Desafio 6
-function highestCount() {
+function highestCount(number) {
   // seu código aqui
+  let higher = number[0]
+  let repet = 0;
+  for (let index = 0; index < number.length; index += 1){
+    if (higher < number[index]){
+      higher = number [index];
+    }
+  }
+  for (let index = 0; index < number.length; index += 1){
+    if (number[index] === higher){
+      repet += 1;
+    }
+  }
+  return repet
 }
+
+let arrayNumbers = [0, 0, 0]
+
+console.log(highestCount(arrayNumbers))
 
 // Desafio 7
-function catAndMouse() {
+function catAndMouse(mouse, cat1, cat2) {
   // seu código aqui
+  let winner;
+  if ((cat2 - mouse) < (cat1 - mouse)){
+    winner = 'cat2';
+  }
+  else if ((cat2 - mouse) > (cat1 - mouse)){
+    winner = 'cat1';
+  }
+  else if ((cat2 - mouse) === (cat1 - mouse)){
+    winner = 'os gatos trombam e o rato foge';
+  }
+  return winner
 }
+
+let mouse = 0; 
+let cat1 = 12;
+let cat2 = 12;
+
+console.log(catAndMouse(mouse, cat1, cat2));
 
 // Desafio 8
-function fizzBuzz() {
+function fizzBuzz(numbers) {
   // seu código aqui
+  let result = [];
+  for (let index = 0; index < numbers.length; index += 1){
+    if (numbers[index] % 3 === 0 && numbers[index] % 5 === 0){
+      result[index] = "fizzBuzz";
+    }
+    else if (numbers[index] % 3 != 0 && numbers[index] % 5 != 0){
+      result[index] = "bug!";
+    }
+    else if (numbers[index] % 3 === 0){
+      result[index] = "fizz";
+    }
+    else if (numbers[index] % 5 === 0){
+      result[index] = "buzz";
+    }
+  }
+  return result
 }
 
+let arrayNumbers2 = [7, 9];
+console.log(fizzBuzz(arrayNumbers2))
+
 // Desafio 9
-function encode() {
+function encode(param) {
   // seu código aqui
+  // links estudados para a realização do código: https://www.devmedia.com.br/javascript-replace-substituindo-valores-em-uma-string/39176
+  //                                              https://stackoverflow.com/questions/15604140/replace-multiple-strings-with-multiple-other-strings
+  let vowels = {
+    a: '1',
+    e: '2',
+    i: '3',
+    o: '4',
+    u: '5'
+  };
+  
+  result = param.replace(/a|e|i|o|u/g, function(param){
+    return vowels[param]
+  });
+  return result
 }
-function decode() {
+function decode(param) {
   // seu código aqui
+  // links estudados para a realização do código: https://www.devmedia.com.br/javascript-replace-substituindo-valores-em-uma-string/39176
+  //                                              https://stackoverflow.com/questions/15604140/replace-multiple-strings-with-multiple-other-strings
+  let numbers = {
+    1: 'a',
+    2: 'e',
+    3: 'i',
+    4: 'o',
+    5: 'u'
+  };
+  
+  result = param.replace(/1|2|3|4|5/g, function(param){
+    return numbers[param]
+  });
+  return result
 }
+
+console.log("Encode: " + encode("Hi there"))
+console.log("Decode: " + decode("H3 th2r2"))
 
 module.exports = {
   calcArea,
--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,30 @@
 // Desafio 10
-function techList() {
+function techList(tech, name) {
   // seu código aqui
+  // links estudados para a realização do código: https://www.w3schools.com/jsref/jsref_sort.asp
+  let techSort = tech.sort();
+  let technologies = [];
+  let technologiesResult;
+  
+  if (tech.length >= 1){
+    for (let index = 0; index < tech.length; index += 1){
+      technologies[index] = {
+        tech: techSort[index],
+        name: name
+      }
+    }
+    technologiesResult = technologies;
+  }
+  else {
+    technologiesResult = "Vazio!"
+  }
+  return technologiesResult;
 }
+
+let arrayTech = ["React", "Jest", "HTML", "CSS", "JavaScript"];
+let myName = "Jodiel";
+
+console.log(techList(arrayTech, myName))
 
 // Desafio 11
 function generatePhoneNumber() {
