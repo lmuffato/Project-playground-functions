e48da7cb537e07a20983b08c2b234c97c69fa55e
ThalitaC <thalitaceci@gmail.com>
2021-03-02 17:23:23 -0300

    corrige parametro da funcao do req 4

--- src/challenges.js
+++ src/challenges.js
@@ -19,9 +19,9 @@ function splitSentence(string) {
 }
 
 // Desafio 4
-function concatName([array]) {
-  let names = `${array[array.length - 1]}, ${array[0]}`;
-  return names;
+function concatName(array) {  
+  let concat = `${array[0]}, ${array[array.length - 1]}`;
+  return concat;
 }
 
 // Desafio 5
