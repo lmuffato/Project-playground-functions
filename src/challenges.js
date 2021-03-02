fb7ad602ea23a50b9d277d82aacfc38a184cdad4
Alex Smith <alexsmith7192@gmail.com>
2021-03-02 17:48:35 -0300

    3 - Cria função que divide frase

--- src/challenges.js
+++ src/challenges.js
@@ -13,8 +13,8 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(phrase) {
+  return phrase.split(' ');
 }
 
 // Desafio 4
