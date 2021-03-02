bad8b11d5be686d25280688528735c2c3858f2d0
Patrick Morais <ppternunes@gmail.com>
2021-03-02 12:26:04 -0300

    lint e requisito 7

--- src/challenges.js
+++ src/challenges.js
@@ -43,11 +43,19 @@ function highestCount(numbers) {
   }
   return repeats;
 }
-}
+
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let positionCat1 = Math.abs(mouse - cat1);
+  let positionCat2 = Math.abs(mouse - cat2);
+  if (positionCat1 > positionCat2) {
+    return 'cat2';
+  } else if (positionCat1 < positionCat2) {
+    return 'cat1';
+  } else {
+    return 'os gatos trombam e o rato foge';
+  }
 }
 
 // Desafio 8
