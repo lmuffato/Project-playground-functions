81fda867ec27fa19326c26d8a597c6b49a06d50a
Patrick Morais <ppternunes@gmail.com>
2021-03-02 18:57:39 -0300

    alteracoes finais

--- src/challenges2.js
+++ src/challenges2.js
@@ -21,8 +21,7 @@ function techList(tech, name) {
 
 // Desafio 11
 function numFormat(num) {
-  return `(${num[0]}${num[1]}) ${num[2]}${num[3]}${num[4]}
-    ${num[5]}${num[6]}-${num[7]}${num[8]}${num[9]}${num[10]}`;
+  return `(${num[0]}${num[1]}) ${num[2]}${num[3]}${num[4]}${num[5]}${num[6]}-${num[7]}${num[8]}${num[9]}${num[10]}`;
 }
 
 function checkNumber(num) {
