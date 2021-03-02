df7389638638bad2b626ce00a93545bc4aae3520
LuanAlexandre <luanrb19@gmail.com>
2021-03-02 14:40:03 -0400

    Dia 4.5 - Projeto2-playground-functions: Requisito 2

--- src/challenges.js
+++ src/challenges.js
@@ -10,8 +10,9 @@ function compareTrue(value1, value2) {
 }
 
 // Desafio 2
-function calcArea() {
+function calcArea(base, height) {
   // seu código aqui
+  return (base * height) / 2;
 }
 
 // Desafio 3
