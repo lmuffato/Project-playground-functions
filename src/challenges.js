f22fd962d5f175958f6ea9fc4e34f507436c38ee
rcalvs <rafaelcalvette@gmail.com>
2021-03-02 16:30:10 -0300

    Desafio 1-2-3

--- src/challenges.js
+++ src/challenges.js
@@ -1,15 +1,18 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(param1, param2) {
+  if (param1 === 'true' && param2 === 'true')
+    return = 'true'
+} else {
+    return = 'false'
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return = (base * height) / 2
 }
 
 // Desafio 3
-function splitSentence() {
+function splitSentence(string) {
   // seu código aqui
 }
 
---
+++ testes.js
@@ -0,0 +1,10 @@
+let string = "go Trybe"
+let split = []
+
+function splitSentence(string) {
+    for (let index = 0; index < string.length; index += 1)
+        let split = string[index];
+
+}
+
+console.log(string)
