8c490288880495159ade8596d77da683d78bb72d
Samuel Melo <samuelmmjunior@gmail.com>
2021-03-02 19:40:54 -0300

    desafio 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,9 +1,13 @@
 // Desafio 1
 function compareTrue(value1, value2) {
- if value1 == value2 {
-   
+  if (value1 == true && value2 == true) {
+    return true
+  } else{
+    return false
+  }
  }
-}
+compareTrue(true, true)
+console.log (return)
 
 // Desafio 2
 function calcArea() {
