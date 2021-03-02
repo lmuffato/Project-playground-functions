9866cab696010240ab3177611b92efbcbd8ef158
Rafael Medeiros <62731714+RafaelMedeirosGomes@users.noreply.github.com>
2021-03-02 15:23:22 -0300

    Done 5

--- src/challenges.js
+++ src/challenges.js
@@ -21,8 +21,8 @@ function concatName(arrayOfStrings) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return (3 * wins + ties);
 }
 
 // Desafio 6
