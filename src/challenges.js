4890d73953d8f379e19feddf5c9d8db62a53ea9a
diegoCorreia <diegomethal@gmail.com>
2021-03-02 15:51:38 -0300

    requisito 1 compareTrue

--- src/challenges.js
+++ src/challenges.js
@@ -1,7 +1,13 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(value1, value2) {
+  if(value1 === true && value2 === true){
+    return true;
+  }else{
+    return false;
+  }
+  
 }
+console.log(compareTrue(true,true));
 
 // Desafio 2
 function calcArea() {
@@ -58,3 +64,4 @@ module.exports = {
   highestCount,
   splitSentence,
 };
+
