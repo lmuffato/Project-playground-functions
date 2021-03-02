d531c4a523f3661019573d867f717f192951f164
Alex Smith <alexsmith7192@gmail.com>
2021-03-02 18:53:15 -0300

    5 - Cria função que calcula a quantidade de pontos

--- src/challenges.js
+++ src/challenges.js
@@ -24,8 +24,9 @@ function concatName(stringList) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let points = ((wins * 3) + (ties));
+  return points;
 }
 
 // Desafio 6
