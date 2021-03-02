31a341462d836c489e114f090c8610d29c137dfe
Rodrigo Rech <rodrigorech92@gmail.com>
2021-03-02 16:11:22 -0300

    Problem 5 footballPoints done

--- src/challenges.js
+++ src/challenges.js
@@ -3,7 +3,8 @@ function compareTrue(value1, value2) {
   if (value1 === true && value2 === true) {
     return true;
   } else {
-    return false;}
+    return false; 
+  }
 }
 
 // Desafio 2
@@ -22,13 +23,14 @@ function splitSentence(sentence) {
 
 // Desafio 4
 function concatName(array) {
-  let result = array[array.length - 1] + ', ' + array[0];
+  let result = `${array[array.length - 1]}, ${array[0]}`;
   return result;
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let result = wins * 3 + ties;
+  return result;
 }
 
 // Desafio 6
