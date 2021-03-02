2b480de989b9eac66183ed93fd894c3b9b476def
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-03-02 17:16:18 -0300

    Desafio 5: Função para retornar a quantidade de pontos que um time marcou em um campeonado implementada - função footballPoints

--- src/challenges.js
+++ src/challenges.js
@@ -19,8 +19,8 @@ function concatName(array) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return (wins * 3) + (ties * 1);
 }
 
 // Desafio 6
