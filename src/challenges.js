0da28092556e874c73623933dd9f771d0a5254cd
Cla Oliveira <clarice.coliveira@yahoo.com.br>
2021-03-02 15:26:11 -0300

    Requisito 1 - compareTrue

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,10 @@
 // Desafio 1
-function compareTrue() {
-  
+function compareTrue(value1, value2) {
+  if(value1 && value2) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Desafio 2
