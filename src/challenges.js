b8eecd823faeddf2fb773f9cdadb08ba920ca0ec
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-03-02 15:11:14 -0300

    exercicio1

--- src/challenges.js
+++ src/challenges.js
@@ -1,7 +1,13 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(valor1, valor2) {
+  if (valor1 === true && valor2 === true) {
+    return true;
+  } else {
+    return false
+  }
 }
+
+console.log(compareTrue(true,false));
 
 // Desafio 2
 function calcArea() {
