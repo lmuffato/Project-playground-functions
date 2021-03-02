c7be55be01014601419a101ace11e035ecc5a20e
Maria Luiza Castelar <mluiza.vcr@gmail.com>
2021-03-02 17:58:48 -0300

    Desafio 1: compareTrue

--- src/challenges.js
+++ src/challenges.js
@@ -1,7 +1,14 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
-}
+function compareTrue(value1, value2) {
+  // eslint-disable-next-line sonarjs/no-redundant-boolean
+  // eslint-disable-next-line eqeqeq
+  if (value1 == true && value2 == true){
+  return true
+  }  
+  return false;
+} 
+
+console.log(compareTrue(true, false))
 
 // Desafio 2
 function calcArea() {
