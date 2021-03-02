5059b4b6971ea0a0b37a595e566b54ad0e4d7a24
Cla Oliveira <clarice.coliveira@yahoo.com.br>
2021-03-02 16:53:11 -0300

    Requisito 5 - footballPoints

--- src/challenges.js
+++ src/challenges.js
@@ -34,8 +34,8 @@ function concatName(arr) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return wins * 3 + ties * 1;
 }
 
 // Desafio 6
