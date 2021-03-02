76512f90bb6e86b821b6848de8eb128fc0fc3b4f
Ana Ventura <anaventura1811@gmail.com>
2021-03-02 15:03:45 -0300

    Inicia o Projeto Playground Functions e adiciona resolução do Desafio 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,10 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(notaProjeto, frequenciaAula) {
+  if (notaProjeto === true && frequenciaAula === true) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Desafio 2
