e474ea10736efd62d5a0654d5256123ee107e543
Rodrigo Queiroz <rodrigoandradequeiroz@gmail.com>
2021-03-02 18:05:14 -0300

    décimo primeiro requisito

--- src/challenges2.js
+++ src/challenges2.js
@@ -12,8 +12,38 @@ function techList(techArray, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  let phoneNumber = '';
+  let countRepeatedNumbers = 0;
+  let lastNumber = array[0]
+  let firstPart = ''
+  let secondPart = ''
+  let thirdPart = ''
+
+  if (array.length != 11) {
+    return 'Array com tamanho incorreto.'
+  }
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] < 0 || array[index] > 9 || countRepeatedNumbers > 3) {
+      return 'não é possível gerar um número de telefone com esses valores'
+    } else if (array[index] == lastNumber) {
+      countRepeatedNumbers += 1
+      phoneNumber += lastNumber
+    } else {
+      phoneNumber += array[index]
+    }
+  }
+
+  for (let index = 0; index < phoneNumber.length; index += 1) {
+    if (index < 2) {
+      firstPart += phoneNumber[index]
+    } else if (index < 7) {
+      secondPart += phoneNumber[index]
+    } else {
+      thirdPart += phoneNumber[index]
+    }
+  }
+  return '(' + firstPart + ')' + secondPart + '-' + thirdPart
 }
 
 // Desafio 12
@@ -33,6 +63,12 @@ module.exports = {
   triangleCheck,
 };
 
-console.log(techList([], "Lucas"))
-console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas"))
+// console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]))
+// console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, -8, 9, 0, 1]))
+// console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 13, 0, 1]))
+// console.log(generatePhoneNumber([1, 2, 3, 3, 3, 3, 7, -8, 13, 0, 1]))
+// console.log(generatePhoneNumber([1, 0, 1, 5, 9,8,9,9,6,2,0,2,3,7,6,7,6]))
+
+
+
 
