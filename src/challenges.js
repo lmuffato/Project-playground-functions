da8b89a188e6b758f55e4d4a061d1ad734982fdd
Bruno Mendes <mendessbruno@gmail.com>
2021-03-02 17:06:02 -0300

    resoluçao exercicio 3

--- src/challenges.js
+++ src/challenges.js
@@ -12,8 +12,9 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(word) {
+  let splitted = word.split(' ');
+  return splitted;
 }
 
 // Desafio 4
