ca2a992fb23e70865819561650239b786662e8f5
Cla Oliveira <clarice.coliveira@yahoo.com.br>
2021-03-02 15:32:25 -0300

    Requisito 2 - calcArea

--- src/challenges.js
+++ src/challenges.js
@@ -8,9 +8,11 @@ function compareTrue(value1, value2) {
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return (base * height) / 2;
 }
+
+console.log(calcArea(51, 1));
 
 // Desafio 3
 function splitSentence() {
