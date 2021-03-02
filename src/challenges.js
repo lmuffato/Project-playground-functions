364bf3523503eb4fb52e4fa7834c54cc089cabaf
luanlsr <luanfswd@gmail.com>
2021-03-02 19:20:40 -0300

    requisito 7 alterado

--- src/challenges.js
+++ src/challenges.js
@@ -29,9 +29,9 @@ function footballPoints(wins, ties) {
 }
 
 // Desafio 6
-//Este código criei baseado na minha amiga de Tribo Cla Oliveira que me ajudou demais nesse projeto.
-//Recriou a função comigo pausadamente me explicando cada detalhe e me fazendo entender o por quê de cada coisa.
-//Obrigado Cla Oliveira!!!
+// Este código criei baseado na minha amiga de Tribo Cla Oliveira que me ajudou demais nesse projeto.
+// Recriou a função comigo pausadamente me explicando cada detalhe e me fazendo entender o por quê de cada coisa.
+// Obrigado Cla Oliveira!!!
 function higherNumber(arrayOfNumber) {
   let highest = arrayOfNumber[0];
   for (let index = 0; index < arrayOfNumber.length; index += 1) {
@@ -50,28 +50,21 @@ function highestCount(arrayOfNumber) {
       higherRepeated += 1;
     }
   }
- 
+
 return higherRepeated;
 }
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
   let catchMouse = 0
-  let distanceCat1 = 0
-  let distanceCat2 = 0
-  if (mouse > cat1) {
-    distanceCat1 = mouse - cat1;
-  }else{
-    distanteCat1 = cat1 - mouse
-  }
-  if (mouse > cat1) {
-    distanceCat2 = mouse - cat2;
-  } else{
-    distanceCat2 = cat2 - mouse
-  }
-  if (distanceCat1 > distanceCat2) {
+  let distanceCat1 = mouse - cat1;
+  let distanceCat2 = mouse - cat2;
+  let distanceCat1Abs = Math.abs(distanceCat1)
+  let distanceCat2Abs = Math.abs(distanceCat2)
+
+  if (distanceCat1Abs < distanceCat2Abs) {
     catchMouse = 'cat1';
-  }else if (distanceCat2 > distanceCat1){
+  }else if (distanceCat2Abs < distanceCat1Abs){
     catchMouse = 'cat2';
   }else{
     catchMouse = 'os gatos trombam e o rato foge!'
