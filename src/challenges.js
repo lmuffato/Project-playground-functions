ff3f178842fde6cd88a278271e90d008a970e8d7
ThalitaC <thalitaceci@gmail.com>
2021-03-02 17:50:15 -0300

    Requisito 5: Crie uma função que calcule a quantidade de pontos no futebol

--- src/challenges.js
+++ src/challenges.js
@@ -27,8 +27,11 @@ function concatName(array) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let winPoints = wins * 3;
+  let tiePoints = ties * 1;
+  let totalPoints = winPoints + tiePoints;
+  return totalPoints;
 }
 
 // Desafio 6
