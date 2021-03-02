c61c1b5f08ff371aa2c25b0753007fe052f50cbe
Rafael <rafittu@outlook.com>
2021-03-02 15:27:13 -0300

    finalizado o desafio 2

--- src/challenges.js
+++ src/challenges.js
@@ -1,17 +1,15 @@
 // Desafio 1
 function compareTrue(value1, value2) {
-  if (value1 == true && value2 == true){
+  if (value1 == true && value2 == true) {
     return true;
   } else {
     return false;
   }
 }
 
-console.log(compareTrue(true, true));
-
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return (base * height) / 2;
 }
 
 // Desafio 3
