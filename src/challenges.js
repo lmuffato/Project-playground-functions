7e1842d67288242c6240d7475c74d996b5b369ad
Rafael Medeiros <62731714+RafaelMedeirosGomes@users.noreply.github.com>
2021-03-02 15:37:57 -0300

    Done 6

--- src/challenges.js
+++ src/challenges.js
@@ -1,35 +1,51 @@
 // Desafio 1
 function compareTrue(bool1, bool2) {
-  return (bool1 && bool2);
+  return bool1 && bool2;
 }
 
 // Desafio 2
 function calcArea(base, height) {
-  return ((base * height) / 2);
+  return (base * height) / 2;
 }
 
 // Desafio 3
 function splitSentence(string) {
-  return (string.split(' '));
+  return string.split(' ');
 }
 
 // Desafio 4
 function concatName(arrayOfStrings) {
   let lastString = arrayOfStrings[arrayOfStrings.length - 1];
   let firstString = arrayOfStrings[0];
-  return (`${lastString}, ${firstString}`);
+  return `${lastString}, ${firstString}`;
 }
 
 // Desafio 5
 function footballPoints(wins, ties) {
-  return (3 * wins + ties);
+  return 3 * wins + ties;
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(arrayOfNumbers) {
+  let dict = {};
+  for (let number in arrayOfNumbers) {
+    if (typeof dict[arrayOfNumbers[number]] === 'undefined') {
+      dict[arrayOfNumbers[number]] = 1;
+    } else {
+      dict[arrayOfNumbers[number]] += 1;
+    }
+  }
+  let highest = -1 * Number.MAX_VALUE;
+  for (let key in dict) {
+    if (dict[key] > highest) {
+      highest = dict[key];
+    }
+  }
+  return highest;
 }
-
+console.log(highestCount([9, 1, 2, 3, 9, 5, 7]));
+console.log(highestCount([0, 4, 4, 4, 9, 2, 1]));
+console.log(highestCount([0, 0, 0]));
 // Desafio 7
 function catAndMouse() {
   // seu código aqui
