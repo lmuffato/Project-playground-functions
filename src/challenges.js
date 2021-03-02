163cb88b07a7d2bd30ba2709787d8dac863b1a7a
Guilherme Sucupira Oliveira <guilherme.so23@gmail.com>
2021-03-02 16:29:16 -0300

    2. Concluido funcao calcArea

--- src/challenges.js
+++ src/challenges.js
@@ -8,8 +8,8 @@ function compareTrue(conditionOne, conditionTwo) {
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return ((base * height) / 2);
 }
 
 // Desafio 3
