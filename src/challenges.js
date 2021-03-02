9453768daec017cc131c755b87c85d9bfd1d2230
diogomarin <diogoamarin@gmail.com>
2021-03-02 15:31:42 -0300

    Resolving the lint for the first challenge

--- src/challenges.js
+++ src/challenges.js
@@ -1,12 +1,11 @@
 // Desafio 1
 function compareTrue(value1, value2) {
-  if (value1 === true && value2 === true){
+  if (value1 === true && value2 === true) {
     return true;
-  }
-  if (value1 === false || value2 === false)
+  } if (value1 === false || value2 === false) {
     return false;
+  }
 }
-
 
 // Desafio 2
 function calcArea() {
