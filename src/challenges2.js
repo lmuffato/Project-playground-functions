21fa43ab96f0bb3cd45db52090fa128bcf5bf3b5
Gui-lira <gui.lira.martins@gmail.com>
2021-03-02 19:17:01 -0300

    finalmente o 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,21 +1,24 @@
 // Desafio 10
 function techList(array, name) {
   // esse codigo foi baseado por essa discussão no stack overflow https://stackoverflow.com/questions/5868850/creating-list-of-objects-in-javascript
+  array.sort();
   let list = [];
   if (array.length === 0)
   { 
     return 'Vazio!';
   }
-else {for (let index = 0;index < array.length;index += 1)
-     {list.push({tech: array[index], name: name})
+  else {for (let index = 0;index < array.length;index += 1)
+     {list.push({ tech: array[index], name: name })
     }
   }
   return list
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(list) {
+  let part1 = '';
+  let part2 = '';
+  let part3 = '';
 }
 
 // Desafio 12
