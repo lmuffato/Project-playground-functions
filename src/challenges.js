3e0ed58ccbace7682a9ab312da456058882551c1
Jorge Meyrelles Jr <jotaengpuc@gmail.com>
2021-03-02 18:47:39 -0300

    exercicio 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,15 @@
 // Desafio 1
-function compareTrue() {
+function compareTrue(value1, value2) {
   // seu código aqui.
+  if (value1 == true && value2 == true) {
+
+    return true;
+
+  } else {
+
+    return false;
+    
+  }
 }
 
 // Desafio 2
