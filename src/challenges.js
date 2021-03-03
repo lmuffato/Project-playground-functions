2c0ec0401a35fcfc517d6d8fa48fbf83cb934d14
diegoCorreia <diegomethal@gmail.com>
2021-03-03 18:34:14 -0300

    requisito 4 concatName

--- src/challenges.js
+++ src/challenges.js
@@ -21,9 +21,10 @@ function splitSentence(string) {
 console.log(splitSentence('vamo que vamo'));
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(array) {
+  return `${array[array.length-1]} ${array[0]}`;
 }
+console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
 
 // Desafio 5
 function footballPoints() {
