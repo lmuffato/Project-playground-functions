de7f97af52d659edca383e7cd11eb5db98311b59
defauth98 <neto.daniribeiro@gmail.com>
2021-03-03 11:55:22 -0300

    Fix: Ordenando o array antes de iterar no desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,16 +1,17 @@
 // Desafio 10
 function techList(techListArray, name) {
   const listTechObj = [];
+  const OrdenedTechListArray = techListArray.sort();
 
   if (techListArray.length === 0) {
     return 'Vazio!';
   }
 
-  for (let index in techListArray) {
+  for (let index in OrdenedTechListArray) {
     // Referencia: https://eslint.org/docs/rules/guard-for-in
-    if (Object.prototype.hasOwnProperty.call(techListArray, index)) {
+    if (Object.prototype.hasOwnProperty.call(OrdenedTechListArray, index)) {
       listTechObj.push({
-        tech: techListArray[index],
+        tech: OrdenedTechListArray[index],
         name,
       });
     }
