31f53bcd71ea1e45a5fa6ab29b43732b1a860d2e
Beatriz Estebanez <bia.estebanez@gmail.com>
2021-03-03 09:51:08 -0300

    Desafio 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,8 +1,13 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
-  let number;
+function compareTrue(bool1 , bool2) {
+  if (bool1 === true && bool2 === true) {
+    return true;
+  } else {
+    return false;
+  }
 }
+
+console.log(compareTrue(true,true))
 
 // Desafio 2
 function calcArea() {
