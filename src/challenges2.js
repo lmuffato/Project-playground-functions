3b7eec94c00a444600bc8ee424fd69d92e4d444c
Edrei <edreimateus@gmail.com>
2021-03-03 11:05:05 -0300

    Adiciona requisito 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,19 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(technology, name) {
+  let techlist = [];
+  technology = technology.sort();
+  for (let index = 0; index < technology.length; index += 1) {
+    techlist.push({
+      technology: technology[index],
+      name,
+    });
+  }
+  if (technology.length === 0) {
+    return 'Vazio';
+  }
+  return techlist;
 }
+// Desafio resolvido com ajuda de StackOverFlow
 
 // Desafio 11
 function generatePhoneNumber() {
