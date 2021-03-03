a2db36ff5406834b3cb9abffb150009644ae027e
Lucas André <lucas.cancado89@gmail.com>
2021-03-03 11:15:04 -0300

    Requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -18,8 +18,41 @@ function techList(arr, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(arr) {
+  if (arr.length < 11 || arr.length > 11) {
+    return 'Array com tamanho incorreto.'
+  } 
+  for (let index = 0; index < arr.length; index++) {
+    if (arr[index] > 9) {
+      return 'não é possível gerar um número de telefone com esses valores'
+    }
+    let repeat = 0
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[index] === arr[i]) {
+        repeat = repeat + 1
+      }
+      if (repeat >= 3 || arr[i] < 0) {
+        return 'não é possível gerar um número de telefone com esses valores'
+      }
+    }
+  }
+  let phoneNum = '('
+  for (index2 = 0; index2 <= 13; index2++) {
+    if (index2 < 2) {
+      phoneNum = phoneNum + arr[index2]
+    } else if (index2 === 2) {
+      phoneNum = phoneNum + ')'
+    } else if (index2 === 3) {
+      phoneNum = phoneNum + ' '
+    } else if (index2 > 3 && index2 <= 8) {
+      phoneNum = phoneNum + arr[index2 - 2]
+    } else if (index2 === 9) {
+      phoneNum = phoneNum + '-'
+    } else {
+      phoneNum = phoneNum + arr[index2 - 3]
+    }
+  }
+  return phoneNum
 }
 
 // Desafio 12
--- src/testes.js
+++
@@ -1,8 +0,0 @@
-var r = /\d+/g;
-var s = "you can enter 333 maximum 500 choices";
-var m;
-while ((m = r.exec(s)) != null) {
-  alert(m[0]);
-}
-
-console.log(m)
