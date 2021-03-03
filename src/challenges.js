64aead9abef054cbb0023ca78d4f7887e919c2b3
André Dantas <aluizcfd@gmail.com>
2021-03-03 15:08:00 -0300

    Adição desafio 9

--- src/challenges.js
+++ src/challenges.js
@@ -87,11 +87,15 @@ function fizzBuzz(arrayOfNumbers) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(string) {
+  let newString = string.replace(/a/g, '1').replace(/e/g, '2').replace(/i/g, '3').replace(/o/g, '4').replace(/u/g, '5');
+
+  return newString;
 }
-function decode() {
-  // seu código aqui
+function decode(string) {
+  let newString = string.replace(/1/g, 'a').replace(/2/g, 'e').replace(/3/g, 'i').replace(/4/g, 'o').replace(/5/g, 'u');
+
+  return newString;
 }
 
 module.exports = {
