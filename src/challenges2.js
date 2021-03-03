dd74c89da01669d6acbd432a97a41ec608cecdb6
Jorge Meyrelles Jr <jotaengpuc@gmail.com>
2021-03-03 18:19:45 -0300

    exercicio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -16,8 +16,33 @@ function techList(tech, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(telNumber) {
   // seu código aqui
+  let repete;
+  let ddd;
+  let prim;
+  let seg;
+  if (telNumber.length !== 11) {
+    console.log('Array com tamanho incorreto.');
+  }
+  for (let i = 0; i < telNumber.length; i += 1) {
+    if (telNumber[i] > 9 || telNumber[i] < 0) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+    repete = 0;
+    for (let j = 0; j < telNumber.length; j += 1) {
+      if (telNumber[i] === telNumber[j]) {
+        repete += 1;
+      }
+    }
+    if (repete >= 3) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  ddd = `(${telNumber[0]}${telNumber[1]})`;
+  prim = `${telNumber[2]}${telNumber[3]}${telNumber[4]}${telNumber[5]}${telNumber[6]}-`;
+  seg = `${telNumber[7]}${telNumber[8]}${telNumber[9]}${telNumber[10]}`;
+  return `${ddd}${prim}${seg}`;
 }
 
 // Desafio 12
