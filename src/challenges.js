ef70904506205c1c0a47484022741ff2c5f93bb7
André Dantas <aluizcfd@gmail.com>
2021-03-03 15:19:38 -0300

    Refatoração desafio 9

--- src/challenges.js
+++ src/challenges.js
@@ -88,12 +88,20 @@ function fizzBuzz(arrayOfNumbers) {
 
 // Desafio 9
 function encode(string) {
-  let newString = string.replace(/a/g, '1').replace(/e/g, '2').replace(/i/g, '3').replace(/o/g, '4').replace(/u/g, '5');
+  let newString = string.replace(/a/g, '1')
+    .replace(/e/g, '2')
+    .replace(/i/g, '3')
+    .replace(/o/g, '4')
+    .replace(/u/g, '5');
 
   return newString;
 }
 function decode(string) {
-  let newString = string.replace(/1/g, 'a').replace(/2/g, 'e').replace(/3/g, 'i').replace(/4/g, 'o').replace(/5/g, 'u');
+  let newString = string.replace(/1/g, 'a')
+    .replace(/2/g, 'e')
+    .replace(/3/g, 'i')
+    .replace(/4/g, 'o')
+    .replace(/5/g, 'u');
 
   return newString;
 }
