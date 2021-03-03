2418c7c178da45fec581e5f233b427e82b71774b
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-03-02 21:34:14 -0300

    exercicio 13

--- src/challenges2.js
+++ src/challenges2.js
@@ -10,16 +10,29 @@ function generatePhoneNumber() {
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
-  if (lineA < lineB + lineC  && lineA > Math.abs(lineB - lineC) ) {
+  if (lineA < lineB + lineC && lineA > Math.abs(lineB - lineC) ) {
     return true;
-  } else if (lineA > lineB + lineC) {
+  } else if (lineA > lineB + lineC || lineA < Math.abs(lineB - lineC)) {
     return false; 
-  } else if (lineA < Math.abs(lineB - lineC)) {
-  } return false;
+  } 
 }
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(string) {
+  let string = string.split('');
+  let alcool = 0;
+  let agua = 0;
+
+  for (let index = 0; index < string.length; index += 1){
+    if ( string[index] === Number){
+      alcool += 1;
+    }
+  }
+
+  if ( acool > 0){
+    agua +=1;
+  }
+
+  return console.log(agua) + ' copos de água';
 }
 
 module.exports = {
