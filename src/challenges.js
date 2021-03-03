a9a90e1c8ac57d38f9cfd2af1bcf4cf0243f38e8
madruga665 <madruga665@hotmail.com>
2021-03-02 22:25:54 -0300

    Desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -42,14 +42,16 @@ function highestCount(array) {
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  let distance1 = (cat1 - mouse);
-  let distance2 = (cat2 - mouse);
+  let distance1 = Math.abs(cat1 - mouse);
+  let distance2 = Math.abs(cat2 - mouse);
 
   if (distance1 < distance2) {
     return 'cat1';
-  } if (distance1 > distance2) {
+  }
+  if (distance2 < distance1) {
     return 'cat2';
-  } if (distance1 === distance2) {
+  }
+  if (distance1 === distance2) {
     return 'os gatos trombam e o rato foge';
   }
 }
