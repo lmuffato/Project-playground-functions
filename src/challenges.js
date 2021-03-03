ccc14e3d30b9ccec423cba50d391c86211343cfa
guilhermemd <guilherme.md@gmail.com>
2021-03-02 22:33:02 -0300

    desafio 8 - fizzBuzz

--- src/challenges.js
+++ src/challenges.js
@@ -56,8 +56,21 @@ function catAndMouse(cat1, cat2, mouse) {
     return 'os gatos trombam e o rato foge';
   }
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(numbers) {
+  let getFizzBuzz = [];
+  for (let index in numbers) {
+    if (numbers[index] % 3 === 0){
+      getFizzBuzz.push('fizz');
+    } 
+    else if (numbers[index] % 5 === 0) {
+      getFizzBuzz.push('buzz');
+    } else if (numbers[index] % 3 === 0 && numbers[index] % 5 === 0) {
+      getFizzBuzz.push('fizzBuzz');
+    } else {
+      getFizzBuzz.push('bug!');
+    }
+  }
+  return getFizzBuzz;
 }
 
 // Desafio 9
