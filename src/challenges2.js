e26cfbce850ec9dd7d64620024ac5aae6a315271
AndreBarroso <andre_barroso17@hotmail.com>
2021-03-02 22:32:05 -0300

    Requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,11 +1,54 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(array,name) {
+  let retorno = [];
+  let objeto = {
+    tech: '',
+    name: name
+  };
+  for(let index in array){
+    objeto['tech'] = array[index];
+    retorno[index] = objeto;
+  }
+  return retorno;
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  let i;
+  let count = 0;
+  let number = '(';
+  if(array.length!==11){
+    return 'Array com tamanho incorreto.'
+  }
+  for(let i=0; i<array.length; i+=1){
+    if(array[i]>9 || array[i]<0){
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+    count=0;
+    for(let j=1; j<array.length; j+=1){
+      if(array[j]===array[i]){
+        count += 1;
+        if(count>=3){
+          return 'não é possível gerar um número de telefone com esses valores';
+        }
+      }
+    }
+
+  }
+  var k;
+  for(let k=0; k<array.length; k+=1){
+
+    if (k===2){
+      number += ') ';
+    }
+    else if (k===7){
+      number += '-';
+    }
+
+    number += array[k];
+
+  }
+  return number;
 }
 
 // Desafio 12
@@ -24,3 +67,5 @@ module.exports = {
   hydrate,
   triangleCheck,
 };
+//console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"],"Lucas"));
+console.log(generatePhoneNumber([3,1,9,8,5,3,0,1,9,5,8]));
