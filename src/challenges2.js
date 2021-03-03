34e5e51d59c998e67d9fc77bbe605d9dae44a69e
Guilherme de Prais <guilherme.prais@gmail.com>
2021-03-03 13:36:13 -0300

    apaga console.log Desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -60,8 +60,6 @@ function generatePhoneNumber(phone) {
   return `(${phone[0]}${phone[1]}) ${phone[2]}${phone[3]}${phone[4]}${phone[5]}${phone[6]}-${phone[7]}${phone[8]}${phone[9]}${phone[10]}`;
 }
 
-console.log(generatePhoneNumber([1, 2, 2, 4, 5, 5, 2, 8, 9, 0, 1]));
-
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
   let trueFalse = true;
