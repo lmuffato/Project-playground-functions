e3b4fd7733b3842ec9cc28c39fcc4c5cb39a681b
pmaiamateus <pmaiamateus@gmail.com>
2021-03-03 08:49:48 -0300

    adiciona o desafio 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,9 @@
 // Desafio 1
-function compareTrue() {
-  a
+function compareTrue(value1, value2) {
+  if (value1 === true && value2 === true)
+    return true
+  else
+    return false
 }
 
 // Desafio 2
