dbdb88ceadbadd06f5f0e85bf633128d344dbb75
Felipe Flores <felipefloresvendas@gmail.com>
2021-03-03 10:46:11 -0300

    correção desafio 6(5) e correção  desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -38,6 +38,10 @@ function arrayFirstBigger(list) {
   for (let index = 0; index < list.length - 1; index += 1) {
     if (list[index] > list[index + 1]) {
       arrayList.push(list[index]);
+    } else if (list[index] < list[index + 1]) {
+      arrayList.push(list[index + 1]);
+    } else {
+      arrayList.push(list[index]);
     }
   }
   return arrayList;
@@ -47,8 +51,8 @@ function arrayFirstBigger(list) {
 function highestCount(list) {
   let biggestNumber = 0;
   let timesRepeat = 0;
-  list = arrayFirstBigger(list);
-  biggestNumber += list[0];
+  let array = arrayFirstBigger(list);
+  biggestNumber += array[0];
   for (let index = 0; index < list.length; index += 1) {
     if (biggestNumber === list[index]) {
       timesRepeat += 1;
@@ -59,20 +63,33 @@ function highestCount(list) {
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  let catToCat1 = cat1 - mouse;
-  let catToCat2 = cat2 - mouse;
+  let cat1ToMouse = cat1 - mouse;
+  let cat2Tomouse = cat2 - cat1;
+  Math.abs(cat1ToMouse);
+  Math.abs(cat2Tomouse);
   if (cat1 === cat2) {
     return 'os gatos trombam e o rato foge';
-  } else if (cat2 < cat1 || catToCat1 > catToCat2) {
+  } else if (cat2Tomouse < cat1ToMouse) {
     return 'cat2';
   } else {
     return 'cat1';
   }
 }
 
+let list = [2, 15, 7, 9, 45];
+
+function arrayRunner(array) {
+  let result = [];
+  for (let index in array) {
+    result.push(array[index]);
+  }
+  return result;
+}
+
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(array) {
+  let divBy = [3, 5];
+
 }
 
 // Desafio 9
