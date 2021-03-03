20610f39a06211e459d930116093da6aa7c50a4f
Rodrigo Queiroz <rodrigoandradequeiroz@gmail.com>
2021-03-02 22:23:51 -0300

    corrigindo lint pt1

--- src/challenges.js
+++ src/challenges.js
@@ -1,33 +1,35 @@
 // Desafio 1
 function compareTrue(value1, value2) {
-  if (value1 == true && value2 == true){
-    return true
+  let compare;
+  if (value1 === true && value2 === true) {
+    compare = true;
   } else {
-    return false
+    compare = false;
   }
+  return compare
 }
 
 // Desafio 2
 function calcArea(base, height) {
-  area = (base*height)/2
-  return area
+  let area = (base*height)/2;
+  return area;
 }
 
 // Desafio 3
 function splitSentence(string) {
-  return string.split(" ")
+  return string.split(" ");
 }
 
 // Desafio 4
 function concatName(array) {
-  let firstName = array[0]
-  let lastName = array[array.length-1]
-  return lastName + ", " + firstName
+  let firstName = array[0];
+  let lastName = array[array.length-1];
+  return lastName + ', ' + firstName;
 }
 
 // Desafio 5
 function footballPoints(wins, ties) {
-  return (3*wins) + ties
+  return (3 * wins) + ties;
 }
 
 // Desafio 6
@@ -35,88 +37,88 @@ function highestCount(array) {
   let countFrequency = 0;
   let highestNumber = array[0];
 
-  for (let index = 0; index < array.length; index +=1) {
+  for (let index = 0; index < array.length; index += 1) {
     if (array[index] > highestNumber) {
-      highestNumber = array[index]
+      highestNumber = array[index];
     }
   }
   for (let index = 0; index < array.length; index += 1) {
-    if (array[index] == highestNumber) {
-      countFrequency += 1
+    if (array[index] === highestNumber) {
+      countFrequency += 1;
     }
   }
 
-  return countFrequency
+  return countFrequency;
 }
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  let whoCatch = ''
+  let whoCatch = '';
   if ((Math.abs(mouse - cat1) > Math.abs(mouse - cat2))) {
-    whoCatch = 'cat2'
+    whoCatch = 'cat2';
   } else if ((Math.abs(mouse - cat2) > Math.abs(mouse - cat1))) {
-    whoCatch = 'cat1'
+    whoCatch = 'cat1';
   } else {
-    whoCatch = 'os gatos trombam e o rato foge'
+    whoCatch = 'os gatos trombam e o rato foge';
 
   }
-  return whoCatch
+  return whoCatch;
 }
 
 // Desafio 8
 function fizzBuzz(array) {
-  let fizzOrBuzzOrBug = []
-  for (let index = 0; index < array.length; index +=1 ) {
+  let fizzOrBuzzOrBug = [];
+  for (let index = 0; index < array.length; index += 1 ) {
     if (array[index] % 3 == 0 && array[index] % 5 == 0) {
-      fizzOrBuzzOrBug.push('fizzBuzz')
-    } else if (array[index] % 3 == 0 && array[index] % 5 != 0){
-        fizzOrBuzzOrBug.push('fizz')
-    } else if (array[index] % 5 == 0 && array[index] % 3 != 0) {
-       fizzOrBuzzOrBug.push('buzz')
-    } else {
-       fizzOrBuzzOrBug.push('bug!')
+      fizzOrBuzzOrBug.push('fizzBuzz');
+  } else if (array[index] % 3 == 0 && array[index] % 5 !== 0) {
+      fizzOrBuzzOrBug.push('fizz');
+  } else if (array[index] % 5 == 0 && array[index] % 3 !== 0) {
+      fizzOrBuzzOrBug.push('buzz');
+  } else {
+      fizzOrBuzzOrBug.push('bug!');
     }
   }
-  return fizzOrBuzzOrBug
+  return fizzOrBuzzOrBug;
 }
 
 // Desafio 9
 function encode(string) {
   let encoded = ''
-  for (index = 0; index < string.length; index +=1) {
-    if (string[index] == 'a') {
-      encoded += '1'
-    } else if (string[index] == 'e') {
-        encoded += '2'
-    } else if (string[index] == 'i') {
-        encoded += '3'
-    } else if (string[index] == 'o') {
-        encoded += '4'
-    } else if (string[index] == 'u') {
-        encoded += '5'
-    } else {
-        encoded += string[index]
+  for (let index = 0; index < string.length; index += 1) {
+    if (string[index] === 'a') {
+      encoded += '1';
+  } else if (string[index] === 'e') {
+      encoded += '2';
+  } else if (string[index] === 'i') {
+      encoded += '3';
+  } else if (string[index] === 'o') {
+      encoded += '4';
+  } else if (string[index] === 'u') {
+      encoded += '5';
+  } else {
+      encoded += string[index]
     }
   }
-  return encoded
+  return encoded;
 }
 function decode(string) {
-  let decoded = ''
-  for (index = 0; index < string.length; index +=1) {
-    if (string[index] == '1') {
-      decoded += 'a'
-    } else if (string[index] == '2') {
-        decoded += 'e'
-    } else if (string[index] == '3') {
-        decoded += 'i'
-    } else if (string[index] == '4') {
-        decoded += 'o'
-    } else if (string[index] == '5') {
-        decoded += 'u'
-    } else {
-        decoded += string[index]
-    }
+  let decoded = '';
+  for (let index = 0; index < string.length; index += 1) {
+    if (string[index] === '1') {
+      decoded += 'a';
+  } else if (string[index] === '2') {
+      decoded += 'e';
+  } else if (string[index] === '3') {
+      decoded += 'i';
+  } else if (string[index] === '4') {
+      decoded += 'o';
+  } else if (string[index] === '5') {
+      decoded += 'u';
+  } else {
+      decoded += string[index];
   }
+}
   return decoded
 }
 
@@ -132,9 +134,3 @@ module.exports = {
   highestCount,
   splitSentence,
 };
-
-
-
-
-
-
--- src/challenges2.js
+++ src/challenges2.js
@@ -1,92 +1,85 @@
 // Desafio 10
 function techList(techArray, name) {
-  techArray.sort()
-  let arrayOfObjects = []
-  if (techArray.length == 0) {
-    arrayOfObjects = 'Vazio!'
+  techArray.sort();
+  let arrayOfObjects = [];
+  if (techArray.length === 0) {
+    arrayOfObjects = 'Vazio!';
   }
-  for (let index = 0; index < techArray.length; index += 1) {
-    arrayOfObjects.push({tech: techArray[index], name: name})
+  for (let index = 0; index < techArray.length; index += 1) { 
+    arrayOfObjects.push( {tech: techArray[index], name: name} )
   }
-  return arrayOfObjects
+  return arrayOfObjects;
 }
 
 // Desafio 11
 function generatePhoneNumber(array) {
-
   let countMax = 1;
   let countElement = 0;
   let phoneNumber = '';
   let firstPart = '';
   let secondPart = '';
   let thirdPart = '';
-
-  if (array.length != 11) {
+  if (array.length !== 11) {
     return 'Array com tamanho incorreto.'
   }
-
   for (let index = 0; index < array.length; index += 1) {
     if (array[index] < 0 || array[index] > 9) {
-      return 'não é possível gerar um número de telefone com esses valores'
+      return 'não é possível gerar um número de telefone com esses valores';
     }
-    for (indexRepeated = index; indexRepeated < array.length+1; indexRepeated += 1) {
+    for (let indexRepeated = index; indexRepeated < array.length+1; indexRepeated += 1) {
       if (countElement > 2) {
-        return 'não é possível gerar um número de telefone com esses valores'
-      } else if (array[index] == array[indexRepeated]) {
-        countElement += 1
-      } else if (countElement > countMax) {
-        countMax = countElement
+        return 'não é possível gerar um número de telefone com esses valores';
+    } else if (array[index] === array[indexRepeated]) {
+        countElement += 1;
+    } else if (countElement > countMax) {
+        countMax = countElement;
       } 
     }
-
-    countElement = 0
-    phoneNumber += array[index]  
+    countElement = 0;
+    phoneNumber += array[index];
   }
-
-  // pesquisei a ideia do contado no site www.stackoverflow.com, segue o link: https://stackoverflow.com/questions/31227687/find-the-most-frequent-item-of-an-array-not-just-strings/31227768
-  
+  // pesquisei a ideia do contado no site www.stackoverflow.com, segue o link: https://stackoverflow.com/questions/31227687/find-the-most-frequent-item-of-an-array-not-just-strings/31227768 
   for (let index = 0; index < phoneNumber.length; index += 1) {
     if (index < 2) {
-      firstPart += phoneNumber[index]
+      firstPart += phoneNumber[index];
     } else if (index < 7) {
-      secondPart += phoneNumber[index]
+      secondPart += phoneNumber[index];
     } else {
-      thirdPart += phoneNumber[index]
+      thirdPart += phoneNumber[index];
     }
   }
-  
   phoneNumber = '(' + firstPart + ') ' + secondPart + '-' + thirdPart
-
-  return phoneNumber
-  
+  return phoneNumber;  
 }
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
-  //Um lado tem que ser menor do que a soma dos outros dois e maior do que o valor absoluto da diferença dos dois (math.abs)
-  //retorno deve ser booleano
+  let triangleChecked;
   if (lineA < lineB + lineC && lineA > Math.abs(lineB-lineC) || lineB < lineA + lineC && lineB > Math.abs(lineA - lineC) || lineC < lineA+lineB && lineC > Math.abs(lineA - lineB)) {
-    return true
+    triangleChecked = true;
   } else {
-    return false
+    triangleChecked = false;
   }
-
+  return triangleChecked
 }
 
 // Desafio 13
 function hydrate(string) {
+
+  //pesquisei a ideia de retirar os numbers da string no stackoverflor. Segue o link: https://stackoverflow.com/questions/30607419/return-only-numbers-from-string
   let quantityOfDrinks = string.replace(/\D/g, "")
-
-  let sum = 0
+  let sum = 0;
   for (let index = 0; index < quantityOfDrinks.length; index += 1) {
-    sum += parseInt(quantityOfDrinks[index])
+    sum += parseInt(quantityOfDrinks[index]);
   }
 
+  let cupOfWater 
   if (sum < 2) {
-    return sum + ' copo de água'
+    cupOfWater = sum + ' copo de água';
   } else {
-    return sum + ' copos de água'
+    cupOfWater = sum + ' copos de água';
   }
+  return cupOfWater
 }
 
 module.exports = {
