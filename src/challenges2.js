1a85a6e84d1366ae74aa0b00d4914eebdc4296eb
Jorge Meyrelles Jr <jotaengpuc@gmail.com>
2021-03-03 14:27:59 -0300

    exercicio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -4,20 +4,40 @@ function techList(tech, name) {
   let techListObject = {};
   let retornoObject = [];
 
-  if (tech === null) {
+  if (tech.length === 0) {
     return 'Vazio!';
   }
   for (let i = 0; i < tech.length; i += 1) {
     techListObject.tech = tech[i];
     techListObject.name = name;
-    retornoObject[i] = `tech: ${techListObject.tech}, name: ${techListObject.name}`;
+    retornoObject.push({tech: [tech[i]], name});
   }
   return retornoObject;
 }
 
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(telNumber) {
   // seu código aqui
+  let j = 0;
+  let repete = 0;
+  for (let i = 0; i < telNumber.length; i += 1) {
+    if (telNumber.length > 11) {
+    return 'Array com tamanho incorreto.'
+    }
+    for (let k = 0; k < telNumber.length; k += 1) {
+      repete = 0;
+      if (telNumber[i] === telNumber[k]) {
+        repete += 1;
+      }
+      if (repete > 3 || telNumber[i] < 0 || telNumber[i] > 9) {
+        return 'não é possível gerar um número de telefone com esses valores';
+      }
+      for (let j = 0; j < telNumber.length; j += 1){
+        ddd1 = telNumber[0];
+        telNumber[0] = '(';
+        telNumber[]
+      }
+    }
 }
 
 // Desafio 12
