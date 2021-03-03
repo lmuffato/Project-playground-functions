d784691bf8ae7ec1a81ad769eff728b1d9407a63
Gustavo Sousa <gustavohl98@gmail.com>
2021-03-03 13:33:13 -0300

    Requisito 09 - encode-decode

--- src/challenges.js
+++ src/challenges.js
@@ -111,12 +111,52 @@ function fizzBuzz(numbersArr) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(str) {
+  const encodedArr = [];
+  const encodingObj = {
+    a: 1,
+    e: 2,
+    i: 3,
+    o: 4,
+    u: 5,
+  };
+
+  for (let index = 0; index < str.length; index += 1) {
+    if (encodingObj[str[index]]) {
+      encodedArr.push(encodingObj[str[index]]);
+    } else {
+      encodedArr.push(str[index]);
+    }
+  }
+
+  return encodedArr.join('');
 }
-function decode() {
-  // seu código aqui
+function decode(str) {
+  const decodedArr = [];
+  const decodingObj = {
+    1: 'a',
+    2: 'e',
+    3: 'i',
+    4: 'o',
+    5: 'u',
+  };
+
+  for (let index = 0; index < str.length; index += 1) {
+    if (decodingObj[str[index]]) {
+      decodedArr.push(decodingObj[str[index]]);
+    } else {
+      decodedArr.push(str[index]);
+    }
+  }
+
+  return decodedArr.join('');
 }
+
+console.log(encode('hi there!'));
+console.log(encode('Gustavo Sousa'));
+
+console.log(decode('h3 th2r2!'));
+console.log(decode('G5st1v4 S45s1'));
 
 module.exports = {
   calcArea,
