f89b0374a5e29b386c67d1b2e8c0dd00a5d5201a
Iago de Paula <iago.pferreiravr@gmail.com>
2021-03-03 11:37:25 -0300

    Décimo requisito

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,19 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(techs, name) {
+  let devTechnology = {tech: '', name: ''};
+  let devTechs = []
+  let devTechnologyCopy = '';
+  for (let index = 0; index < techs.length; index += 1) {
+    devTechnology.tech = techs[index];
+    devTechnology.name = name;
+    devTechnologyCopy = devTechnology.assign({}, de)
+    devTechs.push(devTechnology.slice());
+    // console.log(devTechs)
+  }
+  return devTechs;
 }
+
+console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], 'Iago'));
 
 // Desafio 11
 function generatePhoneNumber() {
