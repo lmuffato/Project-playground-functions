0e99967436591aba1951e10db8f3357305fccbed
Jodiel Briesemeister <jodielll29@gmail.com>
2021-03-02 21:06:09 -0300

    corrigido alguns erros do linter

--- src/challenges.js
+++ src/challenges.js
@@ -2,23 +2,22 @@
 function compareTrue(parameter1, parameter2) {
   // seu código aqui
   let compara;
-  if (parameter1 && parameter2 === true){
+  if (parameter1 && parameter2 === true) {
     compara = true;
-  }
-  else {
+  } else {
     compara = false;
   }
   return compara;
 }
 
-console.log(compareTrue(true, true))
+console.log(compareTrue(true, true));
 
 // Desafio 2
 function calcArea(base, height) {
   // seu código aqui
   let calculo;
-  calculo = (base * height) / 2
-  return calculo
+  calculo = (base * height) / 2;
+  return calculo;
 }
 
 console.log(calcArea(51, 1));
@@ -26,85 +25,86 @@ console.log(calcArea(51, 1));
 // Desafio 3
 function splitSentence(str) {
   // seu código aqui
-  let separa = str.split(" ");
-  return separa
+  let separa = str.split(' ');
+  return separa;
 }
 
-console.log(splitSentence("foguete"))
+console.log(splitSentence('foguete'));
 
 // Desafio 4
 function concatName(list) {
   // seu código aqui
   let lastName;
   let firstName;
-  let concat;
-  for(let index = 0; index < list.length; index += 1){
-    if (index === list.length - 1){
+  for (let index = 0; index < list.length; index += 1) {
+    if (index === list.length - 1) {
       lastName = list[index];
-    }
-    else if (index === 0){
+    } else if (index === 0) {
       firstName = list[index];
     }
   }
-  concat = lastName + ", " + firstName
-  return concat
+  return `${lastName}, ${firstName}`;
 }
 
-let arrayNames = ['captain', 'my', 'captain']
+let arrayNames = ['captain', 'my', 'captain'];
 
 console.log(concatName(arrayNames));
 
 // Desafio 5
 function footballPoints(wins, ties) {
   // seu código aqui
-  let calc = (wins * 3) + (ties * 1)
-  return calc
+  let calc = (wins * 3) + (ties * 1);
+  return calc;
 }
 
-let win = 0;
-let tie = 0;
+let win = 5;
+let tie = 3;
 
-console.log(footballPoints(win, tie))
+console.log(footballPoints(win, tie));
 
 // Desafio 6
-function highestCount(number) {
-  // seu código aqui
-  let higher = number[0]
-  let repet = 0;
-  for (let index = 0; index < number.length; index += 1){
-    if (higher < number[index]){
-      higher = number [index];
+
+function higherNumber(number) {
+  let higher = number[0];
+  for (let index = 0; index < number.length; index += 1) {
+    if (higher < number[index]) {
+      higher = number[index];
     }
   }
-  for (let index = 0; index < number.length; index += 1){
-    if (number[index] === higher){
-      repet += 1;
-    }
-  }
-  return repet
+  return higher;
 }
 
-let arrayNumbers = [0, 0, 0]
+function highestCount(numbers) {
+  // seu código aqui
+  let higher = higherNumber(numbers);
+  let repeat = 0;
+  for (let index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] === higher) {
+      repeat += 1;
+    }
+  }
+  return repeat;
+}
 
-console.log(highestCount(arrayNumbers))
+let arrayNumbers = [9, 11, 9, 11, 11, 10];
+
+console.log(highestCount(arrayNumbers));
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
   // seu código aqui
   let winner;
-  if ((cat2 - mouse) < (cat1 - mouse)){
+  if ((cat2 - mouse) < (cat1 - mouse)) {
     winner = 'cat2';
-  }
-  else if ((cat2 - mouse) > (cat1 - mouse)){
+  } else if ((cat2 - mouse) > (cat1 - mouse)) {
     winner = 'cat1';
-  }
-  else if ((cat2 - mouse) === (cat1 - mouse)){
+  } else if ((cat2 - mouse) === (cat1 - mouse)) {
     winner = 'os gatos trombam e o rato foge';
   }
-  return winner
+  return winner;
 }
 
-let mouse = 0; 
+let mouse = 0;
 let cat1 = 12;
 let cat2 = 12;
 
@@ -114,25 +114,28 @@ console.log(catAndMouse(mouse, cat1, cat2));
 function fizzBuzz(numbers) {
   // seu código aqui
   let result = [];
-  for (let index = 0; index < numbers.length; index += 1){
-    if (numbers[index] % 3 === 0 && numbers[index] % 5 === 0){
-      result[index] = "fizzBuzz";
-    }
-    else if (numbers[index] % 3 != 0 && numbers[index] % 5 != 0){
-      result[index] = "bug!";
-    }
-    else if (numbers[index] % 3 === 0){
-      result[index] = "fizz";
-    }
-    else if (numbers[index] % 5 === 0){
-      result[index] = "buzz";
-    }
+  if (numbers % 3 === 0 && numbers % 5 === 0) {
+    result.push('fizzBuzz');
+  } else if (numbers % 3 === 0) {
+    result.push('fizz');
+  } else if (numbers % 5 === 0) {
+    result.push('buzz');
+  } else {
+    result.push('bug!');
   }
-  return result
+  return result;
+}
+
+function montar(test) {
+  let result = [];
+  for (let index = 0; index < test.length; index += 1) {
+    result[index] = fizzBuzz((test[index]));
+  }
+  return result;
 }
 
 let arrayNumbers2 = [7, 9];
-console.log(fizzBuzz(arrayNumbers2))
+console.log(montar(arrayNumbers2));
 
 // Desafio 9
 function encode(param) {
@@ -144,13 +147,12 @@ function encode(param) {
     e: '2',
     i: '3',
     o: '4',
-    u: '5'
+    u: '5',
   };
-  
-  result = param.replace(/a|e|i|o|u/g, function(param){
-    return vowels[param]
+  let result = param.replace(/a|e|i|o|u/g, function (param2) {
+    return vowels[param2];
   });
-  return result
+  return result;
 }
 function decode(param) {
   // seu código aqui
@@ -161,17 +163,17 @@ function decode(param) {
     2: 'e',
     3: 'i',
     4: 'o',
-    5: 'u'
+    5: 'u',
   };
-  
-  result = param.replace(/1|2|3|4|5/g, function(param){
-    return numbers[param]
+
+  let result = param.replace(/1|2|3|4|5/g, function (param2) {
+    return numbers[param2];
   });
-  return result
+  return result;
 }
 
-console.log("Encode: " + encode("Hi there"))
-console.log("Decode: " + decode("H3 th2r2"))
+console.log(`Encode: ${encode('Hi there')}`);
+console.log(`Decode: ${decode('H3 th2r2')}`);
 
 module.exports = {
   calcArea,
--- src/challenges2.js
+++ src/challenges2.js
@@ -1,30 +1,28 @@
 // Desafio 10
-function techList(tech, name) {
+function techList(tech, name1) {
   // seu código aqui
   // links estudados para a realização do código: https://www.w3schools.com/jsref/jsref_sort.asp
   let techSort = tech.sort();
   let technologies = [];
   let technologiesResult;
-  
-  if (tech.length >= 1){
-    for (let index = 0; index < tech.length; index += 1){
+  if (tech.length >= 1) {
+    for (let index = 0; index < tech.length; index += 1) {
       technologies[index] = {
         tech: techSort[index],
-        name: name
-      }
+        name: name1,
+      };
     }
     technologiesResult = technologies;
-  }
-  else {
-    technologiesResult = "Vazio!"
+  } else {
+    technologiesResult = 'Vazio!';
   }
   return technologiesResult;
 }
 
-let arrayTech = ["React", "Jest", "HTML", "CSS", "JavaScript"];
-let myName = "Jodiel";
+let arrayTech = ['React', 'Jest', 'HTML', 'CSS', 'JavaScript'];
+let myName = 'Jodiel';
 
-console.log(techList(arrayTech, myName))
+console.log(techList(arrayTech, myName));
 
 // Desafio 11
 function generatePhoneNumber() {
