9bdeebb19d46f1a032070ae656e58b4a96c65259
Viegas <mvrdgs@gmail.com>
2021-03-02 22:09:31 -0300

    desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -20,8 +20,38 @@ function techList(array, string) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function verifyNumbers(number) {
+  if (number <= 9 && number >= 0) {
+    return number;
+  }
+}
+
+function formatNumber(string) {
+  let formatedNumber = '(';
+  formatedNumber += string.slice(0, 2);
+  formatedNumber += ') ';
+  formatedNumber += string.slice(2, 7);
+  formatedNumber += '-';
+  formatedNumber += string.slice(7, 11);
+
+  return formatedNumber;
+}
+
+function generatePhoneNumber(array) {
+  if (array.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  let phoneNumber = '';
+
+  for (let index = 0; index < array.length; index += 1) {
+    let number = verifyNumbers(array[index]);
+    if (number === undefined) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+    phoneNumber += number;
+  }
+
+  return formatNumber(phoneNumber);
 }
 
 // Desafio 12
