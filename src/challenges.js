dafbc076e0f98a487c2714063cf1ab0fd0d2872e
Anibal <anibalneto29@gmail.com>
2021-03-02 21:29:32 -0300

    mudança de comandos desafio 4

--- src/challenges.js
+++ src/challenges.js
@@ -20,7 +20,7 @@ function splitSentence(sentenceToSplit) {
 
 // Desafio 4
 function concatName(stringsArray) {
-  return stringsArray.concat(stringsArray.length[-1], ', ', stringsArray.length[0]);
+  return stringsArray.concat(stringsArray.length[-1] + ', ' + stringsArray.length[0]);
 }
 // Source: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/concat //
 
