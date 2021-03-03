d4b110307a89fb874ae811fbd0e23c1d41d8f0b4
Mateus Martins - Turma 10 - Tribo A <mateusmartinsmck@gmail.com>
2021-03-02 23:16:01 -0300

    Requisito 10 corrigido.2 + Requisito 11 concluido

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,8 +1,8 @@
 // Desafio 10
 function techList(arrayTechNames, name) {
+  let exitArray = [];
   if (arrayTechNames.length > 0) {
     let arraySorted = arrayTechNames.sort();
-    let exitArray = [];
     for (let index in arraySorted) {
       let insertObj = {};
       insertObj.tech = arraySorted[index];
@@ -16,9 +16,34 @@ function techList(arrayTechNames, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function repeatNumbers(array) {
+  for (let index = 0; index < array.length; index += 1) {
+    let count = 0;
+    for (let index2 = 0; index2 < array.length; index2 +=1) {
+      if (array[index] === array[index2]) {
+        count+=1;
+        if (count > 3){
+          return true;
+        }
+      }
+    }
+  }
+  return false;
 }
+
+function generatePhoneNumber(arrayPhoneNumbers) {
+  if (arrayPhoneNumbers.length != 11){
+    return 'Array com tamanho incorreto.'
+  }
+  for (let index = 0; index < arrayPhoneNumbers.length; index += 1) {
+    if (arrayPhoneNumbers[index] < 0 || arrayPhoneNumbers[index] > 9 || repeatNumbers(arrayPhoneNumbers)) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  let phone = arrayPhoneNumbers;
+  return ('(' + phone[0] + '' + phone[1] + ') ' + phone[2] + '' + phone[3] + '' + phone[4] + '' + phone[5] + '' + phone[6] + '-' + phone[7] + '' + phone[8] + '' + phone[9] + '' + phone[10]);
+}
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]))
 
 // Desafio 12
 function triangleCheck() {
