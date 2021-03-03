1b3efa8473901ca2ac46cea83897e9a8035b32e8
Iago de Paula <iago.pferreiravr@gmail.com>
2021-03-03 12:15:53 -0300

    Correção do décimo requisito

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,19 +1,21 @@
 // Desafio 10
 function techList(techs, name) {
-  let devTechnology = {tech: techs, name: name};
-  let devTechs = []
-  // let devTechnologyCopy = '';
-  for (let index = 0; index < techs.length; index += 1) {
-    devTechnology.tech = techs[index];
-    devTechnology.name = name;
-    let devTechnologyCopy = Object.assign({}, devTechnology);
-    devTechs.push(devTechnologyCopy);
-    // console.log(devTechs)
+  let devTechnology = { tech: techs, name: name };
+  let devTechs = [];
+  let empty = 'Vazio'
+  for (let index = 0; index <= techs.length; index += 1) {
+    if (devTechnology.tech.length === 0) {
+      devTechs.push('Vazio!');
+    } else {
+      devTechnology.tech = techs[index];
+      devTechnology.name = name;
+      let devTechnologyCopy = Object.assign({}, devTechnology);
+      devTechs.push(devTechnologyCopy);
+    }
   }
   return devTechs;
+  
 }
-
-console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Iago'));
 
 // Desafio 11
 function generatePhoneNumber() {
