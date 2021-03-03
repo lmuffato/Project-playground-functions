ef342490d21ee6978e01517f1c9176780fd5d0fd
Felipe Flores <felipefloresvendas@gmail.com>
2021-03-03 09:27:45 -0300

    correção desafio 6(4) e realizado desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -45,46 +45,31 @@ function arrayFirstBigger(list) {
 
 // Desafio 6
 function highestCount(list) {
-  let array = [0, 4, 4, 4, 9, 2, 1];
   let biggestNumber = 0;
   let timesRepeat = 0;
-  list = arrayFirstBigger(array);
+  list = arrayFirstBigger(list);
   biggestNumber += list[0];
-  for (let index = 0; index < array.length; index += 1) {
-    if (biggestNumber === array[index]) {
+  for (let index = 0; index < list.length; index += 1) {
+    if (biggestNumber === list[index]) {
       timesRepeat += 1;
     }
   }
   return timesRepeat;
 }
 
-function closesToEmentA(mouse, cat1, cat2) {
-  let elementCat1 = mouse - cat1;
-  let elementCat2 = mouse - cat2;
-  if (cat1 === cat2) {
-    return cat1 && cat2;
-  } else if (cat2 < cat1 || elementCat2 > elementCat1) {
-    return cat2;
-  } else {
-    return cat1;
-  }
-}
-
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  mouse = 0;
-  cat1 = 6;
-  cat2 = 12;
-  let beetwen = closesToEmentA(mouse, cat1, cat2);
-  if (beetwen == cat1 && cat2) {
+  let catToCat1 = cat1 - mouse;
+  let catToCat2 = cat2 - mouse;
+  if (cat1 === cat2) {
     return 'os gatos trombam e o rato foge';
-  } else if (beetwen == cat2) {
+  } else if (cat2 < cat1 || catToCat1 > catToCat2) {
     return 'cat2';
   } else {
     return 'cat1';
   }
 }
-console.log(catAndMouse(0, 6, 12));
+
 // Desafio 8
 function fizzBuzz() {
   // seu código aqui
