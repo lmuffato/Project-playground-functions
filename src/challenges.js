84ea0fc370f936e69f16eb2854665e4efec1caca
diegoCorreia <diegomethal@gmail.com>
2021-03-03 19:02:48 -0300

    requisito 4 identaçao

--- src/challenges.js
+++ src/challenges.js
@@ -22,7 +22,7 @@ console.log(splitSentence('vamo que vamo'));
 
 // Desafio 4
 function concatName(array) {
-  return `${array[array.length -1]} ${array[0]}`;
+  return `${array[array.length - 1]} ${array[0]}`;
 }
 console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
 
