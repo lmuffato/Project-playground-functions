eb1e24d29d97c5a275b918a3058680531343cd08
Eduardo Seije <eduardoseije@gmail.com>
2021-03-02 21:26:20 -0300

    Desafio 07 - function catAndMouse - tentativa de correção.

--- src/challenges.js
+++ src/challenges.js
@@ -48,15 +48,15 @@ function highestCount(array) {
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  let cat1Dist = math.abs(cat1 - mouse);
-  let cat2Dist = math.abs(cat2 - mouse);
+  let cat1Dist = Math.abs(cat1 - mouse);
+  let cat2Dist = Math.abs(cat2 - mouse);
   if (cat1Dist < cat2Dist) {
     return 'cat1';
   }
   if (cat1Dist > cat2Dist) {
     return 'cat2';
   }
-    return 'os gatos trombam e o rato foge';
+  return 'os gatos trombam e o rato foge';
 }
 
 // Desafio 8
