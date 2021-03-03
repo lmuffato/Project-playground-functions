5e8c64faa59e4adc6f6ee78c18b457190a938b51
Samuel Melo <samuelmmjunior@gmail.com>
2021-03-03 12:21:48 -0300

    desafio 5

--- src/challenges.js
+++ src/challenges.js
@@ -29,8 +29,9 @@ function concatName(entry) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+let victory = wins * 3;
+return victory + ties;
 }
 
 // Desafio 6
