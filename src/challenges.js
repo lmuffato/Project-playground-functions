2ea3e9f6714f877a7afe9b473d41e4ad0858d048
madruga665 <madruga665@hotmail.com>
2021-03-04 11:20:40 -0300

    Desafio 9

--- src/challenges.js
+++ src/challenges.js
@@ -84,11 +84,21 @@ function fizzBuzz(array) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(string) {
+  return string
+    .replace(/a/g, 1)
+    .replace(/e/g, 2)
+    .replace(/i/g, 3)
+    .replace(/o/g, 4)
+    .replace(/u/g, 5);
 }
-function decode() {
-  // seu código aqui
+function decode(string) {
+  return string
+    .replace(/1/g, 'a')
+    .replace(/2/g, 'e')
+    .replace(/3/g, 'i')
+    .replace(/4/g, 'o')
+    .replace(/5/g, 'u');
 }
 
 module.exports = {
