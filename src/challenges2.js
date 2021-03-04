a8d3fd55e4ec21ba9a52fce69187db1106bff0c2
Bruno Mendes <mendessbruno@gmail.com>
2021-03-04 10:43:15 -0300

    relizada otimizacao do codigo auxiliar do exercicio 11, que cria o numero de telefone

--- src/challenges2.js
+++ src/challenges2.js
@@ -43,9 +43,19 @@ function checkRange(array) {
   return result;
 }
 
-// função auxiliar exercicio 11
+// função auxiliar exercicio 11 visto no w3school o slice 
 function createPhone(array) {
-  return '(' + array[0] + array[1] + ') ' + array[2] + array[3] + array[4] + array[5] + array[6] + '-' + array[7] + array[8] + array[9] + array[10];
+  let num1, num2, num3;
+  let str1 = '(';
+  let str2 = ') ';
+  let str3 = '-';
+  let part1 = array.slice(0, 2);
+  let part2 = array.slice(2, 7);
+  let part3 = array.slice(7, 11);
+  num1 = part1.join('');
+  num2 = part2.join('');
+  num3 = part3.join('');
+  return str1.concat(num1, str2, num2, str3, num3)
 }
 
 // Desafio 11
