4e66d894a108b8eddec55016e25b82e9f22884da
Villefor <franklincrius@gmail.com>
2021-03-04 01:12:10 -0300

    10 requisito

--- src/challenges.js
+++ src/challenges.js
@@ -99,28 +99,29 @@ function fizzBuzz(fizrray) {
 }
 console.log(fizzBuzz([2, 15, 7, 9, 45]));
 
+// Desafio 9
 // Referência Bruno Bastos. Me ajudou a fazer o código, a entender o replace, como funciona o global.
 function encode(letter) {
   let encodedLetter = letter
-   .replace (/a/g, 1)
-   .replace (/e/g, 2)
-   .replace (/i/g, 3)
-   .replace (/o/g, 4)
-   .replace (/u/g, 5)
-  return encodedLetter
+    .replace(/a/g, 1)
+    .replace(/e/g, 2)
+    .replace(/i/g, 3)
+    .replace(/o/g, 4)
+    .replace(/u/g, 5);
+  return encodedLetter;
 }
-console.log (encode('hi there'))
+console.log(encode('hi there'));
 
 function decode(decodeLetter) {
   let decodedLetter = decodeLetter
-  .replace (/1/g, 'a')
-  .replace (/2/g, 'e')
-  .replace (/3/g, 'i')
-  .replace (/4/g, 'o')
-  .replace (/5/g, 'u')
-  return decodedLetter
+    .replace(/1/g, 'a')
+    .replace(/2/g, 'e')
+    .replace(/3/g, 'i')
+    .replace(/4/g, 'o')
+    .replace(/5/g, 'u');
+  return decodedLetter;
 }
-console.log (decode('h3 th2r2!'))
+console.log(decode('h3 th2r2!'));
 
 module.exports = {
   calcArea,
--- src/challenges2.js
+++ src/challenges2.js
@@ -1,9 +1,26 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
-}
+function techList(tech, name) {
+  let functionality = {
+    tech: tech.sort (),
+    techList: []
+  }
+  if (tech === 0) {
+     return 'Vazio!'
+  }
+  for(let index in tech){
+     functionality.techList.push ({
+        tech: tech[index],
+        name: name
+    })
+  } return functionality.techList
+} console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas"))
 
-// Desafio 11
+
+let array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+let arr = array.sort(function(a, b){return a-b});
+console.log (arr)
+
+// referencia - https://www.w3schools.com/jsref/jsref_sort.asp - Me ajudou a entender como o sorte nũmerico em órdem crescente funciona
 function generatePhoneNumber() {
   // seu código aqui
 }
