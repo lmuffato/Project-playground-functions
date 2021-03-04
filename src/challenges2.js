e6771a5775a84516e5d1cb76be0a3cba073de5ad
mallmann02 <leomallmann020302@gmail.com>
2021-03-04 17:48:36 -0300

    Ajusta saída do requisito 13

--- src/challenges2.js
+++ src/challenges2.js
@@ -42,7 +42,7 @@ function generatePhoneNumber(phone) {
   }
 
   return `(${phone[0]}${phone[1]}) ${phone[2]}${phone[3]}${phone[4]}\
-  ${phone[6]}-${phone[7]}${phone[8]}${phone[9]}${phone[10]}`;
+${phone[6]}-${phone[7]}${phone[8]}${phone[9]}${phone[10]}`;
 }
 
 // Desafio 12
