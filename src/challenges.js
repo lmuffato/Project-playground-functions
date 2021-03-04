b453d6637c3c49e427efc08edfca0e3b325d2a26
Luiz <luizfvaccarif@gmail.com>
2021-03-04 20:54:44 +0000

    Update requirement 6

--- src/challenges.js
+++ src/challenges.js
@@ -24,21 +24,11 @@ function footballPoints(win, ties) {
 }
 
 // Desafio 6
-
-function highestNumber(numbers) {
-  let greatestNumber = numbers[0];
+function highestCount(numbers) {
+  let highestNumber = Math.max(...[numbers])
+  let repetitionCounter = 0
   for (let index=0; index<numbers.length; index+=1) {
-    if(numbers[index] > greatestNumber) {
-      greatestNumber = numbers[index]
-    }
-  }
-  return greatestNumber;
-}
-
-function highestCount(numbers, greatestNumber) {
-  let repetitionCounter=0;
-  for (let index=0; index<numbers.length; index+=1) {
-    if (numbers[index] === greatestNumber) {
+    if (numbers[index] === highestNumber) {
     repetitionCounter += 1;
     }
   }
