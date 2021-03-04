036d9f3a075b930d023c86ad3e4457854780e155
AdelinoJnr <adelinojunior96@gmail.com>
2021-03-04 11:04:08 -0300

    Resoluçao do requisito 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -22,32 +22,65 @@ function techList(nomeTech, name) {
 function generatePhoneNumber(listNumber) {
   let listPhone = [];
   let delimitador = 0;
+  let numberZero = false;
+  let bigNumber = false;
+  let repetTree = false;
+  let contRepete = 0;
 
-  for (let index = 0; index < listNumber.length + 4; index += 1) {
-    if(index === 0) {
-      listPhone[index] = '(';
-      delimitador += 1;
-    } else if (index === 3) {
-      listPhone[index] = ')';
-      delimitador += 1;
-    } else if (index === 4) {
-      listPhone[index] == ' ';
-      delimitador += 1;
-    } else if (index === 10) {
-      listPhone[index] == '-';
-      delimitador += 1;
-    } else {
-      listPhone[index] = listNumber[index - delimitador].toString()
+  for (let index = 0; index < listNumber.length; index += 1) {
+
+  }
+
+  for (let index = 0; index < listNumber.length; index += 1) {
+    if (listNumber[index] === 0) {
+      numberZero = true;
+    } else if (listNumber[index] > 9) {
+      bigNumber = true;
+    } else if (listNumber[]) {
+
     }
   }
 
-  let phone = listPhone;
+  if (listNumber.length > 11) {
+    return "Array com tamanho incorreto."
+  } else if (numberZero === true || bigNumber === true || repetTree === true) {
+    return "não é possível gerar um número de telefone com esses valores"
+  } else {
+    for (let index = 0; index < listNumber.length + 4; index += 1) {
+      if(index === 0) {
+        listPhone[index] = '(';
+        delimitador += 1;
+      } else if (index === 3) {
+        listPhone[index] = ')';
+        delimitador += 1;
+      } else if (index === 4) {
+        listPhone[index] == ' ';
+        delimitador += 1;
+      } else if (index === 10) {
+        listPhone[index] == '-';
+        delimitador += 1;
+      } else {
+        listPhone[index] = listNumber[index - delimitador].toString()
+      }
+    }
+  }
+
+  let phone = listPhone.join('');
   return phone;
 }
 
+console.log(generatePhoneNumber())
+
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
-  
+  let somaBC = lineB + lineC;
+  let valueAbs = Math.abs(lineB - lineC);
+
+  if (lineA < somaBC && lineA > valueAbs) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Desafio 13
@@ -67,7 +100,7 @@ function hydrate(myDrink) {
   return myCopyH20;
 }
 
-hydrate('1 cerveja, 2 refrigerante')
+console.log(hydrate('1 cerveja, 2 refrigerante'));
 
 module.exports = {
   generatePhoneNumber,
