2fd6ae06be571f500945bd4c90257c131f3e7d24
Nathalia Zebral <natzebral@gmail.com>
2021-03-04 20:41:10 -0300

    desafios 1 a 5 e 7 correcao para passar nos requisitos

--- src/challenges.js
+++ src/challenges.js
@@ -5,8 +5,8 @@ function compareTrue(value1,value2) {
   } else {
     return false;
   }
-
 }
+
 // Desafio 2
 function calcArea(base,height) {
   let triangleArea = (base * height)/2;
@@ -24,20 +24,17 @@ function concatName(array) {
   let lastFirstArray = array[array.length-1] + ',' + array[0];
   return lastFirstArray;
 }
-console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
+
 // Desafio 5
-let wins = 14;
-let ties = 8;
 
 function footballPoints(wins,ties) {
-  let championshipPoints = wins*3 + ties*1 ;
+  let championshipPoints = wins*3 + ties*1;
   return championshipPoints;
 }
 
 // Desafio 6
 
-function highestCount([9, 1, 2, 3, 9, 5, 7]) {
-
+function highestCount() {
 }
 
 // Desafio 7
@@ -52,8 +49,8 @@ return 'cat1';
 }
 
 // Desafio 8
+
 function fizzBuzz() {
-  // seu código aqui
 }
 
 // Desafio 9
