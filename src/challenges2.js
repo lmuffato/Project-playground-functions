401a572217ce4abf7720bdc995c6e34a7f80b358
Mateus Alencar <mtauws@gmail.com>
2021-03-04 16:17:38 -0300

    chore: exercise 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -30,9 +30,58 @@ function techList(array, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function lengthValid(array) {
+  if (array.length !== 11) {
+    return true;
+  }
+  return false;
 }
+
+function isValidNumber(array) {
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] < 0 || array[index] > 9) {
+      return false;
+    }
+  }
+  return true;
+}
+/**
+ * Estava com um pouco de dificuldade para verificar quantas vezes um número se repete,
+ * dei uma pesquisada no códigos dos colegas e achei a implementação do Bruno Mendes bem interessante
+ * e resolvi implementá-la. Link do GitHub: https://github.com/tryber/sd-010-a-project-playground-functions/tree/bruno-s-mendes-project-playground-functions
+ */
+function isRepeat(array) {
+  let count = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+  for (let index = 0; index < array.length; index += 1) {
+    count[array[index]] += 1;
+  }
+
+  for (let index = 0; index < count.length; index += 1) {
+    if (count[index] >= 3) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function phoneNumber(array) {
+  return `(${array[0]}${array[1]}) ${array[2]}${array[3]}${array[4]}${array[5]}${array[6]}`
+  + `-${array[7]}${array[8]}${array[9]}${array[10]}`;
+}
+
+function generatePhoneNumber(array) {
+  if (lengthValid(array)) {
+    return 'Array com tamanho incorreto.';
+  }
+
+  if (isValidNumber(array) && !isRepeat(array)) {
+    return phoneNumber(array);
+  }
+  return 'não é possível gerar um número de telefone com esses valores';
+}
+
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
 function triangleCheck() {
