e3c78cded89c2a7924dc2cd80afd5d2791f08037
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-03-04 19:37:27 -0300

    tentando req 9

--- src/challenges.js
+++ src/challenges.js
@@ -121,26 +121,27 @@ function encode(word) {
 function decode(word) {
   let changeWord = [];
   let encondeWord = '';
-  for (let i = 0; i < word.length; i += 1) {
-    changeWord.push(word[i]);
+  for(let i = 0;i < word.length ; i += 1) {
+      changeWord.push(word[i]);
   }
-  for (let i in changeWord) {
-    if (changeWord[i] === 1) {
-      changeWord[i] = 'a';
-    } else if (changeWord[i] === 2) {
-      changeWord[i] = 'e';
-    } else if (changeWord[i] === 3) {
-      changeWord[i] = 'i';
-    } else if (changeWord[i] === 4) {
-      changeWord[i] = 'o';
-    } else if (changeWord[i] === 5) {
-      changeWord[i] = 'u';
+  // modificando string
+  for(let i in changeWord) {
+    if(changeWord[i] == 1) {
+        changeWord[i] = 'a';
+    } else if (changeWord[i] == 2) {
+        changeWord[i] = 'e';
+    } else if (changeWord[i] == 3) {
+        changeWord[i] = 'i';
+    } else if (changeWord[i] == 4) {
+        changeWord[i] = 'o';
+    } else if (changeWord[i] == 5) {
+        changeWord[i] = 'u';
     }
   }
-  for (let i = 0; i < changeWord.length; i += 1) {
-    encondeWord += changeWord[i];
+  for(let i in changeWord) {
+      encondeWord += changeWord[i]
   }
-  return encondeWord;
+  return encondeWord 
 }
 
 // Desafio 10
