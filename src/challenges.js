72605bddd155e454b1238f110711f32887413cbb
Rafael Cardoso Trybe truma 10 A <rafael.vimur@gmail.com>
2021-03-04 11:42:38 -0300

    Correção requisito 7

--- src/challenges.js
+++ src/challenges.js
@@ -52,9 +52,11 @@ function highestCount(arrayNumbers) {
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  if (cat1 > cat2) {
+  let distance1 = Math.abs(cat1 - mouse)
+  let distance2 = Math.abs(cat2 - mouse)
+  if (distance1 > distance2) {
     return 'cat2'
-  } else if (cat2 > cat1){
+  } else if (distance2 > distance1){
     return 'cat1'
   } else{
     return 'os gatos trombam e o rato foge'
