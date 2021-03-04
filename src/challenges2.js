33d27e749f6a2e0cd7385fd05283d6f0197232ef
Lucas André <lucas.cancado89@gmail.com>
2021-03-04 11:39:45 -0300

    ESlint ex 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -49,8 +49,8 @@ function generatePhoneNumber(a) {
   if (phonenumberValid(a) === false || phoneNumrepeat(a) === false) {
     return 'não é possível gerar um número de telefone com esses valores';
   }
-  let p = `(${a[0]}${a[1]}) ${a[2]}${a[3]}${a[4]}${a[5]}${a[6]}-${a[7]}${a[8]}${a[9]}${a[10]}`;
-  return p;
+  let phone = `(${a[0]}${a[1]}) ${a[2]}${a[3]}${a[4]}${a[5]}${a[6]}-${a[7]}${a[8]}${a[9]}${a[10]}`;
+  return phone;
 }
 
 console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 2]));
