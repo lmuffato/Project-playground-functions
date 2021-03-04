00844c727c2ed0a3b973259b4245216d68ec7e98
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-03-04 20:52:26 -0300

    exigencia 9 corrigida

--- src/challenges.js
+++ src/challenges.js
@@ -124,16 +124,16 @@ function decode(word) {
   for (let i = 0; i < word.length; i += 1) {
     changeWord.push(word[i]);
   }
-  for (let i in changeWord) {
-    if (changeWord[i] === 1) {
+  for (let i = 0; i < changeWord.length; i += 1) {
+    if (changeWord[i] === '1') {
       changeWord[i] = 'a';
-    } else if (changeWord[i] === 2) {
+    } else if (changeWord[i] === '2') {
       changeWord[i] = 'e';
-    } else if (changeWord[i] === 3) {
+    } else if (changeWord[i] === '3') {
       changeWord[i] = 'i';
-    } else if (changeWord[i] === 4) {
+    } else if (changeWord[i] === '4') {
       changeWord[i] = 'o';
-    } else if (changeWord[i] === 5) {
+    } else if (changeWord[i] === '5') {
       changeWord[i] = 'u';
     }
   }
