c31abe627871ab123ed6c8441c4ef39f4d4a55c0
guilhermemd <guilherme.md@gmail.com>
2021-03-03 23:34:38 -0300

    deafio 8 - enviando com uma linha a mais

--- src/challenges.js
+++ src/challenges.js
@@ -63,6 +63,20 @@ function catAndMouse(mouse, cat1, cat2) {
 
 // Desafio 8
 function fizzBuzz() {
+  let getFizzBuzz = [];
+  for (let index in numbers) {
+    if (numbers[index] % 3 === 0){
+      getFizzBuzz.push('fizz');
+    } 
+    else if (numbers[index] % 5 === 0) {
+      getFizzBuzz.push('buzz');
+    } else if (numbers[index] % 3 === 0 && numbers[index] % 5 === 0) {
+      getFizzBuzz.push('fizzBuzz');
+    } else {
+      getFizzBuzz.push('bug!');
+    }
+  }
+  return getFizzBuzz;
 }
 
 // Desafio 9 credits https://www.youtube.com/watch?v=7a-a6lKoyIQ - https://www.youtube.com/watch?v=ZYPqPoijCAQ
