7443ccdf8890046576af1ef0130262cfdece457c
William Willers <w.willers@outlook.com>
2021-03-04 15:11:40 -0400

    Requisito 9

--- src/challenges.js
+++ src/challenges.js
@@ -91,13 +91,46 @@ function fizzBuzz(arrayOfNumbers) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+/* Ajuda do grande Rafael Medeiros Gomes para formular o for in
+https://github.com/tryber/sd-010-a-project-playground-functions/pull/55/files
+e thread no Slack que não consigo linkar mas o assunto é
+[:sos:] [Projeto - Requisito 9 - JSLint - Function Complexity] */
+function encode(stringsToEncode) {
+  let letterToEncode = {
+    a: 1,
+    e: 2,
+    i: 3,
+    o: 4,
+    u: 5,
+  };
+  let newEncodedString = '';
+  for (let letter in stringsToEncode) {
+    if (letterToEncode[stringsToEncode[letter]]) {
+      newEncodedString += letterToEncode[stringsToEncode[letter]];
+    } else {
+      newEncodedString += stringsToEncode[letter];
+    }
+  }
+  return newEncodedString;
 }
-function decode() {
-  // seu código aqui
+function decode(stringsToDecode) {
+  let numberToDecode = {
+    1: 'a',
+    2: 'e',
+    3: 'i',
+    4: 'o',
+    5: 'u',
+  };
+  let newDecodedString = '';
+  for (let letter in stringsToDecode) {
+    if (numberToDecode[stringsToDecode[letter]]) {
+      newDecodedString += numberToDecode[stringsToDecode[letter]];
+    } else {
+      newDecodedString += stringsToDecode[letter];
+    }
+  }
+  return newDecodedString;
 }
-
 module.exports = {
   calcArea,
   catAndMouse,
