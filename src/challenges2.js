5003a528e0e5efa454a1cb516ce50f7520128ff7
Alexandre Oliveira <alexandremolive@gmail.com>
2021-03-04 13:03:08 -0300

    requisito 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,24 @@
 // Desafio 10
-function techList() {
+function techList(techNames, name) {
   // seu código aqui
-}
+  techNames.sort()
+  let techNameslist = {}
+  let technologies = [];
+  if (techNames.length === 0) {
+    console.log('Vazio!')
+  }
+  else {
+
+    for (let tech in techNames) {
+
+      techNameslist.tech = (techNames[tech])
+      techNameslist.name = name
+      technologies.push(techNameslist)
+
+    } console.log(technologies)
+  }
+} techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Alexandre')
+
 
 // Desafio 11
 function generatePhoneNumber() {
@@ -23,4 +40,4 @@ module.exports = {
   techList,
   hydrate,
   triangleCheck,
-};
+}
