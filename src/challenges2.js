e816d7a8b703bfd5553e53b5783b963eff493540
Felipe Flores <felipefloresvendas@gmail.com>
2021-03-04 02:17:35 -0300

    Chalengue 10 Done!

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,8 +1,18 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(array, studentName) {
+  let object = [];
+  if (array.length === 0) {
+    object = 'Vazio!';
+  }
+  for (let index = 0; index < array.sort().length; index += 1) {
+    let techRefer = { tech: array[index], name: studentName };
+    object.push(techRefer);
+  }
+  return object;
 }
-
+let meuNome = 'Felipe';
+let techs = ['React', 'Jest', 'HTML', 'CSS', 'JavaScript'];
+console.log(techList(techs, meuNome));
 // Desafio 11
 function generatePhoneNumber() {
   // seu código aqui
