dfab8ee22754588888c70363eef6470241f9de2c
Gustavo Sousa <gustavohl98@gmail.com>
2021-03-04 18:57:07 -0300

    Requisito 13 com filter e sem \D

--- src/challenges2.js
+++ src/challenges2.js
@@ -25,9 +25,26 @@ function triangleCheck() {
 
 // Desafio 13
 // Função replace e parâmetros retirados do Stackoverflow(https://stackoverflow.com/questions/30607419/return-only-numbers-from-string) combinado com documentação do MDN
+/*
 function hydrate(drinks) {
   let glassesOfWater = 0;
   const numbers = drinks.replace(/\D/g, '').split('');
+
+  for (let number of numbers) {
+    glassesOfWater += Number(number);
+  }
+
+  return `${glassesOfWater} copo${glassesOfWater > 1 ? 's' : ''} de água`;
+}
+*/
+
+// Refazendo com filter por estar inseguro com "/\D/g"
+function hydrate(drinks) {
+  let glassesOfWater = 0;
+  const numbers = drinks
+    .replace(/ /g, '')
+    .split('')
+    .filter((character) => !Number.isNaN(Number(character)));
 
   for (let number of numbers) {
     glassesOfWater += Number(number);
