b93510abc83a43524fec805e9a0dc8ed0a56eef3
Felipe Flores <felipefloresvendas@gmail.com>
2021-03-04 11:41:57 -0300

    challengue 11 done.

--- src/challenges2.js
+++ src/challenges2.js
@@ -10,12 +10,53 @@ function techList(array, studentName) {
   }
   return object;
 }
-let meuNome = 'Felipe';
-let techs = ['React', 'Jest', 'HTML', 'CSS', 'JavaScript'];
-console.log(techList(techs, meuNome));
+
+function indexBiggerLower(array) {
+  let indexBigger = 9;
+  let indexLower = 0;
+  let result = false;
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] > indexBigger || array[index] < indexLower) {
+      result = true;
+    }
+  }
+  return result;
+}
+
+function checkEqual(array) {
+  for (let index = 0; index < array.length; index += 1) {
+    let isEqual = 0;
+    for (let index1 = 0; index1 < array.length; index1 += 1) {
+      if (array[index] === array[index1]) {
+        isEqual += 1;
+      }
+      if (isEqual >= 3) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  let result = '';
+  if (array.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  } if (indexBiggerLower(array) === true && checkEqual(array) === true) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+  for (let index = 0; index < array.length; index += 1) {
+    if (index === 0) {
+      result += '(';
+    } if (index === 2) {
+      result += ')';
+    } if (index === 7) {
+      result += '-';
+    }
+    result += `${array[index]}`;
+  }
+  return result;
 }
 
 // Desafio 12
