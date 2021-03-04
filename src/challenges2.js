430af0340aa330ae642533f1630ec70e20424d83
William Willers <w.willers@outlook.com>
2021-03-04 16:19:42 -0400

    Requisito bonus 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,23 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function sortTechListArray(array) {
+  /* https://pt.stackoverflow.com/questions/46600/  como-ordenar-uma-array-de-objetos-com-array-sort */
+  return array.sort((firsItem, secondItem) => {
+    if (firsItem.tech < secondItem.tech) return -1;
+    if (firsItem.tech > secondItem.tech) return 1;
+    return 0;
+  });
+}
+
+function techList(arrayOfTechs, name) {
+  let outputArray = [];
+  if (arrayOfTechs.length === 0) {
+    return 'Vazio!';
+  }
+  for (let index = 0; index < arrayOfTechs.length; index += 1) {
+    let tech = arrayOfTechs[index];
+    outputArray.push({ tech, name });
+  }
+  return sortTechListArray(outputArray);
 }
 
 // Desafio 11
