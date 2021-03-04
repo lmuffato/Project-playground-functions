f8cab22680dfa6837317bd33becd72b6efadb3f2
Felipe Flores <felipefloresvendas@gmail.com>
2021-03-04 01:08:50 -0300

    Chalengue 9 ok!

--- src/challenges.js
+++ src/challenges.js
@@ -120,11 +120,42 @@ function fizzBuzz(array) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(phrase) {
+  let numberWord = '';
+  let strings = {
+    a: 1,
+    e: 2,
+    i: 3,
+    o: 4,
+    u: 5,
+  };
+  for (let index = 0; index < phrase.length; index += 1) {
+    if (strings[phrase[index].toLowerCase()]) {
+      numberWord += strings[phrase[index]];
+    } else {
+      numberWord += phrase[index];
+    }
+  }
+  return numberWord;
 }
-function decode() {
-  // seu código aqui
+
+function decode(phrase) {
+  let wordNumber = '';
+  let strings = {
+    1: 'a',
+    2: 'e',
+    3: 'i',
+    4: 'o',
+    5: 'u',
+  };
+  for (let index = 0; index < phrase.length; index += 1) {
+    if (strings[phrase[index].toLowerCase()]) {
+      wordNumber += strings[phrase[index]];
+    } else {
+      wordNumber += phrase[index];
+    }
+  }
+  return wordNumber;
 }
 
 module.exports = {
