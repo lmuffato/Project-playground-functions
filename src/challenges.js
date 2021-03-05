e27b798041fccee452da036a124b4b6254dbf3d0
andy silva <andssilva.23@gmail.com>
2021-03-05 11:00:51 -0400

    requisito 3

--- src/challenges.js
+++ src/challenges.js
@@ -16,9 +16,13 @@ console.log(calcArea(5, 2));
 console.log(calcArea(51, 1));
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+  let arrayStrings = string.split(' ');
+  return arrayStrings;
 }
+console.log(splitSentence('go Trybe'));
+console.log(splitSentence('vamo que vamo'));
+console.log(splitSentence('foguete'));
 
 // Desafio 4
 function concatName() {
