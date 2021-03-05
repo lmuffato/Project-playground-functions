53cd2b503df5763bde460adc4b9b22be409344ef
¨Moises <moisescoimbra.digitalpro@gmail.com>
2021-03-05 11:49:13 -0300

    Requisitos 10 e 12 - resolvidos

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,14 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(arrayTech, fame) {
+  let newArrayObjects = [];
+  if (arrayTech.length === 0) {
+    console.log('Vazio!');
+  } else {
+    for (let index = 0; index < arrayTech.length; index += 1) {
+      newArrayObjects.push({ tech: arrayTech[index], name: fame });
+    }
+  }
+  return newArrayObjects;
 }
 
 // Desafio 11
@@ -9,8 +17,14 @@ function generatePhoneNumber() {
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  if (lineA > Math.abs(lineB - lineC) && lineA < (lineB + lineC)
+   && lineB > Math.abs(lineA - lineC) && lineB < (lineA + lineC)
+   && lineC > Math.abs(lineA - lineB) && lineC < (lineA + lineB)) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Desafio 13
