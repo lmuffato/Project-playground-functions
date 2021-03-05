58da903e52a45783daf7117cdd44d899870ded26
Paulo Vitor Borges <pvtrybe209@gmail.com>
2021-03-05 14:18:55 -0300

    Ajuste requisito 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -3,17 +3,18 @@ function techList(currentTech, name) {
   let techName = {};
   let arrayTechName = [];
   currentTech.sort();
+  if (currentTech.length === 0) {
+    return ('Vazio!');
+  }
   for (let index = 0; index < currentTech.length; index += 1) {
     techName = {
       name,
       tech: currentTech[index],
     };
     arrayTechName.push(techName);
-
   }
   return arrayTechName;
 }
-
 // Desafio 11
 function generatePhoneNumber() {
   // seu código aqui
@@ -28,8 +29,8 @@ function triangleCheck() {
 function hydrate() {
   // seu código aqui
 }
-a = ['React', 'Jest', 'HTML', 'CSS','JavaScript'];
-console.log(techList(a,'Lucas'));
+a = [];
+console.log(techList(a, 'Lucas'));
 module.exports = {
   generatePhoneNumber,
   techList,
