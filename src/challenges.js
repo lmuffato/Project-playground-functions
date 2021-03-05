cabc24fcdba76e41f47a71327433ed6d3c90e69a
JuliaBaptista <juliabaptistadeoliveira@gmail.com>
2021-03-05 18:35:53 -0300

    Conclusao requisito 02 e 03 com devidas correçoes de espaçamento de linhas

--- src/challenges.js
+++ src/challenges.js
@@ -10,7 +10,7 @@ function compareTrue(value1, value2) {
 
 // Desafio 2
 function calcArea(base, height) {
-  return base*height/2;
+  return base * height / 2;
 }
 
 // abaixo o print do resultado da função
