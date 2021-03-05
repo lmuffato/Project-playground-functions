bfad134512d6c0fa8990f91fd63335ab95e9129c
GetulioGutemberg <getulio.dev@gmail.com>
2021-03-05 02:26:09 -0300

    soluciona quesito10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,20 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(arrayTech, name) {
+  let result = [];
+  if (arrayTech.length === 0){
+      console.log('Vazio!')
+  }else {
+      for (let index = 0; index < arrayTech.length; index += 1){
+          let step = [];
+          step.push('tech: ' + arrayTech[index]);
+          step.push('name: ' + name);
+          result[index] = (step);
+          step.pop;
+          step.pop;
+      }
+
+      console.log(result);
+  }
 }
 
 // Desafio 11
