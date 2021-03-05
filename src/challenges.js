31f7528aee7571a682267112aceda2028e035665
Debora Passos <deborinhapassos@gmail.com>
2021-03-05 19:18:11 -0300

    arrumando eslint

--- src/challenges.js
+++ src/challenges.js
@@ -12,8 +12,7 @@ console.log(compareTrue(true, true));
 
 // Desafio 2
 function calcArea(base, heigth) {
-  // seu código aqui
-  return  (base * heigth) / 2; 
+  return (base * heigth) / 2; 
 }
 
 console.log(calcArea(10, 50));
@@ -24,47 +23,43 @@ console.log(calcArea(51, 1));
 function splitSentence(shareString) {
   return shareString.split(' ');
 }
-console.log(splitSentence('go, Trybe'))
-console.log(splitSentence('vamo que vamo'))
-console.log(splitSentence('foguete'))
+console.log(splitSentence('go, Trybe'));
+console.log(splitSentence('vamo que vamo'));
+console.log(splitSentence('foguete'));
 
 // Desafio 4
 function concatName(arrayString) {
-  return `${arrayString[arrayString.length-1]}, ${arrayString[0]}` ;
+  return `${arrayString[arrayString.length - 1]}, ${arrayString[0]}`;
 }
-arrayString = ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']
-console.log(concatName(arrayString));
-arrayString = ['foguete', 'não', 'tem', 'ré']
-console.log(concatName(arrayString));
-arrayString = ['captain', 'my', 'captain']
-console.log(concatName(arrayString));
+console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
+console.log(concatName(['foguete', 'não', 'tem', 'ré']));
+console.log(concatName(['captain', 'my', 'captain']));
 
 // Desafio 5
 function footballPoints(wins, ties) {
-  return (wins * 3 + ties);
+return (wins * 3 + ties);
 }
 
 console.log(footballPoints(14, 8));
 console.log(footballPoints(1, 2));
 console.log(footballPoints(0, 0));
 
-
 // Desafio 6
 function highestCount(numbers) {
   let maior = 0;
   let count = 0;
 
- for (let index = 0; index < numbers.length; index +=1) {
-    if(numbers[index] > maior) {
+  for (let index = 0; index < numbers.length; index += 1) {
+  if (numbers[index] > maior) {
     maior = numbers[index];
     }
  }
 
- for (let index = 0; index < numbers.length; index +=1){
-    if (numbers[index] === maior) {
-      count +=1;
-    }
+  for (let index = 0; index < numbers.length; index += 1){
+  if (numbers[index] === maior) {
+  count += 1;
   }
+}
   return count;
 }
 
@@ -79,38 +74,37 @@ console.log(highestCount(meuArray));
 function catAndMouse(mouse, cat1, cat2) {
   let res;
 
-  if(Math.abs(mouse - cat1) < Math.abs(mouse - cat2)){
+  if (Math.abs(mouse - cat1) < Math.abs(mouse - cat2)) {
     res = 'cat1';
-  }else if(Math.abs(mouse - cat1) > Math.abs(mouse - cat2)){
+  }else if (Math.abs(mouse - cat1) > Math.abs(mouse - cat2)) {
     res = 'cat2';
   } else {
-    res = 'os gatos trombam e o rato foge' ;
+    res = 'os gatos trombam e o rato foge';
   }
   return res;
 }
-  console.log(catAndMouse(1, 4, 3));
-  console.log(catAndMouse(0, 6, 12));
-  console.log(catAndMouse(1, 0, 2));
-
+console.log(catAndMouse(1, 4, 3));
+console.log(catAndMouse(0, 6, 12));
+console.log(catAndMouse(1, 0, 2));
 
 // Desafio 8
 function fizzBuzz(arrayNumber) {
   let number = [];
 
-  for(let index = 0; index < arrayNumber.length; index += 1) {
-    if(arrayNumber[index] % 5 === 0 && arrayNumber[index] % 3 === 0) {
-      number.push ("fizzBuzz");
-    } else if(arrayNumber[index] % 5 === 0) {
-      number.push ("buzz");
-    } else if(arrayNumber[index] % 3 === 0) {
-      number.push ("fizz");
+  for (let index = 0; index < arrayNumber.length; index += 1) {
+    if (arrayNumber[index] % 5 === 0 && arrayNumber[index] % 3 === 0) {
+      number.push('fizzBuzz');
+    } else if (arrayNumber[index] % 5 === 0) {
+      number.push('buzz');
+    } else if (arrayNumber[index] % 3 === 0) {
+      number.push('fizz');
     } else {
-      number.push ("bug!");
+      number.push('bug!');
     }
   }
   return number;
 }
-  console.log(fizzBuzz([2, 15, 7, 9, 45]));
+console.log(fizzBuzz([2, 15, 7, 9, 45]));
 
 // Desafio 9
 function encode(vowel) {
@@ -121,7 +115,7 @@ function encode(vowel) {
 
 function decode(vowel) {
   res = vowel.replace(/1/g, 'a').replace(/2/g, 'e').replace(/3/g, 'i').replace(/4/g, 'o').replace(/5/g, 'u');
-  return vowel;
+  return res;
 }
 console.log(encode('hi, there'));
 
