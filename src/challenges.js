1874069445d5f67776e76790ac2b3640071df3a1
GetulioGutemberg <getulio.dev@gmail.com>
2021-03-05 01:31:43 -0300

    soluciona quesito9

--- src/challenges.js
+++ src/challenges.js
@@ -80,10 +80,75 @@ function fizzBuzz() {
 
 // Desafio 9
 function encode() {
-  // seu código aqui
+  let output = [];
+    let encodeString = [];
+    for (let index = 0; index < arraystring.length; ){
+        if (arraystring[index] === 'a'){
+            encodeString.push('1');
+            index += 1;
+        }
+        if (arraystring[index] === 'e'){
+            encodeString.push('2');
+            index += 1;
+            
+        }
+        if (arraystring[index] === 'i'){
+            encodeString.push('3');
+            index += 1;
+        }
+        if (arraystring[index] === 'o'){
+            encodeString.push('4');
+            index += 1;
+        }
+        if (arraystring[index] === 'u'){
+            encodeString.push('5');
+            index += 1;
+        }
+        else {
+            encodeString.push(arraystring[index]);
+            index += 1;
+        }
+    }
+    encodeString.pop();
+    for(index = 0; index < encodeString.length; index += 1){
+        output = output + encodeString[index];
+    }
+    console.log(output)// seu código aqui
 }
 function decode() {
-  // seu código aqui
+  let output = [];
+    let decodeString = [];
+    for (let index = 0; index < arraystring.length + 1; ){
+        if (arraystring[index] === '1'){
+            decodeString.push('a');
+            index += 1;
+        }
+        if (arraystring[index] === '2'){
+            decodeString.push('e');
+            index += 1;
+        }
+        if (arraystring[index] === '3'){
+            decodeString.push('i');
+            index += 1;
+        }
+        if (arraystring[index] === '4'){
+            decodeString.push('o');
+            index += 1;
+        }
+        if (arraystring[index] === '5'){
+            decodeString.push('u');
+            index += 1;
+        }
+        else {
+            decodeString.push(arraystring[index]);
+            index += 1;
+        }
+    }
+    decodeString.pop();
+    for(index = 0; index < decodeString.length; index += 1){
+        output = output + decodeString[index];
+    }
+    console.log(output)// seu código aqui
 }
 
 module.exports = {
