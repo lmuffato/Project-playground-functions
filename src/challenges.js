d3b900d9cce7f75756f25d6653d48599c3484d8a
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2021-03-05 20:46:20 -0300

    Correction errors requirement sevem

--- src/challenges.js
+++ src/challenges.js
@@ -47,17 +47,14 @@ let repeticao = 0;
 }
 
 // Desafio 7 - calcule as distâncias entre o rato e os gatos 
-let rato = mouse;
-let gato1 = mouse - cat1;
-let gato2 = mouse - cat2;
 function catAndMouse(mouse,cat1,cat2) {
-  let gatoProximo1 = "cat1";
-  let gatoProximo2 = "cat2";
+  let gato1 = cat1 - mouse ;
+  let gato2 = cat2 - mouse ;
   if (gato1 < gato2 ) {
-    return gatoProximo1;
-  }else if (gato2 < gato1) {
-    return gatoProximo2;
-  }else {
+    return "cat1";
+  } else if (gato2 < gato1) {
+    return "cat2";
+  } else {
     return "os gatos trombam e o rato foge";
   }
 }
