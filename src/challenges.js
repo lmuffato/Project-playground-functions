ccb4dd846ab11c092f527718dc067717a0979601
Douglas <douguinhodns@gmail.com>
2021-03-04 22:54:13 -0300

    exercicio 9 concluido

--- src/challenges.js
+++ src/challenges.js
@@ -85,10 +85,16 @@ function fizzBuzz(array) {
 
 // Desafio 9
 function encode(name) {
-  // seu código aqui
+  name = name.replace('a', '1')
+    .replace('e', '2').replace('i', '3')
+      .replace('o', '4').replace('u', '5');
+  return name;
 }
-function decode() {
-  // seu código aqui
+function decode(name) {
+  name = name.replace('1', 'a')
+    .replace('2', 'e').replace('3', 'i')
+      .replace('4', 'o').replace('5', 'u');
+  return name;
 }
 
 module.exports = {
