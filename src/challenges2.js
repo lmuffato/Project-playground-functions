542f78aef7cae7532d6abce5af789debf83bc47c
Paulo Vitor Borges <pvtrybe209@gmail.com>
2021-03-05 20:47:36 -0300

    Requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -15,9 +15,92 @@ function techList(currentTech, name) {
   }
   return arrayTechName;
 }
+function checkphoneNumber3(number, check) {
+  let currentNumber = number[0];
+  let counter = 0;
+  for (let index = 0; index < number.length; index += 1) {
+    if (number[index] === currentNumber) {
+      counter += 1;
+      currentNumber = number[index];
+    } else if (counter >= 3) {
+      check = false;
+    }
+  }
+  return check;
+}
+function checkPhoneNumber2(number, check) {
+  for (let index = 0; index < number.length; index += 1) {
+    if (number[index] < 0 || number[index] > 9) {
+      check = false;
+    }
+  }
+  return check;
+}
+function checkPhoneNumber1(number, check) {
+  if (number.length !== 11) {
+    check = false;
+  }
+  return check;
+}
+function writingMessage(check1, check2, check3) {
+  let message;
+  if (check1 === false) {
+    message = 'Array com tamanho incorreto.';
+  } else if (check2 === false || check3 === false) {
+    message = 'não é possível gerar um número de telefone com esses valores';
+  }
+  return message;
+}
+function generateDDD(number) {
+  let phoneNumber = '(';
+  for (let index = 0; index < number.length; index += 1) {
+    if (index === 0) {
+      phoneNumber += number[index];
+    }
+    if (index === 1) {
+      phoneNumber += number[index];
+      phoneNumber += ')';
+    }
+    
+  }
+  return phoneNumber;
+}
+function firstPartNumber(number, phoneNumber) {
+  for (let index = 0; index < number.length; index += 1) {
+    if (index >= 2 && index <= 6) {
+      phoneNumber += number[index];
+    } else if (index === 7) {
+      phoneNumber += '-';
+    }
+  }
+  return phoneNumber;
+}
+function secondPartNumber(number, phoneNumber) {
+  for (let index = 0; index < number.length; index += 1) {
+    if (index >= 7) {
+      phoneNumber += number[index];
+    }
+  }
+  return phoneNumber;
+}
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(phoneNumber) {
+  let check1;
+  let check2;
+  let check3;
+  let message;
+  let number;
+  check1 = checkPhoneNumber1(phoneNumber, check1);
+  check2 = checkPhoneNumber2(phoneNumber, check2);
+  check3 = checkphoneNumber3(phoneNumber, check3);
+  message = writingMessage(check1, check2, check3);
+  if (check1 === false || check2 === false || check3 === false) {
+    return message;
+  }
+  number = generateDDD(phoneNumber);
+  number = firstPartNumber(phoneNumber, number);
+  number = secondPartNumber(phoneNumber, number);
+  return number;
 }
 
 // Desafio 12
@@ -29,8 +112,8 @@ function triangleCheck() {
 function hydrate() {
   // seu código aqui
 }
-a = [];
-console.log(techList(a, 'Lucas'));
+let a = [1, 2, 3, 4, 5, 6, 7, 8, 7, 4, 1];
+console.log(generatePhoneNumber(a));
 module.exports = {
   generatePhoneNumber,
   techList,
