477dd7901bf7c5075cc633b9c41b0b2a7824b5d3
andy silva <andssilva.23@gmail.com>
2021-03-05 10:53:27 -0400

    requisito 2

--- src/challenges.js
+++ src/challenges.js
@@ -7,9 +7,13 @@ function compareTrue(value1, value2) {
 console.log(compareTrue(true, true));
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let triangle = (base * height) / 2;
+  return triangle;
 }
+console.log(calcArea(10, 50));
+console.log(calcArea(5, 2));
+console.log(calcArea(51, 1));
 
 // Desafio 3
 function splitSentence() {
