c323714ed64b7668fbc98d43dd0d43a83616da94
Nathalia Zebral <natzebral@gmail.com>
2021-03-04 21:30:46 -0300

    correcoes erros lint e mudanca no desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -1,34 +1,32 @@
 // Desafio 1
-function compareTrue(value1,value2) {
-  if(value1 === true && value2 === true) {
+function compareTrue(value1, value2) {
+  if (value1 === true && value2 === true) {
     return true;
-  } else {
-    return false;
-  }
+  } 
 }
 
 // Desafio 2
-function calcArea(base,height) {
-  let triangleArea = (base * height)/2;
+function calcArea(base, height) {
+  let triangleArea = (base * height) / 2;
   return triangleArea;
 } 
 
 // Desafio 3
 function splitSentence(string) {
-  let sentenceSplit = string.split(" ");
+  let sentenceSplit = string.split(' ');
   return sentenceSplit;
-} 
+}
 
 // Desafio 4
 function concatName(array) {
-  let lastFirstArray = array[array.length-1] + ',' + array[0];
+  let lastFirstArray = array[array.length-1] + ', ' + array[0];
   return lastFirstArray;
 }
 
 // Desafio 5
 
-function footballPoints(wins,ties) {
-  let championshipPoints = wins*3 + ties*1;
+function footballPoints(wins, ties) {
+  let championshipPoints = wins * 3 + ties * 1;
   return championshipPoints;
 }
 
@@ -38,14 +36,15 @@ function highestCount() {
 }
 
 // Desafio 7
+
 function catAndMouse(mouse, cat1, cat2) {
-if (mouse < cat1 && cat1 < cat2) {
-return 'cat1';
-} else if (mouse < cat2 && cat2 < cat1) {
-  return 'cat2';
-} else {
-  return 'os gatos trombam e o rato foge';
-}
+  if (cat1 < cat2) {
+    return 'cat1';
+    } else if (cat2 < cat1) {
+    return 'cat2';
+    } else if (cat1 === cat2) {
+    return 'os gatos trombam e o rato foge';
+    }
 }
 
 // Desafio 8
@@ -73,4 +72,3 @@ module.exports = {
   highestCount,
   splitSentence,
 }
-
