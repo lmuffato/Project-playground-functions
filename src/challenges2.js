de69156d669ae4a193afc5c4be82a837c88dab89
Marcelo Maurício <mmauricio.design@gmail.com>
2021-03-05 13:09:59 -0300

    Requisito 10 - Refatoracao

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,27 +1,39 @@
 // Desafio 10
-function techList(stringArray, nameObject) {
+function objectCreator(array, names) {
   let arrayObjects = [0];
-  for (let index = 0; index < stringArray.length; index += 1) {
+  for (let index = 0; index < array.length; index += 1) {
     let objects = {
-      tech: stringArray[index],
-      name: nameObject,
+      tech: array[index],
+      name: names,
     };
     arrayObjects[index] = objects;
   }
+  return arrayObjects;
+}
+
+function sortList(a, b) {
+  if (a.tech > b.tech) {
+    return 1;
+  }
+  if (a.tech < b.tech) {
+    return -1;
+  }
+  return 0;
+}
+
+function arrangeArray(arrayObjects) {
   if (arrayObjects.length >= 5) {
-    arrayObjects.sort(function (a, b) { // Entrei no MDN para entender sobre o comando sort de array, e lá tem essa explicação de como ordenar objetos.
-      if (a.tech > b.tech) {
-        return 1;
-      }
-      if (a.tech < b.tech) {
-        return -1;
-      }
-      return 0;
-    });
+    arrayObjects.sort(sortList);
     return arrayObjects;
   } if (arrayObjects.tech === undefined) {
     return 'Vazio!';
   }
+}
+
+function techList(stringArray, nameObject) {
+  let arrayObjects = objectCreator(stringArray, nameObject);
+  arrayObjects = arrangeArray(arrayObjects);
+  return arrayObjects;
 }
 
 // Desafio 11
