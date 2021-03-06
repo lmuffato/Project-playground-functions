2e914a7813d8db5a1092dbfbe6941be426170dad
biazidioti <bzferreira.bio@gmail.com>
2021-03-06 10:01:28 -0300

    Start functon 8.

--- src/challenges.js
+++ src/challenges.js
@@ -71,11 +71,24 @@ function fizzBuzz(number) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(encodeArray) {
+  let encodeDone = 0;
+  for (var index = 0; index < sentenceArray.length; index += 1) {
+    if (sentenceArray[index])
+    a 1
+    e 2
+    i 3
+    o 4
+    u 5
+  }
 }
-function decode() {
-  // seu código aqui
+function decode(decodeArray) {
+  let encodeDone = 0;
+1 a
+2 e
+3 i
+4 o
+5 u
 }
 
 module.exports = {
