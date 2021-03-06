73bea21379068aeeaf1fe06844f5f172a8d0e8f0
ThalitaC <thalitaceci@gmail.com>
2021-03-06 20:17:40 -0300

    corrige requisito 10 para retornar vazio! caso o array techNameArray seja vazio

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,16 +1,18 @@
 // Desafio 10 //used this documentation about sort method https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
-function techList(TechNameArray, userName) {
+function techList(techNameArray, userName) {
   let items = [];
-  for (let index = 0; index < TechNameArray.sort().length; index += 1) {
+  for (let index = 0; index < techNameArray.sort().length; index += 1) {
     items[index] = {
-      tech: TechNameArray[index],
+      tech: techNameArray[index],
       name: userName,
     };
   }
+  if (techNameArray.length === 0) {
+    items = 'Vazio!';
+  }
   return items;
 }
-
-console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas'));
+console.log(techList([], 'Lucas'));
 
 // Desafio 11
 function generatePhoneNumber() {
