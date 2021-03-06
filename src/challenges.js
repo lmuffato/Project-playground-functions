6c2ae9be7f2d301ec491e5fdc2eb1bdfea55e96b
lucas-paz <lucas.hpaz@outlook.com>
2021-03-06 15:28:14 -0300

    Muda a lógica na questão 7, salvando o resultado em uma variável

--- src/challenges.js
+++ src/challenges.js
@@ -11,22 +11,22 @@ function compareTrue(value1, value2) {
 
 // Desafio 2
 function calcArea(base, height) {
-  return (base * height)/2;
+  return (base * height) / 2;
 }
 
 // // Desafio 3
 function splitSentence(phrase) {
-    return phrase.split(' ');
+  return phrase.split(' ');
 }
 
 // Desafio 4
 function concatName(phraseList) {
-  return phraseList[phraseList.length -1] + ', ' + phraseList[0]
+  return phraseList[phraseList.length - 1] + ', ' + phraseList[0]
 }
 
 // Desafio 5
 function footballPoints(wins, ties) {
-  let points = ((wins * 3) + ( ties * 1));
+  let points = ((wins * 3) + (ties * 1));
   return points;
 }
 
@@ -35,7 +35,7 @@ function highestCount(numberList) {
   let maxNumber = Math.max(...numberList);
   let countNumber = 0;
   for (let index = 0; index < numberList.length; index += 1) {
-    if(numberList[index] === maxNumber) {
+    if (numberList[index] === maxNumber) {
       countNumber += 1;
     };
   }
@@ -46,31 +46,33 @@ function highestCount(numberList) {
 function catAndMouse(mouse, cat1, cat2) {
   let cat1Distance = mouse - cat1;
   let cat2Distance = mouse - cat2;
-  if (cat1Distance > cat2Distance){
-    return "cat2";
+  let resultCats = "";
+  if (cat1Distance > cat2Distance) {
+    resultCats= "cat2";
   } else if (cat2Distance > cat1Distance) {
-    return "cat1";
+    resultCats = "cat1";
   } else {
-    return "os gatos trombam e o rato foge"
+    resultCats = "os gatos trombam e o rato foge";
   };
+  return resultCats;
 }
 
 // Desafio 8
 function fizzBuzz(arrayNumberList) {
   let fizzCode = [];
-  for (let indice = 0; indice < arrayNumberList.length; indice += 1){
-    if ( (arrayNumberList[indice]%3 === 0) && (arrayNumberList[indice]%5 === 0)){
+  for (let indice = 0; indice < arrayNumberList.length; indice += 1) {
+    if ((arrayNumberList[indice] % 3 === 0) && (arrayNumberList[indice] % 5 === 0)) {
       fizzCode.push("fizzBuzz");
-    } else if (arrayNumberList[indice]%3 === 0) {
+    } else if (arrayNumberList[indice] % 3 === 0) {
       fizzCode.push("fizz");
-    } else if (arrayNumberList[indice]%5 === 0) {
+    } else if (arrayNumberList[indice] % 5 === 0) {
       fizzCode.push("buzz");
     } else {
-        fizzCode.push("bug!");
-      };
+      fizzCode.push("bug!");
     };
-    return fizzCode;
-  }
+  };
+  return fizzCode;
+}
 
 // Desafio 9
 function encode() {
