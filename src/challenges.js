cf9e5c2af16bf6e9ada53d1d5e30ada188d93ab0
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2021-03-05 21:12:20 -0300

    Correction errors requirement sevem

--- src/challenges.js
+++ src/challenges.js
@@ -31,37 +31,36 @@ function footballPoints(wins, ties) {
 
 // Desafio 6 - Crie uma função que calcule a repetição do maior número
 function highestCount(arrayRip) {
-let guardaMaiorNumero = 0;
-let repeticao = 0;
-  for (let index = 0; index < arrayRip.length; index += 1){
+  let guardaMaiorNumero = 0;
+  let repeticao = 0;
+  for (let index = 0; index < arrayRip.length; index += 1) {
     if (guardaMaiorNumero < arrayRip[index]) {
-      guardaMaiorNumero = arrayRip[index] 
+      guardaMaiorNumero = arrayRip[index];
     }
-  }    
-  for ( let index = 0; index < arrayRip.length; index += 1) {
+  }
+  for (let index = 0; index < arrayRip.length; index += 1) {
     if (guardaMaiorNumero === arrayRip[index]) {
       repeticao += 1;
     }
   }
-  return repeticao; 
+  return repeticao;
 }
 
-// Desafio 7 - calcule as distâncias entre o rato e os gatos 
-function catAndMouse(mouse,cat1,cat2) {
-  let gato1 = cat1 - mouse ;
-  let gato2 = cat2 - mouse ;
-  if (gato1 < gato2 ) {
-    return "cat1";
+// Desafio 7 - calcule as distâncias entre o rato e os gatos
+function catAndMouse(mouse, cat1, cat2) {
+  let gato1 = cat1 - mouse;
+  let gato2 = cat2 - mouse;
+  if (gato1 < gato2) {
+    return 'cat1';
   } else if (gato2 < gato1) {
-    return "cat2";
-  } else {
-    return "os gatos trombam e o rato foge";
+    return 'cat2';
   }
+  return 'os gatos trombam e o rato foge';
 }
 
 // Desafio 8
 function fizzBuzz(returnDeArray) {
- let fizzBuzzArray = [];
+  let fizzBuzzArray = [];
   for (let index = 0; index < returnDeArray.length; index += 1) {
     if (returnDeArray[index] % 3 === 0 && returnDeArray[index] % 5 === 0) {
         fizzBuzzArray.push('fizzBuzz');
