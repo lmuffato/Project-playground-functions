037ea9e781a5a08900f7a73b3df2b1e46ddf36f1
Maria Baeta <m.martinsdg@gmail.com>
2021-03-06 11:16:44 -0300

    Teste do requisito 7

--- src/challenges.js
+++ src/challenges.js
@@ -57,10 +57,10 @@ console.log(highestCount([9, 1, 2, 3, 9, 5, 7]));
 
 // Desafio 7 (fazer)
 function catAndMouse(mouse, cat1, cat2) {
-  if ((cat1 - mouse) < (cat2 - mouse)) {
+  if (Math.abs(cat1 - mouse) < Math.abs(cat2 - mouse)) {
     return 'cat1';
   }
-  if ((cat1 - mouse) > (cat2 - mouse)) {
+  if (Math.abs(cat1 - mouse) > Math.abs(cat2 - mouse)) {
     return cat2;
   }
   return 'os gatos trombam e o rato foge';
