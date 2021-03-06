c5ed69e46b3f2865001bd5fa74290e95cf1dc0f2
Hernanne <hernannedomiciano@gmail.com>
2021-03-06 16:30:16 -0300

    exercicio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,28 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function sorting(a, b) {
+  return b.tech > a.tech ? -1 : 1;
 }
+
+function techList(numbers, name) {
+  let namelist = name;
+  let numberlist = [];
+  if (numbers.length === 5) {
+    for (let index = 0; index < 5; index += 1) {
+      let techlist = {
+        name: '',
+        tech: '',
+      };
+      techlist.tech = numbers[index];
+      techlist.name = namelist;
+      numberlist[index] = techlist;
+    }
+    return numberlist.sort(sorting);
+  } if (name.length === 0) {
+    return 'vazio!';
+  }
+} 
+console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Hernanne'));
+// novamente exercicio 10 finalizado com ajuda, por estar tendo mt dificuldade com for
 
 // Desafio 11
 function generatePhoneNumber() {
