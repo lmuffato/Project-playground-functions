ea826bf2b1ccf1b44cfd3aac1f9380a591035ec9
Maria Lisboa <marialisboa1515@gmail.com>
2021-03-06 12:30:00 -0300

    Desafio 4

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,6 @@
 // Desafio 1
 function compareTrue(value1, value2) {
-  if(value1==true && value2==true){
+  if(value1===true && value2===true) {
     return true;
   }
   else{
@@ -11,22 +11,37 @@ console.log(compareTrue(true, false));
 
 // Desafio 2
 function calcArea(base, heigth) {
-  return (base*heigth)/2;
+  return (base * heigth) / 2;
   }
-  console.log(calcArea(10,50))
-  console.log(calcArea(5,2))
-  console.log(calcArea(51,1))
-
-
+console.log(calcArea(10, 50))
+console.log(calcArea(5, 2))
+console.log(calcArea(51, 1));
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+let recebe = [];
+recebe = string.split(' ');
+return recebe ;
+}
+console.log(splitSentence('go Trybe'))
+console.log(splitSentence('vamo que vamo'))
+console.log(splitSentence('foguete'))
+// Desafio 4
+function concatName(arrayString) {
+let names = '';
+for(index = 0; index<arrayString.length; index+=1) {
+if(index == 0) {
+names =arrayString[arrayString.length-1] ;
+}
+if(index ==(arrayString.length-1)) {
+names = names + ' , ' + arrayString[0] ;
+}
+}
+return names ;
 }
 
-// Desafio 4
-function concatName() {
-  // seu código aqui
-}
+console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
+console.log(concatName(['foguete', 'não', 'tem', 'ré']));
+console.log(concatName(['captain', 'my', 'captain']));
 
 // Desafio 5
 function footballPoints() {
