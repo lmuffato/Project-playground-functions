a17822a93f5ded97681eb2d7c0ef58d2e755990e
ThalitaC <thalitaceci@gmail.com>
2021-03-06 17:52:34 -0300

    Requisito 9

--- src/challenges.js
+++ src/challenges.js
@@ -79,14 +79,23 @@ function fizzBuzz(arrayNumbers) {
   return result;
 }
 
-console.log(fizzBuzz([2, 15, 7, 9, 45]));
-
-// Desafio 9
-function encode() {
-  // seu código aqui
+// Desafio 9 //learned about replaceAll on the documentation at https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/replaceAll
+function encode(string) {
+  let vowelsToNumber = string.replaceAll('a', '1')
+    .replaceAll('e', '2')
+    .replaceAll('i', '3')
+    .replaceAll('o', '4')
+    .replaceAll('u', '5');
+  return vowelsToNumber;
 }
-function decode() {
-  // seu código aqui
+
+function decode(string) {
+  let numbersToVowels = string.replaceAll('1', 'a')
+    .replaceAll('2', 'e')
+    .replaceAll('3', 'i')
+    .replaceAll('4', 'o')
+    .replaceAll('5', 'u');
+  return numbersToVowels;
 }
 
 module.exports = {
