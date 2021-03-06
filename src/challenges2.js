de2a5e472fc565ab17232a80dbe90308e43bb82b
Cla Oliveira <clarice.coliveira@yahoo.com.br>
2021-03-06 18:04:20 -0300

    Requisito 11 - generatePhoneNumber

--- src/challenges2.js
+++ src/challenges2.js
@@ -2,7 +2,6 @@
 function techList(techNames, personName) {
   let techNamesSorted;
   let techObjects;
-  
   if (techNames.length !== 0) {
     techObjects = [];
     techNamesSorted = techNames.sort();
@@ -20,39 +19,72 @@ function techList(techNames, personName) {
 // Desafio 11
 function checkLength(numbers) {
   if (numbers.length !== 11) {
-    return 'Array com tamanho incorreto.';
+    return true;
   }
 }
 
 function checkNumberRange(numbers) {
   for (let index = 0; index < numbers.length; index += 1) {
     if (numbers[index] < 0 || numbers[index] > 9) {
-      return 'não é possível gerar um número de telefone com esses valores';
+      return true;
     }
   }
 }
 
 function checkRepeatThreeTimes(numbers) {
-  let count = 0;
   for (let index = 0; index < numbers.length; index += 1) {
+    let count = 0;
     let number = numbers[index];
     for (let innerIndex = 0; innerIndex < numbers.length; innerIndex += 1) {
       if (numbers[innerIndex] === number) {
         count += 1;
       }
     }
+    if (count >= 3) {
+      return true;
+    }
   }
-  if (count >= 3) {
+}
+
+function checkError(numbers) {
+  switch (true) {
+  case checkLength(numbers):
+    return 'Array com tamanho incorreto.';
+  case checkNumberRange(numbers) || checkRepeatThreeTimes(numbers):
     return 'não é possível gerar um número de telefone com esses valores';
+  default:
+    return '';
   }
+}
+
+function phoneNumberStandard(numbers) {
+  let phoneNumber = '(';
+  for (let index = 0; index < 2; index += 1) {
+    /** Source: https://www.w3schools.com/jsref/jsref_tostring_number.asp */
+    phoneNumber += numbers[index].toString();
+  }
+  phoneNumber += ') ';
+  for (let index = 2; index < 7; index += 1) {
+    phoneNumber += numbers[index].toString();
+  }
+  phoneNumber += '-';
+  for (let index = 7; index < 11; index += 1) {
+    phoneNumber += numbers[index].toString();
+  }
+
+  return phoneNumber;
 }
 
 function generatePhoneNumber(numbers) {
   let errorInfo = '';
-
-  errorInfo = checkLength(numbers);
-  errorInfo = checkNumberRange(numbers);
-  errorInfo = checkRepeatThreeTimes(numbers);
+  let finalMessage = '';
+  errorInfo = checkError(numbers);
+  if (errorInfo === '') {
+    finalMessage = phoneNumberStandard(numbers);
+  } else {
+    finalMessage = errorInfo;
+  }
+  return finalMessage;
 }
 
 // Desafio 12
