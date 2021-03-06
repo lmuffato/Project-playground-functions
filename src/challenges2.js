e8e628a94011848d7eeac3ce31425581af5553ec
Alexandre Oliveira <alexandremolive@gmail.com>
2021-03-06 13:28:04 -0300

    ajustes de correcao exercicio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -20,12 +20,18 @@ let secondPart = '';
 let thirdPart = '';
 let numberAdjusted = ''
 let allNumber = ''
+
 function verifyArrayLength(arrayNumbers) {
   for (let number in arrayNumbers) {
-    if (arrayNumbers[number] < 0) {
-      return 'Array com tamanho incorreto.';
-    } else if (arrayNumbers[number] > 9) {
-      return 'Array com tamanho incorreto.';
+    if (arrayNumbers[number] < 0 || arrayNumbers[number] > 9) {
+      return false
+    }
+  } return true
+}
+function verifyArrayIndex(arrayNumbers) {
+  for (let index = 0; index < arrayNumbers.length; index += 1) {
+    if (arrayNumbers.length != 11) {
+      return false
     }
   } return true
 }
@@ -35,24 +41,27 @@ function verifyRepeatedNumber(arrayNumbers) {
     for (let secondIndex = 0; secondIndex < arrayNumbers.length; secondIndex += 1) {
       if (arrayNumbers[index] === arrayNumbers[secondIndex]) {
         numberRepeat += 1;
-      } if (arrayNumbers.length != 11) {
-        return 'não é possível gerar um número de telefone com esses valores';
       } if ((numberRepeat >= 3)) {
-        return 'não é possível gerar um número de telefone com esses valores';
+        return false
       }
     }
   } return true
 }
 function generatePhoneNumber(phoneNumber) {
-  if (verifyRepeatedNumber(phoneNumber) && verifyArrayLength(phoneNumber)) {
+  if (verifyRepeatedNumber(phoneNumber) === false) {
+    return console.log('não é possível gerar um número de telefone com esses valores')
+  } if (verifyArrayLength(phoneNumber) === false) {
+    return console.log('Array com tamanho incorreto.')
+  } if (verifyArrayIndex(phoneNumber) === false) {
+    return console.log('não é possível gerar um número de telefone com esses valores')
   }
   allNumber = phoneNumber.join('');
   firstPart = allNumber.slice(0, 2);
   secondPart = allNumber.slice(2, 7);
   thirdPart = allNumber.slice(7, 11);
   numberAdjusted = `(${firstPart}) ${secondPart}-${thirdPart}`;
-  return numberAdjusted
-}
+  return console.log(numberAdjusted)
+} generatePhoneNumber([2, 4, 5, 6, 5, 8, 9, 3, 4, 8, 1])
 // Desafio 12
 function triangleCheck() {
   // seu código aqui
