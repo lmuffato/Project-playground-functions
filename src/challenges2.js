ad9532f3a7b599c40ec8059bab10b59104078c26
ThalitaC <thalitaceci@gmail.com>
2021-03-06 20:03:34 -0300

    Requisito 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,16 @@
-// Desafio 10
-function techList() {
-  // seu código aqui
+// Desafio 10 //used this documentation about sort method https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+function techList(TechNameArray, userName) {
+  let items = [];
+  for (let index = 0; index < TechNameArray.sort().length; index += 1) {
+    items[index] = {
+      tech: TechNameArray[index],
+      name: userName,
+    };
+  }
+  return items;
 }
+
+console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas'));
 
 // Desafio 11
 function generatePhoneNumber() {
