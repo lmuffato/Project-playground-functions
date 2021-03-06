d4ea10f363f4ae0b5639cf7e395270bb3e1f86c0
johnpligeiro <johnpligeiro@gmail.com>
2021-03-06 13:05:55 -0300

    Apanhando do requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -15,14 +15,32 @@ function techList(techs, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+
+function wrongRangeCheck(currentValue, index, arr) {
+  if (currentValue > 9 || currentValue < 0) {
+    return "não é possível gerar um número de telefone com esses valores"
+  }
 }
+
+function generatePhoneNumber(phoneNumb) {
+
+  if (phoneNumb.length !== 11) {
+    return "Array com tamanho incorreto."
+  };
+
+  phoneNumb.some(wrongRangeCheck);
+
+
+  return `(${arr[0]}${arr[1]}) ${arr[2]}${arr[3]}${arr[4]}${arr[5]}${arr[6]}-${arr[7]}${arr[8]}${arr[9]}${arr[10]}`;
+}
+
+let aaa = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1];
+console.log(generatePhoneNumber(aaa));
 
 // Desafio 12
 function triangleCheck() {
   // seu código aqui
-}
+
 
 // Desafio 13
 function hydrate() {
