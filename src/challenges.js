4eecc65bb561bb761ecf3d59880344a30cfc39f6
igson <igson660@gmail.com>
2021-03-06 11:17:00 -0500

    conserta parte do lint no desafio 09

--- src/challenges.js
+++ src/challenges.js
@@ -73,13 +73,21 @@ function fizzBuzz() {
 }
 
 // Desafio 9
-function encode(phrase) {
-  let cript = phrase.replace(/a/gi, '1').replace(/e/gi, '2').replace(/i/gi, '3').replace(/o/gi, '4').replace(/u/gi, '5')
+function encode(encripta) {
+  let cript = encripta.replace(/a/gi, '1')
+    .replace(/e/gi, '2')
+    .replace(/i/gi, '3')
+    .replace(/o/gi, '4')
+    .replace(/u/gi, '5');
   return cript;
 }
 
-function decode(phrase2) {
-  let decript = phase.replace(/1/gi, 'a').replace(/2/gi, 'e').replace(/3/gi, 'i').replace(/4/gi, 'o').replace(/5/gi, 'u')
+function decode(decripta) {
+  let decript = decripta.replace(/1/gi, 'a')
+    .replace(/2/gi, 'e')
+    .replace(/3/gi, 'i')
+    .replace(/4/gi, 'o')
+    .replace(/5/gi, 'u');
   return decript;
 }
 
