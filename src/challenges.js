e605556324d4da6f52f4af3774f1e694ecb264a2
Maria Baeta <m.martinsdg@gmail.com>
2021-03-06 10:44:41 -0300

    Atende ao requisito 9

--- src/challenges.js
+++ src/challenges.js
@@ -61,7 +61,6 @@ function catAndMouse() {
 }
 
 // Desafio 8
-
 function fizzBuzz(numbers) {
   let modulo = [];
   for (let index = 0; index < numbers.length; index += 1) {
@@ -80,12 +79,58 @@ function fizzBuzz(numbers) {
 console.log(fizzBuzz([9, 25]));
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(string) {
+  let aux = '';
+  for (let index = 0; index < string.length; index += 1) {
+    switch (string[index]) {
+    case 'a':
+      aux += 1;
+      break;
+    case 'e':
+      aux += 2;
+      break;
+    case 'i':
+      aux += 3;
+      break;
+    case 'o':
+      aux += 4;
+      break;
+    case 'u':
+      aux += 5;
+      break;
+    default:
+      aux += string[index];
+    }
+  }
+  return aux;
 }
-function decode() {
-  // seu código aqui
+console.log(encode('Maria Baeta'));
+function decode(string) {
+  let letter = '';
+  for (let index = 0; index < string.length; index += 1) {
+    switch (string[index]) {
+    case '1':
+      letter += 'a';
+      break;
+    case '2':
+      letter += 'e';
+      break;
+    case '3':
+      letter += 'i';
+      break;
+    case '4':
+      letter += 'o';
+      break;
+    case '5':
+      letter += 'u';
+      break;
+    default:
+      letter += string[index];
+    }
+  }
+  return letter;
 }
+console.log(decode('M1r31 B12t1'));
 
 module.exports = {
   calcArea,
