b4aabf16510a4fa512db96896e0bcbeb5df0e0f4
madruga665 <madruga665@hotmail.com>
2021-03-06 16:39:48 -0300

    Desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -20,8 +20,37 @@ function techList(array, yourName) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function invalidNumbers(arrayNumbers) {
+  for (let number of arrayNumbers) {
+    if (number < 0 || number > 9) {
+      return true;
+    }
+  }
+}
+
+function repeatedNumbers(arrayNumbers) {
+  let counter = [];
+  for (let index of arrayNumbers) {
+    if (arrayNumbers[index] === index) {
+      counter.push(index);
+    }
+    if (counter.length >= 3) {
+      return true;
+    }
+  }
+}
+
+function generatePhoneNumber(phoneNumber) {
+  if (phoneNumber.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  if (invalidNumbers(phoneNumber) === true || repeatedNumbers(phoneNumber) === true) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+  return `(${phoneNumber[0]} ${phoneNumber[1]}) ${phoneNumber[2]} 
+  ${phoneNumber[3]} ${phoneNumber[4]} ${phoneNumber[5]} - 
+  ${phoneNumber[6]} ${phoneNumber[7]} ${phoneNumber[8]} 
+  ${phoneNumber[9]} ${phoneNumber[10]}`;
 }
 
 // Desafio 12
