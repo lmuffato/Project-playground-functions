0dc0a233cd1c39f825cab51b3669c2f65ab91700
Maria Luiza Castelar <mluiza.vcr@gmail.com>
2021-03-06 05:24:58 -0300

    Desafio 10: Hydrate

--- src/challenges2.js
+++ src/challenges2.js
@@ -42,31 +42,21 @@ console.log(triangleCheck(7, 20, 10));
 
 // Desafio 13
 function hydrate(string) {
-  let numbers = string.replace(/[^0-9]/g,''); // string vira só número (mas continua sendo string)
-  let numbersArray = [] //array para armazenar
-  let array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] // array para comparar
-  let soma = 0;
-  for(let i = 0; i < numbers.length; i++) {
-    for (let j = 0; j < array.length; j++) {
-      if (numbers[i] == array[j]) {
-        numbersArray.push(array[j])
-      }
-    }
+  let water = 0;
+  for (let i = 0; i < string.length; i += 1) {
+    if (string[i] > 0 && string[i] < 10)
+      water = water + parseInt(string[i]);
   }
-  for (let i = 0; i < numbersArray.length; i ++) {
-    soma = soma + numbersArray[i]
-    if (soma = 1) {
-      return soma + ' copo de água'
-    } else if (soma > 1) {
-      return soma + ' copos de água'
-    }
+  if (water === 1) {
+    return water + ' copo de água';
+  } else if (water > 1) {
+    return water + ' copos de água';
   }
 }
 
 console.log(hydrate('1 cerveja'));
 console.log(hydrate('1 cachaça, 5 cervejas e 1 copo de vinho'));
 console.log(hydrate('2 vodkas e 8 tequilas'));
-
 
 module.exports = {
   generatePhoneNumber,
