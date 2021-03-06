0d79ae7850ff3d4b004077031cc8c02495089734
Matheus Lima Pimenta <mateus.krauss@gmail.com>
2021-03-05 22:41:30 -0300

    envia projeto techlist

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,14 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
-}
+function techList(techName, humanName) {
+      if (techName.length < 0){
+        console.log('Vazio!');
+      }
+      let list = [];
+      for (let i = 0; i < techName.length; i += 1){
+        list.push([{name: humanName, tech: techName[i]}]);
+        }
+      return list;
+    }
 
 // Desafio 11
 function generatePhoneNumber() {
@@ -18,9 +25,13 @@ function hydrate() {
   // seu código aqui
 }
 
+console.log(techList(['react', 'jest', 'HTML', 'CSS', 'javascript'], 'Matheus'));
+
 module.exports = {
   generatePhoneNumber,
   techList,
   hydrate,
   triangleCheck,
 };
+
+
