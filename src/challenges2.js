7b026097be4ef21e7e58cbe27889b67e506ee9f4
madruga665 <madruga665@hotmail.com>
2021-03-06 16:44:37 -0300

    Desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -48,7 +48,7 @@ function generatePhoneNumber(phoneNumber) {
     return 'não é possível gerar um número de telefone com esses valores';
   }
   return `(${phoneNumber[0]} ${phoneNumber[1]}) ${phoneNumber[2]} 
-  ${phoneNumber[3]} ${phoneNumber[4]} ${phoneNumber[5]} - 
+  ${phoneNumber[3]} ${phoneNumber[4]} ${phoneNumber[5]}-
   ${phoneNumber[6]} ${phoneNumber[7]} ${phoneNumber[8]} 
   ${phoneNumber[9]} ${phoneNumber[10]}`;
 }
