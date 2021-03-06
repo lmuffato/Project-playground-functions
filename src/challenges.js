00d6d7a987c62ba032640d4e682e66fff17e1365
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-03-05 21:27:24 -0300

    Exercicio 4 .

--- src/challenges.js
+++ src/challenges.js
@@ -21,7 +21,7 @@ console.log(splitSentence('go Trybe'));
 // Desafio 4 ;
 
 function concatName(array) {
-  let string = `${array[array.length - 1]},${array[[0]]}`;
+  let string = `${array[array.length - 1]}, ${array[[0]]}`;
   return string;
 }
 console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
