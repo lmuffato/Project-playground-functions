785be26a44a534d53de7d1cf189e8d5d582dc98a
Jessica Arrais <jessica.fmarrais@gmail.com>
2021-03-06 15:53:37 -0300

    desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,8 +1,22 @@
 // Desafio 10
-function techList(technologies, name) {
+function techList(techArray, name) {
   let technologies = [];
-  
-}
+
+  tech = techArray.sort();
+  for(index = 0; index < techArray.length; index += 1) {
+    technologies.push({
+      tech: techArray[index],
+      name,
+    });
+    if(techArray.length === 0) {
+      return "Vazio!";
+    }
+  } 
+return technologies;  
+} 
+// let tech = ["React", "Jest", "HTML", "CSS", "JavaScript"]
+// let name = 'Lucas'
+// console.log(techList(tech, name));
 
 // Desafio 11
 function generatePhoneNumber() {
