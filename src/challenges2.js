85d9e49117068973618d47e0eac85c89bb5cedf0
Carlos Campo <carlosgabrielcampo@gmail.com>
2021-03-07 10:46:51 -0300

    challenge 10 new try

--- src/challenges2.js
+++ src/challenges2.js
@@ -3,27 +3,63 @@
 // Desafio 10
 function techList(tech, name) {
 
-  let result = [];
+  let techs = [];
+  techs.sort();
 
-if (tech.length === 5){
-  tech = tech.sort()
-} else if (tech.lenght > 0){
-  tech 
-} else {
+if (techs.length === 0){
   return 'Vazio!'
-} 
+}
 
 for (let indexTech = 0; indexTech < tech.length; indexTech += 1){
-    result.push("tech: " + tech[indexTech]) + result.push (" name: " + name)
+    techs.push(
+    { tech: tech[indexTech],   
+      name: name, 
+    });
   }
+
   return result
 }
 
-console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"],'carlos'))
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1];
+
+let telephoneNumber;
+
+let lowerNumber = Math.min.apply(Math,numbers)
+
+let repeatedNumber = 0;
+
+for (let repeatIndex = 0; repeatIndex < numbers.length; repeatIndex += 1){
+  for (let compareIndex = 0; compareIndex < numbers.length; compareIndex += 1){
+    if (numbers[repeatIndex] === numbers[compareIndex]){
+      repeatedNumber += 1;
+    } else {
+      repeatedNumber -= 1;
+    }
+      if (repeatedNumber <-8){
+        repeatedNumber === 0;
+    }
+  } 
+}
+
+console.log(repeatedNumber)
 
 
-// Desafio 11
-function generatePhoneNumber() {
+if (numbers.length > 11 || numbers.length < 11){
+  telephoneNumber = "Array com tamanho incorreto.";
+} else if (lowerNumber < 0 || repeatedNumber > 2){
+  telephoneNumber = "não é possível gerar um número de telefone com esses valores";
+}
+
+console.log(lowerNumber)
+
+
+
+function generatePhoneNumber(numbers) {
+
+
+
+
   // seu código aqui
 }
 
