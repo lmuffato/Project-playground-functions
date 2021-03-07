79579cdde85c9467ded11923d9e9a056675ffba3
Nathalia Zebral <natzebral@gmail.com>
2021-03-07 13:20:33 -0300

    tentativa  desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,19 @@
 // Desafio 10
-function techList(array,name) {
-//
+//  auxilio para resolução desse desafio foi revisão de codigos de colegas 
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Guide/Working_with_Objects
+function techList(technologies, studantsname) {
+  let arrayOfTechs = [];
+
+  for (let index = 0; index < technologies.length; index += 1) {
+    let newObject = { tech: technologies[index], name: studantsname };
+    arrayOfTechs.push(newObject);
+  }
+  
+  if (technologies.length === 0) {
+    arrayOfTechs = 'Vazio!';
+  }
+  
+  return arrayOfTechs;
 }
 
 // Desafio 11
