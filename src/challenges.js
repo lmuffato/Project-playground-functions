d2538f3f17acc268e436eeefc0394fc49f91ecd0
walvesb65 <willian.alvesb65@gmail.com>
2021-03-07 15:45:28 -0300

    corrigindo desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -49,14 +49,14 @@ function highestCount(maxArray) {
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
  if(Math.abs(mouse - cat2) < Math.abs(mouse - cat1)){
-   return cat2;
+   return 'cat2';
  }else if(Math.abs(mouse - cat1) < Math.abs(mouse - cat2)){
-  return cat1;
+  return 'cat1';
  }else{
-   return "os gatos trombam e o rato foge"
+   return 'os gatos trombam e o rato foge'
  }
  }
- 
+
 // Desafio 8
 function fizzBuzz() {
   // seu código aqui
