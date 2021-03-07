d5a8a3148e772753b2c3867e059a5eb0bcadd2ba
Carlos Campo <carlosgabrielcampo@gmail.com>
2021-03-07 13:04:36 -0300

    exercise 11 first try

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,4 @@
 
-
-// Desafio 10
 function techList(tech, name) {
 
   tech.sort();
@@ -20,49 +18,66 @@ for (let indexTech = 0; indexTech < tech.length; indexTech += 1){
   return techs
 }
 
-techList(["React", "Jest", "HTML", "CSS", "JavaScript"],"Lucas")
+
+numbers = [1, 2, 0, 4, 4, 0, 7, 8, 7, 9, 1];
 
 
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1];
-
-let telephoneNumber;
-
-let lowerNumber = Math.min.apply(Math,numbers)
-
-let repeatedNumber = 0;
-
-for (let repeatIndex = 0; repeatIndex < numbers.length; repeatIndex += 1){
-  for (let compareIndex = 0; compareIndex < numbers.length; compareIndex += 1){
-    if (numbers[repeatIndex] === numbers[compareIndex]){
-      repeatedNumber += 1;
-    } else {
-      repeatedNumber -= 1;
-    }
-      if (repeatedNumber <-8){
-        repeatedNumber === 0;
-    }
-  } 
-}
-
-console.log(repeatedNumber)
 
 
-if (numbers.length > 11 || numbers.length < 11){
-  telephoneNumber = "Array com tamanho incorreto.";
-} else if (lowerNumber < 0 || repeatedNumber > 2){
-  telephoneNumber = "não é possível gerar um número de telefone com esses valores";
-}
 
-console.log(lowerNumber)
+
+
+
 
 
 
 function generatePhoneNumber(numbers) {
 
+  let telephoneNumber;
 
-
-
-  // seu código aqui
+  let lowerNumber = Math.min.apply(Math,numbers)
+  
+  let repeatedNumber = 0;
+  
+  let comparedNumber = 0;
+  
+  
+  for (let repeatIndex = 0; repeatIndex < numbers.length; repeatIndex += 1){
+  
+    for (let compareIndex = 0; compareIndex < numbers.length; compareIndex += 1){
+  
+      if (numbers[repeatIndex] === numbers[compareIndex]){
+        repeatedNumber += 1;
+      } 
+  
+      if (repeatedNumber >= 3){
+        comparedNumber += 1
+      } 
+  
+     if (compareIndex === (numbers.length-1)){
+        repeatedNumber = 0;
+      }
+    } 
+  }
+  
+  if (numbers.length > 11 || numbers.length < 11){
+    telephoneNumber = "Array com tamanho incorreto.";
+  } else if (lowerNumber < 0 || comparedNumber > 2){
+    telephoneNumber = "não é possível gerar um número de telefone com esses valores";
+  } else {
+    telephoneNumber = "("
+    for (let indexTel = 0; indexTel < 2; indexTel += 1){
+    telephoneNumber += numbers[indexTel]
+    }
+    telephoneNumber += ") "
+    for (let indexTel = 2; indexTel < 7; indexTel += 1){
+      telephoneNumber += numbers[indexTel]
+      }
+    telephoneNumber += "-"
+  for (let indexTel = 7; indexTel < 11; indexTel += 1){
+    telephoneNumber += numbers[indexTel]
+      } 
+  }
 }
 
 // Desafio 12
