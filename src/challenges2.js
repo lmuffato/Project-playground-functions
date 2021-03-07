bf3d6fdad975d564ef028b43e3e828e60cec7f72
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-03-07 05:01:45 -0300

    Requisito 13: Bar da Trybe - Hidratacao

--- src/challenges2.js
+++ src/challenges2.js
@@ -14,9 +14,29 @@ function triangleCheck() {
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function encontraNumCopoAlcool(drink) {
+  let aux = drink.match(/\d+/g);
+  let contaCopo = 0;
+  for (let index = 0; index < aux.length; index += 1) {
+    contaCopo += parseInt(aux[index], 10);
+  }
+  return contaCopo;
 }
+
+function hydrate(drink) {
+  let coposDeAgua = 'copos de água';
+  let copoDeAgua = 'copo de água';
+  let recebeContagem;
+  recebeContagem = encontraNumCopoAlcool(drink);
+  if (recebeContagem === 1) {
+    return `${recebeContagem} ${copoDeAgua}`;
+  }
+  if (recebeContagem > 1) {
+    return `${recebeContagem} ${coposDeAgua}`;
+  }
+}
+
+console.log(hydrate('1 cachaça, 5 cervejas e 1 copo de vinho'));
 
 module.exports = {
   generatePhoneNumber,
