1a87d4320ad4a4512fbf134589c8254e16fa0e15
diegoCorreia <diegomethal@gmail.com>
2021-03-07 18:13:48 -0300

    requisito 9 identaçao

--- src/challenges.js
+++ src/challenges.js
@@ -92,20 +92,20 @@ console.log(fizzBuzz([2, 15, 7, 9, 45]));
 // Desafio 9
 function encode(arrayPhrase) {
   let vowels = {
-  a : 1,
-  e : 2,
-  i : 3,
-  o : 4,
-  u : 5,
+    a: 1,
+    e: 2,
+    i: 3,
+    o: 4,
+    u: 5,
   };
 
   let stringWords = '';
   for (let index = 0; index < arrayPhrase.length; index += 1) {
     if (vowels[arrayPhrase[index].toLowerCase()]) {
-    stringWords += vowels[arrayPhrase[index]];
-      } else {
-    stringWords += arrayPhrase[index];
-      }
+      stringWords += vowels[arrayPhrase[index]];
+    } else {
+      stringWords += arrayPhrase[index];
+    }
   }
   return stringWords;
 }
@@ -113,19 +113,19 @@ console.log(encode('hi there!'));
 
 function decode(arrayPhrase) {
   let vowels = {
-  1 : 'a',
-  2 : 'e',
-  3 : 'i',
-  4 : 'o',
-  5 : 'u',
+    1: 'a',
+    2: 'e',
+    3: 'i',
+    4: 'o',
+    5: 'u',
   };
 
   let stringWords = '';
   for (let index = 0; index < arrayPhrase.length; index += 1) {
     if (vowels[arrayPhrase[index].toLowerCase()]) {
-    stringWords += vowels[arrayPhrase[index]];
+      stringWords += vowels[arrayPhrase[index]];
     } else {
-    stringWords += arrayPhrase[index];
+      stringWords += arrayPhrase[index];
     }
   }
   return stringWords;
