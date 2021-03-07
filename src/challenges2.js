851021b25c0828d3487350cfb7aa989fcdcd5f70
luanlsr <luanfswd@gmail.com>
2021-03-07 01:40:50 -0300

    Requisito terminado com ajuda da Elisa França

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,8 +1,23 @@
 // Desafio 10
+//Este código fiz com ajuda da Elisa França.
+// Usei também o W3School para aprender sobre o sort.
 function techList(tech, name) {
-
+  if(tech.length === 0) {
+    return 'Vazio!';
+  }
+  let tecno = tech.sort();
+  let techSortList = [];
+  for (let index = 0; index < tecno.length; index += 1) {
+    techSortList.push(
+      {
+        tech: tecno[index],
+        name,
+      }
+    );
+  }
+  return techSortList;
 }
-
+console.log(techList([]));
 // Desafio 11
 function generatePhoneNumber() {
   // seu código aqui
