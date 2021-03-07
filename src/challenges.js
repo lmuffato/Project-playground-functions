a9120d84ed26b9961ac1f9c1e8e112d77ef7d8e8
lucas-chave <lcchavesr150@gmail.com>
2021-03-07 17:20:58 -0300

    adicionando o requisito sete

--- src/challenges.js
+++ src/challenges.js
@@ -42,10 +42,16 @@ function highestCount(elementArr) {
 }
 
 // Desafio 7
-function catAndMouse() {
-// asdasdasd
+function catAndMouse(mouse, cat1, cat2) {
+  if (cat1 - mouse < cat2 - mouse) {
+    return 'cat1';
+  } else if (cat2 - mouse < cat1 - mouse) {
+    return 'cat2';
+  } else if (cat1 === cat2) {
+    return 'os gatos trombam e o rato foge';
+  }
 }
-
+console.log(catAndMouse(10, 12, 6))
 // Desafio 8
 function fizzBuzz() {
   // seu código aqui
