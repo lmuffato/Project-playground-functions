49aa472c8e009626aa3c61682a9bec3a41fbb24f
Rodrigo Rech <rodrigorech92@gmail.com>
2021-03-07 09:36:56 -0300

    Problem 11 trying to fix it

--- src/challenges2.js
+++ src/challenges2.js
@@ -10,8 +10,7 @@ function techList(array, name) {
   for (let index = 0; index < arraySorted.length; index += 1) {
     objectArray.push({
       tech: array[index],
-      name
-    });
+      name });
   }
   return objectArray;
 }
@@ -35,10 +34,10 @@ function numbersRepetitionVerificator(array) {
       }
     }
     if (sum >= 3) {
-      return true;
+      return false;
     }
   }
-  return false;
+  return true;
 }
 
 function generatePhoneNumber(array) {
@@ -52,14 +51,14 @@ function generatePhoneNumber(array) {
     }
     phoneNumber = phoneNumber.substring(0, 0) + '(' + phoneNumber.substring(0, phoneNumber.length);
     phoneNumber = phoneNumber.substring(0, 3) + ') ' + phoneNumber.substring(3, phoneNumber.length);
-    phoneNumber = phoneNumber.substring(0, 10) + '-'+ phoneNumber.substring(10, phoneNumber.length);
+    phoneNumber = phoneNumber.substring(0, 10) + '-' + phoneNumber.substring(10, phoneNumber.length);
 
     return phoneNumber;
   }
   return 'Array com tamanho incorreto.';
 }
 
-console.log(generatePhoneNumber([1, 2, 3, 3, 3, 6, 7, 8, 9, 0, 1]));
+console.log(generatePhoneNumber([0, 2, 3, 4, 4, 2, 7, 8, 9, 9, 4]));
 
 // Desafio 12
 function triangleCheck() {
