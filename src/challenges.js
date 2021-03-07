b42efdc69443582ed612f267a94766ed542b5e06
carlos25xrm <carlos25xrm@gmail.com>
2021-03-07 16:25:52 -0300

    Refazendo(7) o Desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -50,9 +50,7 @@ function catAndMouse(mouse, cat1, cat2) {
   if (cat2 < cat1) {
     return 'cat2';
   }
-  if (cat1 == cat2) {
-    return 'os gatos trombam e o rato foge';
-  }
+  return 'os gatos trombam e o rato foge';
 }
 
 // Desafio 8
