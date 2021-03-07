d175586a1555c826fc5356c7ba9f40f3fb838239
ThalitaC <thalitaceci@gmail.com>
2021-03-07 17:31:37 -0300

    Requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -12,11 +12,37 @@ function techList(techNameArray, userName) {
   }
   return items;
 }
-console.log(techList([], 'Lucas'));
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function checkRepeat(arrNum) {
+  let count = 0;
+  for (let index = 0; index < arrNum.length; index += 1) {
+    if (arrNum[index] === arrNum[0]) {
+      count += 1;
+    }
+  }
+  return count;
+}
+
+function checkConditions(arrNum) {
+  for (let index = 0; index < arrNum.length; index += 1) {
+    if (arrNum[index] < 0 || arrNum[index] > 9) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function generatePhoneNumber(arrNum) {
+  if (checkConditions(arrNum) === true || checkRepeat(arrNum) >= 3) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+  if (arrNum.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  return `(${arrNum[0]}${arrNum[1]})${arrNum[2]}${arrNum[3]}\
+${arrNum[4]}${arrNum[5]}${arrNum[6]}-${arrNum[7]}${arrNum[8]}\
+${arrNum[9]}${arrNum[10]}`;
 }
 
 // Desafio 12
