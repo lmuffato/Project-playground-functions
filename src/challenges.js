1f5cb561df0557c443aa2d8e3a42043bc9f3c9b6
Tiago Santos <tiago881@gmail.com>
2021-03-07 18:49:57 -0300

    6 - Crie uma função que calcule a repetição do maior número

--- src/challenges.js
+++ src/challenges.js
@@ -34,8 +34,22 @@ function footballPoints(wins, ties) {
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestNumber(listNumbers) {
+  let number = Math.max.apply(null, listNumbers);
+  return number;
+}
+highestNumber([9, 1, 2, 3, 9, 5, 7]);
+function highestCount(list) {
+  let num = highestNumber(list);
+  console.log(highestNumber(list));
+  let count = 0;
+
+  for (let key in list) {
+    if (list[key] === num) {
+      count += 1;
+    }
+  }
+  return count;
 }
 
 // Desafio 7
