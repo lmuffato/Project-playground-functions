ed1de60a929aef0240b148209c2edd7fa2e0fb30
ScriptCamilo <rodrigo.camilo228@gmail.com>
2021-03-07 13:40:05 -0300

    Bonus: Eleventh requirement done

--- src/challenges2.js
+++ src/challenges2.js
@@ -18,8 +18,64 @@ function techList(techArray, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function repeatedCount(number, numbersArray, error) {
+  const { bool } = error;
+  let count = 0;
+
+  if (bool) {
+    return error;
+  }
+  for (let index = 0; index < numbersArray.length; index += 1) {
+    if (number === numbersArray[index]) {
+      count += 1;
+    }
+  }
+  if (count >= 3) {
+    return {
+      bool: true,
+      msg: 'não é possível gerar um número de telefone com esses valores',
+    };
+  }
+  return { bool: false, msg: '' };
+}
+
+function phoneNumberError(numbersArray) {
+  let error = { bool: null, msg: null };
+  if (numbersArray.length !== 11) {
+    error.bool = true;
+    error.msg = 'Array com tamanho incorreto';
+    return error;
+  }
+
+  for (let index = 0; index < numbersArray.length; index += 1) {
+    if (numbersArray[index] < 0 || numbersArray[index] > 9) {
+      error.bool = true;
+      error.msg = 'não é possível gerar um número de telefone com esses valores';
+      return error;
+    }
+    error = repeatedCount(numbersArray[index], numbersArray, error);
+  }
+  return error;
+}
+
+function generatePhoneNumber(numbersArray) {
+  const { bool, msg } = phoneNumberError(numbersArray);
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator
+  if (bool) return msg;
+
+  let ddd = '';
+  let number = '';
+  for (let index = 0; index < numbersArray.length; index += 1) {
+    if (index < 2) {
+      ddd += numbersArray[index];
+    } else {
+      number += numbersArray[index];
+    }
+  }
+  // https://www.w3schools.com/jsref/jsref_substring.asp
+  let firstPart = number.substring(0, 5);
+  let secondPart = number.substring(5);
+  return `(${ddd})${firstPart}-${secondPart}`;
 }
 
 // Desafio 12
