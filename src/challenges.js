6b73ef4061282608937c3d21a5223fc785827a38
root <root@DESKTOP-74CRLKE.localdomain>
2021-03-06 23:28:03 -0300

    requisito 8: modificação

--- src/challenges.js
+++ src/challenges.js
@@ -75,12 +75,12 @@ function catAndMouse(Mouse, cat1, cat2) {
 function fizzBuzz(array) {
   // seu código aqui
   let result = [];
-  for (let index = 0; index < array.length; index += 1) {
-    if (array[index] % 5 === 0 && array[index] % 3 === 0) {
+  for (let index of array) {
+    if (index % 5 === 0 && index % 3 === 0) {
       result.push('fizzbuzz');
-    } else if (array[index] % 3 === 0) {
+    } else if (index % 3 === 0) {
       result.push('fizz');
-    } else if (array[index] % 5 === 0) {
+    } else if (index % 5 === 0) {
       result.push('buzz');
     } else {
       result.push('bug');
@@ -88,7 +88,6 @@ function fizzBuzz(array) {
   }
   return result;
 }
-
 // Desafio 9
 function encode() {
   // seu código aqui
