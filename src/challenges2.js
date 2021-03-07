4990cd57c1cf05d04d2f79746aa14addb0a42dcd
Rafael Ribeiro <rafittu@outlook.com>
2021-03-07 14:52:51 -0300

    finalizado desafio 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,5 +1,5 @@
 // Desafio 10
-function techList() {
+function techList(array, name) {
   // seu código aqui
 }
 
@@ -9,9 +9,18 @@ function generatePhoneNumber() {
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  if (lineA < (lineB + lineC) && lineA > Math.abs(lineB - lineC)) {
+    return 'true';
+  } else if (lineB < (lineA + lineC) && lineB > Math.abs(lineA - lineC)) {
+    return 'true';
+  } else if (lineC < (lineA + lineB) && lineC > Math.abs(lineA - lineB)) {
+    return 'true';
+  } else {
+    return 'false';
+  }
 }
+console.log(triangleCheck(10, 15, 30));
 
 // Desafio 13
 function hydrate() {
