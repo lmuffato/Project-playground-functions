4f7efaf9a827fd8da09a1ba46976441bd8686267
Felipe Muller <felipeavmuller@gmail.com>
2021-03-06 23:01:42 -0300

    finalização do exercicio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -16,43 +16,52 @@ function techList(array, name) {
 }
 
 // Desafio 11
-// function lessZeroMoreNine(array) {
-//   for (let index = 0; index < array.length; index += 1) {
-//     if (array[index] < 0 || array[index] > 9) {
-//       return true;
-//     }
-//   }
-//   return false;
-// }
-// function doesItRepeat(array) {
-//   let counter = 0;
-//   for (let index = 0; index < array.length; index += 1) {
-//     for (let jndex = 0; jndex < array.length; jndex += 1) {
-//       if (array[index] === array[jndex]) {
-//         counter += 1;
-//       }
-//     }
-//     if (counter >= 3) {
-//       return true;
-//     }
-//   }
-//   return false;
-// }
-// function generatePhoneNumber(array) {
-//   let aux = '';
-//   aux += `(${array[0]}${array[1]}) `;
-//   for (let index = 2; index < array.length; index += 1) {
-//     aux += array[index];
-//     if (index === 6) {
-//       aux += '-';
-//     }
-//   }
-//   if (lessZeroMoreNine(array) === true || doesItRepeat(array) === true) {
-//     return 'não é possível gerar um número de telefone com esses valores';
-//   }
-//   return aux;
-// }
-// console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
+function lessZeroMoreNine(array) {
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] < 0 || array[index] > 9) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function tooBigOrTooSmall(array) {
+  if (array.length !== 11) {
+    return true;
+  }
+  return false;
+}
+function conta(array, value) {
+  let counter = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] === value) {
+      counter += 1;
+    }
+  }
+  return counter;
+}
+function doesItRepeat(array) {
+  for (let index = 0; index < array.length; index += 1) {
+    let counter = conta(array, array[index]);
+    if (counter >= 3) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function generatePhoneNumber(array) {
+  let aux = `(${array[0]}${array[1]}) ${array[2]}${array[3]}${array[4]}${array[5]}${array[6]}`;
+  aux += `-${array[7]}${array[8]}${array[9]}${array[10]}`;
+  if (tooBigOrTooSmall(array) === true) {
+    return 'Array com tamanho incorreto.';
+  }
+  if (lessZeroMoreNine(array) === true || doesItRepeat(array) === true) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+  return aux;
+}
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
 function triangleCheck() {
