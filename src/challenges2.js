f5bf8b4ed3bc829334d93cf5ff9bd7f94ca596da
Gabriel Ferreira <glucas709@gmail.com>
2021-03-06 21:03:04 -0300

    Requisito 11 - generatePhoneNumber()

--- src/challenges2.js
+++ src/challenges2.js
@@ -6,11 +6,11 @@ function techList(techName, name) {
   techNameOrdened = techName.length > 0 ? techName.sort() : '';
 
   for (let i = 0; i < techNameOrdened.length; i += 1) {
-    let techListObjetc = {
+    let techListObietc = {
       tech: techNameOrdened[i],
     };
-    techListObjetc.name = name;
-    techListArray.push(techListObjetc);
+    techListObietc.name = name;
+    techListArray.push(techListObietc);
   }
   if (techName.length > 0) {
     return techListArray;
@@ -19,9 +19,83 @@ function techList(techName, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+// eslint-disable-next-line sonarjs/cognitive-complexity
+function numberRepetition(arrayNumbers) {
+  let repeatedNumber = 0;
+  for (let i = 0; i < arrayNumbers.length; i += 1) {
+    repeatedNumber = 0;
+    for (let j = 0; j < arrayNumbers.length; j += 1) {
+      if (arrayNumbers[i] === arrayNumbers[j]) {
+        repeatedNumber += 1;
+      }
+    }
+    if (repeatedNumber >= 3) {
+      break;
+    }
+  }
+  return repeatedNumber;
 }
+
+function numberGreaterLimit(arrayNumbers) {
+  let biggerNumber = 0;
+  for (let i = 0; i < arrayNumbers.length; i += 1) {
+    if (arrayNumbers[i] > 9) {
+      biggerNumber = arrayNumbers[i];
+    }
+  }
+  return biggerNumber;
+}
+
+function generateDdd(arrayNumbers) {
+  let ddd = '';
+  for (let i = 0; i < 1; i += 1) {
+    let num1 = arrayNumbers[i];
+    let num2 = arrayNumbers[i + 1];
+    ddd = `${num1}${num2}`;
+  }
+  return ddd;
+}
+
+function generateNumber(arrayNumbers) {
+  let numberPhone1 = '';
+  let numberPhone2 = '';
+  for (let i = 2; i < 7; i += 1) {
+    numberPhone1 = `${numberPhone1}${arrayNumbers[i]}`;
+  }
+  for (let j = 7; j < 11; j += 1) {
+    numberPhone2 = `${numberPhone2}${arrayNumbers[j]}`;
+  }
+  return `${numberPhone1}-${numberPhone2}`;
+}
+
+function lessThanZero(arrayNumbers) {
+  let number = 0;
+  for (let i of arrayNumbers) {
+    if (i < 0) {
+      number = i;
+    }
+  }
+  return number;
+}
+
+function generatePhoneNumber(arrayNumbers) {
+  let numberTel = '';
+
+  if (arrayNumbers.length > 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  if (numberRepetition(arrayNumbers) >= 3 || lessThanZero(arrayNumbers) < 0) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+  if (numberGreaterLimit(arrayNumbers) > 9) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+
+  numberTel = `(${generateDdd(arrayNumbers)}) ${generateNumber(arrayNumbers)}`;
+
+  return numberTel;
+}
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
 function triangleCheck() {
