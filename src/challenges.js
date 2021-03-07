18fb393d13fc189ddaaa23128b32e234181878f6
Gui-lira <gui.lira.martins@gmail.com>
2021-03-07 13:11:54 -0300

    bonus 11

--- src/challenges.js
+++ src/challenges.js
@@ -2,10 +2,10 @@
 function compareTrue(a, b) {
   return a === true && b === true;
 }
- 
+
 // Desafio 2
 function calcArea(base, height) {
-  return (base * height) / 2; 
+  return (base * height) / 2;
 }
 
 // Desafio 3
@@ -15,7 +15,7 @@ function splitSentence(str) {
 
 // Desafio 4
 function concatName(array) {
-  return array[array.length - 1] + ', ' + array[0];
+  return (array[array.length - 1] + ', ' + array[0]);
 }
 
 // Desafio 5
@@ -25,21 +25,23 @@ function footballPoints(wins, ties) {
 
 // Desafio 6
 function highestCount(array) {
-  let greatest = array[0];
-  let times = 0;
-  for (let index in array)
-  { 
-    if (array[index] > greatest)
-  { 
-    greatest = array[index]; 
+  let counter = 0;
+  function greatest(array1) {
+    let greatestN = array1[0];
+    for (let index in array1) {
+      if (array1[index] > greatestN) {
+        greatestN = array1[index];
+      }
+    }
+    return greatestN;
   }
-}
-  for (let index in array)
-  { 
-    if (array[index] === greatest)
-{ times += 1; }
-}
-  return times;
+  let numberG = greatest(array)
+  for (let index in array) {
+    if (array[index] === numberG) {
+      counter += 1;
+    }
+  }
+  return counter;
 }
 
 // Desafio 7
@@ -47,19 +49,16 @@ function catAndMouse(mouse, cat1, cat2) {
   let distance1 = cat1 - mouse;
   let distance2 = cat2 - mouse
   if (distance1 < 0)
-  { 
-    distance1 = distance1 * -1 
+  { distance1 = distance1 * -1 
   }
   if (distance2 < 0)
   { 
     distance2 = distance2 * -1 };
-  if ( distance2 === distance1)
-  {
-    return 'os gatos trombam e o rato foge'
+  if (distance2 === distance1)
+  { return 'os gatos trombam e o rato foge'
   }
   else if (distance1 < distance2)
-  {
-    return 'cat1';
+  {return 'cat1';
   }
   else {
     return 'cat2';
--- src/challenges2.js
+++ src/challenges2.js
@@ -3,10 +3,10 @@ function techList(array, name) {
   // esse codigo foi baseado por essa discussão no stack overflow https://stackoverflow.com/questions/5868850/creating-list-of-objects-in-javascript
   array.sort();
   let list = [];
-  if (array.length === 0)
-  { 
+  if (array.length === 0) {
     return 'Vazio!';
   }
+
   else {for (let index = 0;index < array.length;index += 1)
      {list.push({ tech: array[index], name: name })
     }
@@ -16,42 +16,44 @@ function techList(array, name) {
 
 // Desafio 11
 function generatePhoneNumber(list) {
-  let part1 = '';
-  let part2 = '';
-  let part3 = '';
+  function times(a,b) {
+    let counter = 0;
+    for (let index of a) {
+      if (index === b) {
+        counter += 1;
+      }
+    }
+    return counter;
+  }
+  for (let index of list) {
+    if (times(list, index) >= 3 || index > 9 || index < 0) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  if (list.length != 11) {
+    return 'Array com tamanho incorreto.'
+  }
+  else {
+    return '(' + list[0] + list[1] + ')' + ' ' + list[2] + list[3] + list[4] + list[5] + list[6] + ' - ' + list[7] + list[8] + list[9] + list[10]  
+  }
 }
+console.log(generatePhoneNumber([1,2,6,8,7,9,8,3,4,5,6]))
 
 // Desafio 12
 function triangleCheck(a, b, c) {
-  if (a < b + c && a > Math.abs(b - c))
-  {
-    return true;
-  }
-  else if (b < a + c && b > Math.abs(c - a))
-  {
-    return true;
-  }
-  else if (c < a + b && c > Math.abs(a - b))
-  {
-    return true;
-  }
-  else 
-  {
-    return false;
-  }
+  return (a < b + c && a > Math.abs(b - c));
 }
 
 // Desafio 13
 function hydrate(str) {
   let sum = 0;
   let macth = '123456789';
-  for (let index in str)
-  {
-    if (macth.includes(str[index]))
-    {sum += Number(str[index])}
-  };
-  if (sum === 1)
-  {
+  for (let index in str) {
+    if (macth.includes(str[index])) {
+      sum += Number(str[index]);
+    }
+  }
+  if (sum === 1) {
     return (sum+'') + ' copo de água'
   }
   else {
