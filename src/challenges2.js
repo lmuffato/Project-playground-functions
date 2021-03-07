3d072dd35034f313d884d374b72c47c7e6927db2
PollyanaOliveira <pollyana.deoliveir@gmail.com>
2021-03-07 16:55:51 -0400

    desafio 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -7,7 +7,7 @@ function techList(techName, name) {
   techName.sort(); // ordenar;
   for (let index = 0; index < techName.length; index += 1) {
     list.push({ tech: techName[index], name }); // empurrar dentro do array
-  } // name - propriedade do JS: https://www.w3schools.com/js/js_objects.asp;
+  } // name - propriedade do JS: https://www.w3schools.com/js/js_objects.asp
   return list;
 }
 console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas'));
@@ -18,9 +18,38 @@ function generatePhoneNumber() {
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+// function triangleCheck(lineA, lineB, lineC) {
+function sideOne(lineA, lineB, lineC) {
+  let side1 = false;
+  if ((lineA > (lineB - lineC)) && (lineA < Math.abs(lineB + lineC))) {
+    side1 = true;
+  }
+  return side1;
 }
+function sideTwo(lineA, lineB, lineC) {
+  let side2 = false;
+  if ((lineA > (lineB - lineC)) && (lineA < Math.abs(lineB + lineC))) {
+    side2 = true;
+  }
+  return side2;
+}
+function sideThree(lineA, lineB, lineC) {
+  let side3 = false;
+  if ((lineC > (lineA - lineB)) && (lineC < Math.abs(lineA + lineB))) {
+    side3 = true;
+  }
+  return side3;
+}
+function triangleCheck(lineA, lineB, lineC) {
+  let value1 = sideOne(lineA, lineB, lineC);
+  let value2 = sideTwo(lineA, lineB, lineC);
+  let value3 = sideThree(lineA, lineB, lineC);
+  if (value1 && value2 && value3) {
+    return true;
+  }
+  return false;
+}
+console.log(triangleCheck(10, 10, 15));
 
 // Desafio 13
 function hydrate() {
