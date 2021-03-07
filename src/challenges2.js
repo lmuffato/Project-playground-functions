a3972e19fdeb37d898d7bfd141b1aeab58388394
Rafael Ribeiro <rafittu@outlook.com>
2021-03-07 15:21:44 -0300

    feito desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,18 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(array, name) {
+  let value = [];
+  if (array.length === 0) {
+    return 'Vazio!';
+  }
+  value.sort();
+  for (let index = 0; index < array.lenght; index += 1) {
+    let student = {
+      tech: array[index],
+      name: name,
+    };
+    value[index] = student;
+  }
+  return value;
 }
 
 // Desafio 11
@@ -10,7 +22,11 @@ function generatePhoneNumber() {
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
-  if (lineA < (lineB + lineC) && lineA > Math.abs(lineB - lineC) || lineB < (lineA + lineC) && lineB > Math.abs(lineA - lineC) || lineC < (lineA + lineB) && lineC > Math.abs(lineA - lineB)) {
+  if (lineA < (lineB + lineC) && lineA > Math.abs(lineB - lineC)) {
+    return true;
+  } else if (lineB < (lineA + lineC) && lineB > Math.abs(lineA - lineC)) {
+    return true;
+  } else if (lineC < (lineA + lineB) && lineC > Math.abs(lineA - lineB)) {
     return true;
   } else {
     return false;
