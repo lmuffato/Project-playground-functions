62f6b0a62e2235ea1d3eebde1e8c84fca6ee805a
Robertson <robertson.nunes@gmail.com>
2021-03-07 11:08:59 -0300

    desafio 10 ok

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,16 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(tech, name) {
+  if (tech.length < 1) {
+    return 'Vazio!';
+  }
+  let objects = tech.sort();
+  let listTech = [];
+    for (let index = 0; index < objects.length; index += 1) {
+      listTech[index] = {
+        tech: objects[index],
+        name,
+      };
+    } return listTech;
 }
 
 // Desafio 11
