05585ee22297717e46b8f78563400a8918dcd26c
diegoCorreia <diegomethal@gmail.com>
2021-03-08 13:49:32 -0300

    requisito 11 refatoraçao de codigo

--- src/challenges2.js
+++ src/challenges2.js
@@ -31,7 +31,7 @@ function countPhoneNumber(arrayNumber) {
   for (let index in arrayNumber) {
     if (arrayNumber[index] === arrayNumber) {
       count += 1;
-      } else if (count >= 3) {
+    } else if (count >= 3) {
       return 'não é possível gerar um número de telefone com esses valores';
     }
   }
@@ -44,17 +44,17 @@ function generateNumber(arrayNumber) {
     }
   }
 }
-function allFunctionsPhone(conditions, createNumber) {
-  conditions = conditionsPhoneNumber + countPhoneNumber + generateNumber;
+function allFunctionsPhone(conditionsPhoneNumber, countPhoneNumber, generateNumber) {
+  conditionsPhoneNumber;
+  countPhoneNumber;
+  generateNumber;
 }
 function generatePhoneNumber(arrayPhone) {
   let phone = allFunctionsPhone;
-  if (generatePhoneNumber = phone) {
-    phone = `(${arrayPhone[0]}${arrayPhone[1]})`;
-    phone += `${arrayPhone[2]}${arrayPhone[3]}${arrayPhone[4]}${arrayPhone[5]}${arrayPhone[6]}`;
-    phone += `-${arrayPhone[7]}${arrayPhone[8]}${arrayPhone[9]}${arrayPhone[10]}`;
-  }
-  return phone;
+  phone = `(${arrayPhone[0]}${arrayPhone[1]})`;
+  phone += `${arrayPhone[2]}${arrayPhone[3]}${arrayPhone[4]}${arrayPhone[5]}${arrayPhone[6]}`;
+  phone += `-${arrayPhone[7]}${arrayPhone[8]}${arrayPhone[9]}${arrayPhone[10]}`;
+    return phone;
 }
 console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
