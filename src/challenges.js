b3d7483453636f32e71ef6d4d5ca3b42ae197df3
Edmilson Bernardo <ebedmilson48@gmail.com>
2021-03-08 01:41:12 -0300

    Corrige erros detectados pelo linter no challenges

--- src/challenges.js
+++ src/challenges.js
@@ -1,40 +1,39 @@
 // Desafio 1
 function compareTrue(x, y) {
-  if (typeof x == 'boolean' && typeof y == 'boolean') {
+  if (typeof x === 'boolean' && typeof y === 'boolean') {
     return x && y;
-  } else {
-    return false;
   }
+  return false;
 }
 
 // Desafio 2
 function calcArea(base, heigth) {
-  return base * heigth / 2
+  return (base * heigth) / 2;
 }
 
 // Desafio 3
 function splitSentence(phrase) {
-  let words = []
-  let path = 0
-  for (index = 0; index < phrase.length; index++) {
-    if (phrase[index] != " ") {
-      if (words[path] != undefined) {
-        words[path] = words[path] + phrase[index];
+  let words = [];
+  let path = 0;
+  for (let index = 0; index < phrase.length; index++) {
+    if (phrase[index] !== ' ') {
+      if (words[path] !== undefined) {
+        words[path] += phrase[index];
       } else {
         words[path] = phrase[index];
       }
     } else {
-      path++;
+      path += 1;
     }
   }
-  return words
+  return words;
 }
 
 // Desafio 4
 function concatName(names) {
   let lastName = names[names.length - 1];
   let firstName = names[0];
-  return lastName + ", " + firstName
+  return lastName + ', ' + firstName;
 }
 
 // Desafio 5
@@ -42,21 +41,19 @@ function footballPoints(wins, ties) {
   let pointsWins = wins * 3;
   let pointsTies = ties * 1;
   let totalPoints = pointsWins + pointsTies;
-  return totalPoints
+  return totalPoints;
 }
 
 // Desafio 6
 function highestCount(randomNumbers) {
   let larger = 0;
-  let count = 0
+  let count = 0;
   for (let index = 0; index < randomNumbers.length; index++) {
     if (randomNumbers[index] > larger) {
       larger = randomNumbers[index];
-    }
-  }
-  for (let i = 0; i < randomNumbers.length; i++) {
-    if (randomNumbers[i] == larger) {
-      count++;
+      count = 1;
+    } else if (randomNumbers[index] === larger) {
+      count += 1;
     }
   }
   return count;
@@ -74,7 +71,7 @@ function catAndMouse(mouse, cat1, cat2) {
   }
   if (distanceCat1 > distanceCat2) {
     return 'cat2';
-  } else if (distanceCat1 == distanceCat2) {
+  } else if (distanceCat1 === distanceCat2) {
     return 'os gatos trombam e o rato foge';
   } else {
     return 'cat1';
@@ -84,12 +81,12 @@ function catAndMouse(mouse, cat1, cat2) {
 // Desafio 8
 function fizzBuzz(numbersGroup) {
   let numbersGroupResult = [];
-  for (positionIndex = 0; positionIndex < numbersGroup.length; positionIndex++) {
-    if (numbersGroup[positionIndex] % 3 == 0 && numbersGroup[positionIndex] % 5 == 0) {
+  for (let positionIndex = 0; positionIndex < numbersGroup.length; positionIndex += 1) {
+    if (numbersGroup[positionIndex] % 3 === 0 && numbersGroup[positionIndex] % 5 === 0) {
       numbersGroupResult[positionIndex] = "fizzBuzz";
-    } else if (numbersGroup[positionIndex] % 3 == 0) {
+    } else if (numbersGroup[positionIndex] % 3 === 0) {
       numbersGroupResult[positionIndex] = "fizz";
-    } else if (numbersGroup[positionIndex] % 5 == 0) {
+    } else if (numbersGroup[positionIndex] % 5 === 0) {
       numbersGroupResult[positionIndex] = "buzz";
     } else {
       numbersGroupResult[positionIndex] = "bug!";
@@ -100,17 +97,17 @@ function fizzBuzz(numbersGroup) {
 
 // Desafio 9
 function encode(contents) {
-  let textEncoded = ''
-  for (indicator = 0; indicator < contents.length; indicator++) {
-    if (contents[indicator] == 'a') {
+  let textEncoded = '';
+  for (let indicator = 0; indicator < contents.length; indicator += 1) {
+    if (contents[indicator] === 'a') {
       textEncoded += '1';
-    } else if (contents[indicator] == 'e') {
+    } else if (contents[indicator] === 'e') {
       textEncoded += '2';
-    } else if (contents[indicator] == 'i') {
+    } else if (contents[indicator] === 'i') {
       textEncoded += '3';
-    } else if (contents[indicator] == 'o') {
+    } else if (contents[indicator] === 'o') {
       textEncoded += '4';
-    } else if (contents[indicator] == 'u') {
+    } else if (contents[indicator] === 'u') {
       textEncoded += '5';
     } else {
       textEncoded += contents[indicator];
@@ -120,17 +117,17 @@ function encode(contents) {
 }
 
 function decode(textEncoded) {
-  let textDecoded = ''
-  for (indicator = 0; indicator < textEncoded.length; indicator++) {
-    if (textEncoded[indicator] == '1') {
+  let textDecoded = '';
+  for (let indicator = 0; indicator < textEncoded.length; indicator += 1) {
+    if (textEncoded[indicator] === '1') {
       textDecoded += 'a';
-    } else if (textEncoded[indicator] == '2') {
+    } else if (textEncoded[indicator] === '2') {
       textDecoded += 'e';
-    } else if (textEncoded[indicator] == '3') {
+    } else if (textEncoded[indicator] === '3') {
       textDecoded += 'i';
-    } else if (textEncoded[indicator] == '4') {
+    } else if (textEncoded[indicator] === '4') {
       textDecoded += 'o';
-    } else if (textEncoded[indicator] == '5') {
+    } else if (textEncoded[indicator] === '5') {
       textDecoded += 'u';
     } else {
       textDecoded += textEncoded[indicator];
