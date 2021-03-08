6a2cc282acc478d6136492b099df8557721be95e
¨WesleyFernandoAlmeida¨ <wesley.fer.almeida98@gmail.com>
2021-03-08 18:35:22 -0300

    Resoluçao do requisito 7

--- src/challenges.js
+++ src/challenges.js
@@ -1,10 +1,10 @@
 // Desafio 1
 function compareTrue(value1, value2) {
   // seu código aqui.
-  if (value1 === true && value2 === true){
+  if (value1 === true && value2 === true) {
     return true;
   }
-    return false;
+  return false;
 }
 console.log(compareTrue(false, true));
 console.log(compareTrue(false, false));
@@ -17,8 +17,8 @@ function calcArea(base, height) {
   return count2;
 }
 console.log(calcArea(10, 50));
-console.log(calcArea(5,2));
-console.log(calcArea(51,1));
+console.log(calcArea(5, 2));
+console.log(calcArea(51, 1));
 
 // Desafio 3
 function splitSentence(sentence) {
@@ -32,9 +32,10 @@ console.log(splitSentence('foguete'));
 // Desafio 4
 function concatName(array) {
   // seu código aqui
- let lastItem = array.pop();
- let firstItem = array.shift();
- return lastItem + ', ' + firstItem;
+  let lastItem = array.pop();
+  let firstItem = array.shift();
+
+  return lastItem + ', ' + firstItem;
 }
 console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
 console.log(concatName(['foguete', 'não', 'tem', 'ré']));
@@ -54,26 +55,38 @@ console.log(footballPoints(0, 0));
 // Desafio 6
 function highestCount(set) {
   // seu código aqui
-   let higherNumber = Math.max(...set);
-   let quantity = 0
+  let higherNumber = Math.max(...set);
+  let quantity = 0
 
-   for (let index = 0; index < set.length; index ++) {
-     
-  if(higherNumber === set[index]) {
+  for (let index = 0; index < set.length; index++) {
+
+    if (higherNumber === set[index]) {
       quantity++
 
-  }  
- }
-return quantity;
+    }
+  }
+  return quantity;
 }
 
 console.log(highestCount([9, 1, 2, 3, 9, 5, 7]))
 console.log(highestCount([0, 4, 4, 4, 9, 2, 1]))
 console.log(highestCount([0, 0, 0]))
 // Desafio 7
-function catAndMouse() {
+function catAndMouse(mouse, cat1, cat2) {
   // seu código aqui
+  if (cat1 > cat2 && cat1 > mouse) {
+    return 'cat2'
+  }
+  else if (cat2 > cat1 && cat2 > mouse) {
+    return 'cat1'
+  }
+  else if (cat1 === cat2) {
+    return 'os gatos trombam e o rato foge'
+  }
 }
+console.log(catAndMouse(1, 4, 3));
+console.log(catAndMouse(1, 7, 13));
+console.log(catAndMouse(1, 2, 2));
 
 // Desafio 8
 function fizzBuzz() {
