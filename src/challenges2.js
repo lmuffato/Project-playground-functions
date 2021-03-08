326474243236fbba73e7c8f4120a6480d8efa06a
Gabriel Pereira <g.pereira.a.m@gmail.com>
2021-03-08 15:27:23 -0300

    Requisito 11 - ESlint (25)

--- src/challenges2.js
+++ src/challenges2.js
@@ -21,36 +21,43 @@ function techList(tecnologias, nome) {
 // Desafio 11
 // Exercício corrigido com a ajuda do código do Felipe Flores
 // Link: https://github.com/tryber/sd-010-a-project-playground-functions/blob/9cc66910b7a0b4bd1a816105407ad14e7594d1b1/src/challenges2.js
-function checkingBiggerAndSmaller(numbers) {
-  if (numbers <= 9 || numbers >= 0) {
-    return numbers;
-  }
-}
-
-function checkingRepetition(array, numbers) {
-  let contadorNumbers = 0;
-  for (let index = 0; index < array.length; index += 1) {
-    if (numbers === array[index]) {
-      contadorNumbers += 1;
+function checkingRepetition(numbers) {
+  for (let index = 0; index < numbers.length; index += 1) {
+    let contadorNumbers = 0;
+    for (let index2 = 0; index2 < numbers.length; index2 += 1) {
+      if (numbers[index] === numbers[index2]) {
+        contadorNumbers += 1;
+      }
+    }
+    if (contadorNumbers >= 3) {
+      return true;
     }
   }
-  return contadorNumbers;
+  return false;
+}
+
+function checkingBiggerAndSmaller(numbers) {
+  let evaluator = false;
+  for (let index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] < 0 || numbers[index] > 9) {
+      evaluator = true;
+    }
+  }
+  return evaluator;
 }
 
 function generatePhoneNumber(numbers) {
   // seu código aqui
   let numberFone = [];
+
   if (numbers.length !== 11) {
     return 'Array com tamanho incorreto.';
   }
-  for (let index = 0; index < numbers.length; index += 1) {
-    let numbersArray = checkingBiggerAndSmaller(numbers[index]);
-    let checkRepeat = checkingRepetition(numbers, numbersArray);
 
-    if (numbersArray === undefined || checkRepeat >= 3) {
-      return 'não é possível gerar um número de telefone com esses valores';
-    }
+  if (checkingBiggerAndSmaller(numbers) === true || checkingRepetition(numbers) === true) {
+    return 'não é possível gerar um número de telefone com esses valores';
   }
+
   numberFone = `${'('}${numbers[0]}${numbers[1]}${')'}`;
   numberFone += `${' '}${numbers[2]}${numbers[3]}${numbers[4]}${numbers[5]}${numbers[6]}`;
   numberFone += `${'-'}${numbers[7]}${numbers[8]}${numbers[9]}${numbers[10]}`;
