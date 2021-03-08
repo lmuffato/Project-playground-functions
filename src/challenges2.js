868461ee1c779391cd3a61c680d80d571e908b18
Gabriel L Ferreira <78182689+gabriellferreira-dev@users.noreply.github.com>
2021-03-08 10:55:45 -0300

    Code Review
    Co-authored-by: Gustavo Sousa <34840033+gustavofds@users.noreply.github.com>

--- src/challenges2.js
+++ src/challenges2.js
@@ -84,7 +84,7 @@ function generatePhoneNumber(arrayNumbers) {
   if (arrayNumbers.length !== 11) {
     return 'Array com tamanho incorreto.';
   }
-  if (numberRepetition(arrayNumbers) >= 3 || lessThanZero(arrayNumbers) < 0) {
+  if (numberRepetition(arrayNumbers) >= 3 || lessThanZero(arrayNumbers) < 0 || numberGreaterLimit(arrayNumbers) > 9) {
     return 'não é possível gerar um número de telefone com esses valores';
   }
   if (numberGreaterLimit(arrayNumbers) > 9) {
