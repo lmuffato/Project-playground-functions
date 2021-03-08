6b55cf06e8165d09113bfb67880df5069e98e32c
Debora Passos <deborinhapassos@gmail.com>
2021-03-08 11:42:45 -0300

    Exercicio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,17 +1,11 @@
 // Desafio 10
 function techList(techs, name) {
-
   let ret = [];
 
   for (let index = 0; index < techs.length; index += 1) {
-    let obj = {};
-
-    obj.tech = techs[index];
-    obj.name = name;
-
+    let obj = {tech: techs[index], name: name};
     ret.push(obj);
   }
-
   ret.sort(function (a, b) {
     if (a.tech > b.tech) {
       return 1;
@@ -22,20 +16,39 @@ function techList(techs, name) {
     return 0;
   });
 
-  if (techs.length === 0){
+  if (techs.length === 0) {
     return "Vazio!";
   } else {
     return ret;
   }
 }
-
 console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas"));
-console.log(techList([], "Teste"));
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
-}
+function generatePhoneNumber(numberPhone) {
+  let phone = [];
+  let res = '';
+
+  if (numberPhone.length !== 11) {
+    return "Array com tamanho incorreto."
+  }
+  for (index = 0; index < numberPhone.length; index += 1) { 
+    if (numberPhone[index] < 0 || numberPhone[index] > 9) {
+      return "não é possível gerar um número de telefone com esses valores"
+    }
+    var quantidadeElementos = numberPhone.filter(x => x === numberPhone[index]).length;
+    if (quantidadeElementos >= 3) {
+      return "não é possível gerar um número de telefone com esses valores"
+    }
+    res = res + numberPhone[index];
+  }
+   res = res.replace(/\D/g,"");             //Remove tudo o que não é dígito
+   res = res.replace(/^(\d{2})(\d)/g,"($1) $2"); //Coloca parênteses em volta dos dois primeiros dígitos
+   res = res.replace(/(\d)(\d{4})$/,"$1-$2");    //Coloca hífen entre o quarto e o quinto dígitos
+
+   return res;
+ };
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 3, 0, 1]));
 
 // Desafio 12
 function triangleCheck() {
