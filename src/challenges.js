e4805a83e2c131ff725bfeaf525336885997ccaf
¨WesleyFernandoAlmeida¨ <wesley.fer.almeida98@gmail.com>
2021-03-08 20:13:50 -0300

    Resolução do requisito 8

--- src/challenges.js
+++ src/challenges.js
@@ -68,31 +68,52 @@ function highestCount(set) {
   return quantity;
 }
 
-console.log(highestCount([9, 1, 2, 3, 9, 5, 7]))
-console.log(highestCount([0, 4, 4, 4, 9, 2, 1]))
-console.log(highestCount([0, 0, 0]))
+console.log(highestCount([9, 1, 2, 3, 9, 5, 7]));
+console.log(highestCount([0, 4, 4, 4, 9, 2, 1]));
+console.log(highestCount([0, 0, 0]));
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
   // seu código aqui
   if (cat1 > cat2 && cat1 > mouse) {
-    return 'cat2'
+    return 'cat2';
   }
   else if (cat2 > cat1 && cat2 > mouse) {
-    return 'cat1'
+    return 'cat1';
   }
   else if (cat1 === cat2) {
-    return 'os gatos trombam e o rato foge'
+    return 'os gatos trombam e o rato foge';
   }
 }
-console.log(catAndMouse(1, 4, 3));
-console.log(catAndMouse(1, 7, 13));
-console.log(catAndMouse(1, 2, 2));
+console.log(catAndMouse(0, 3, 2));
+console.log(catAndMouse(0, 6, 12));
+console.log(catAndMouse(0, 1, 1));
 
 // Desafio 8
-function fizzBuzz() {
+function fizzBuzz(arrays) {
   // seu código aqui
+  let retorno = [];
+
+  for (let index2 = 0; index2 < arrays.length; index2++) {
+
+    if (arrays[index2] % 5 === 0 && arrays[index2] % 3 === 0) {
+      retorno.push('fizzBuzz')
+    }
+    else if (arrays[index2] % 5 === 0) {
+      retorno.push('buzz')
+    }
+    else if (arrays[index2] % 3 === 0) {
+      retorno.push('fizz')
+    }
+    else {
+      retorno.push('bug!')
+    }
+  }
+  return retorno;
 }
 
+console.log(fizzBuzz([2, 15, 7, 9, 45]))
+console.log(fizzBuzz([7, 9]))
+console.log(fizzBuzz([9, 25]))
 // Desafio 9
 function encode() {
   // seu código aqui
