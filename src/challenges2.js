8460ec2b11bee7a8d38442431492991e9674d8c0
André Jaques Batista <andrejaques.b@gmail.com>
2021-03-08 13:03:19 -0300

    add require 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,22 @@
 // Desafio 10
-function techList(tech, name) {
-    // seu código aqui
+function createObject(name1, name2) {
+  let object = {
+    tech: name1,
+    name: name2,
+  };
+  return object;
+}
+
+function techList(techArray, name) {
+  if (techArray.length === 0) {
+    return 'Vazio!';
+  }
+  techArray.sort();
+  let objects = [];
+  for (let index = 0; index < techArray.length; index += 1) {
+    objects.push(createObject(techArray[index], name));
+  }
+  return objects
 }
 
 // Desafio 11
