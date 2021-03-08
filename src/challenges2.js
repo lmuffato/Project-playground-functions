a9a5ab5b7061b37c933d72e23b438f13a25a3e84
diegoCorreia <diegomethal@gmail.com>
2021-03-08 14:07:33 -0300

    requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -44,17 +44,16 @@ function generateNumber(arrayNumber) {
     }
   }
 }
-function allFunctionsPhone(conditionsPhoneNumber, countPhoneNumber, generateNumber) {
-  conditionsPhoneNumber;
-  countPhoneNumber;
-  generateNumber;
+function allFunctionsPhone(functionsPhone) {
+functionsPhone = conditionsPhoneNumber + countPhoneNumber + generateNumber;
 }
 function generatePhoneNumber(arrayPhone) {
-  let phone = allFunctionsPhone;
+  let functionsPhone = allFunctionsPhone;
+  let phone = functionsPhone;
   phone = `(${arrayPhone[0]}${arrayPhone[1]})`;
   phone += `${arrayPhone[2]}${arrayPhone[3]}${arrayPhone[4]}${arrayPhone[5]}${arrayPhone[6]}`;
   phone += `-${arrayPhone[7]}${arrayPhone[8]}${arrayPhone[9]}${arrayPhone[10]}`;
-    return phone;
+  return phone;
 }
 console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
