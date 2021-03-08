e5a35660370aaed755b6d04917f00cd65fb45b99
Maria Lisboa <marialisboa1515@gmail.com>
2021-03-07 23:01:00 -0300

    corrigindo erros linst

--- src/challenges.js
+++ src/challenges.js
@@ -1,9 +1,9 @@
 // Desafio 1
 function compareTrue(value1, value2) {
-  if(value1===true && value2===true) {
+  if (value1 === true && value2 === true) {
     return true;
   }
-  else{
+  else {
     return false;
   }
 }
@@ -12,31 +12,31 @@ console.log(compareTrue(true, false));
 // Desafio 2
 function calcArea(base, heigth) {
   return (base * heigth) / 2;
-  }
-console.log(calcArea(10, 50))
-console.log(calcArea(5, 2))
+}
+console.log(calcArea(10, 50));
+console.log(calcArea(5, 2));
 console.log(calcArea(51, 1));
 // Desafio 3
 function splitSentence(string) {
-let recebe = [];
-recebe = string.split(' ');
-return recebe ;
+  let recebe = [];
+  recebe = string.split(' ');
+  return recebe ;
 }
-console.log(splitSentence('go Trybe'))
-console.log(splitSentence('vamo que vamo'))
-console.log(splitSentence('foguete'))
+console.log(splitSentence('go Trybe')) ;
+console.log(splitSentence('vamo que vamo'));
+console.log(splitSentence('foguete'));
 // Desafio 4
 function concatName(arrayString) {
-let names = '';
-for(index = 0; index<arrayString.length; index+=1) {
-if(index == 0) {
-names =arrayString[arrayString.length-1] ;
-}
-if(index ==(arrayString.length-1)) {
-names = names + ' , ' + arrayString[0] ;
-}
-}
-return names ;
+  let names = '';
+  for(index = 0; index < arrayString.length; index+=1) {
+  if(index === 0) {
+      names = arrayString[arrayString.length - 1];
+    }
+  if (index == (arrayString.length - 1)) {
+      names = names + ' , ' + arrayString[0];
+  }
+  }
+  return names;
 }
 
 console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
@@ -48,51 +48,43 @@ console.log(concatName(['captain', 'my', 'captain']));
 function footballPoints(wins, ties) {
   return (wins * 3) + ties;
 }
-let pontos = footballPoints(14 , 8);
+let pontos = footballPoints(14, 8);
 console.log(pontos);
-console.log(footballPoints(1 , 2));
-console.log(footballPoints(0 , 0));
+console.log(footballPoints(1, 2));
+console.log(footballPoints(0, 0));
 
 // Desafio 6
 function highestCount(arrayNumbers) {
-  let maiorNumero = 0 ; 
-  let repete = 0 ;
-  for(let item of arrayNumbers){
+  let maiorNumero = 0; 
+  let repete = 0;
+  for (let item of arrayNumbers) {
    
-  if(item > maiorNumero){
-    maiorNumero = item ; 
-    repete = 1 ;
+  if (item > maiorNumero) {
+    maiorNumero = item; 
+      repete = 1 ;
 
-  }else if(item == maiorNumero){
-    repete = repete + 1 ;
+  }else if (item === maiorNumero) {
+    repete = repete + 1;
   }
- 
-
-  }
-  
-  return repete ;
- 
+} 
+  return repete; 
 }
 console.log(highestCount([3, 1, 2, 3, 9, 5, 9]));
-
-
 // Desafio 7
-function catAndMouse(cat1, cat2, mouse) {
-  
+function catAndMouse(cat1, cat2, mouse) { 
 }
-
 // Desafio 8
 function fizzBuzz(arrayNumber) {
-  let returnArray =[] ;
-  for(index=0; index<arrayNumber.length; index+=1){
-    if(arrayNumber[index] % 3 == 0 && arrayNumber[index] % 5 != 0){
-      returnArray.push('fizz') ;
+  let returnArray = [];
+  for (index=0; index<arrayNumber.length; index += 1){
+    if (arrayNumber[index] % 3 == 0 && arrayNumber[index] % 5 != 0){
+      returnArray.push('fizz');
     }else if (arrayNumber[index] % 5 == 0 && arrayNumber[index] % 3 != 0){
-     returnArray.push('buzz') ;
-    }else if(arrayNumber[index] % 5 == 0 && arrayNumber[index] % 3 == 0){
-       returnArray.push('fizzbuzz') ;
-    }else{
-      returnArray.push('bug')
+     returnArray.push('buzz');
+    }else if (arrayNumber[index] % 5 == 0 && arrayNumber[index] % 3 == 0){
+       returnArray.push('fizzBuzz');
+    }else {
+      returnArray.push('bug!');
     }
   }
   return returnArray;
@@ -100,7 +92,6 @@ function fizzBuzz(arrayNumber) {
 console.log(fizzBuzz([2, 15, 7, 9, 45]));
 console.log(fizzBuzz([7, 9]));
 console.log(fizzBuzz([9, 25]));
-
 // Desafio 9
 function encode() {
   // seu código aqui
