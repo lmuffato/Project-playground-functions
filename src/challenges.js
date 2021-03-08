6d6451a79af8986141dd7904945d953124f945e5
André Aguilar <andreaguilar.ti@gmail.com>
2021-03-08 12:10:47 -0300

    exercicio 7 pronto

--- src/challenges.js
+++ src/challenges.js
@@ -57,8 +57,17 @@ function highestCount(numeros) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let dist1 = Math.abs(mouse - cat1);
+  let dist2 = Math.abs(mouse - cat2);
+  // Até essa parte, peguei com os colegas, daqui para baixo eu mesmo fiz.
+  if (dist1 < dist2) {
+    return "cat1";
+  } if (dist2 < dist1) {
+    return "cat2";
+  } if (dist1 === dist2) {
+    return "os gatos trombam e o rato foge";
+  }
 }
 
 // Desafio 8
