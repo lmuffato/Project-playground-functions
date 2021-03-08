79eaeb2f444495b446d777253c99e22babef514e
Nathalia Zebral <natzebral@gmail.com>
2021-03-08 13:48:19 -0300

    alteracao no desafio 8 else if

--- src/challenges.js
+++ src/challenges.js
@@ -4,7 +4,6 @@ function compareTrue(value1, value2) {
     return true;
   }
   return false;
-
 }
 
 // Desafio 2
@@ -61,17 +60,15 @@ function catAndMouse(mouse, cat1, cat2) {
 }
 
 // Desafio 8
-let array = [2, 15, 7, 9, 45];
-function fizzBuzz() {
-  let arrayToReturn = [];
+//let array = [2, 15, 7, 9, 45];
+function fizzBuzz(array) {
+   let arrayToReturn = [];
   for (let index = 0; index < array.length; index += 1) {
     if (array[index] % 15 === 0) {
       arrayToReturn.push('fizzBuzz');
-    }
-    if (array[index] % 3 === 0) {
+    } else if (array[index] % 3 === 0) {
       arrayToReturn.push('fizz');
-    }
-    if (array[index] % 5 === 0) {
+    } else if (array[index] % 5 === 0) {
       arrayToReturn.push('buzz');
     } else {
       arrayToReturn.push('bug!');
@@ -79,20 +76,20 @@ function fizzBuzz() {
   }
   return arrayToReturn;
 }
+console.log(fizzBuzz([2, 15, 7, 9, 45]))
 // Desafio 9
 
 function encode(string) {
   string = string.replace(/a/g, 1).replace(/e/g, 2).replace(/i/g, 3)
-    .replace(/o/g, 4).replace(/u/g, 5);
+  .replace(/o/g, 4).replace(/u/g, 5);
   return string;
 }
 
 function decode(string) {
   string = string.replace(/1/g, 'a').replace(/2/g, 'e').replace(/3/g, 'i')
-    .replace(/4/g, 'o').replace(/5/g, 'u');
+  .replace(/4/g, 'o').replace(/5/g, 'u');
   return string;
 }
-
 
 module.exports = {
   calcArea,
