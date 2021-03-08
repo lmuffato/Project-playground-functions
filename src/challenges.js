d1fbedf6fbc4d497ac539e839ee19099b83d3780
Eicke Yumoto <eickeyumoto@gmail.com>
2021-03-08 12:04:48 -0300

    test

--- src/challenges.js
+++ src/challenges.js
@@ -1,37 +1,39 @@
 // Desafio 1
 function compareTrue(value1, value2) {
-  if (value1 = true && value2 = true) {
-    return true
-  } else { return false}
+  let result;
+  if (value1 === true && value2 === true) {
+    result = true;
+  } else { result = false }
+  return result;
 }
 
 // Desafio 2
 function calcArea(base, height) {
-  return base * height / 2
+  return (base * height) / 2;
 }
 
 // Desafio 3
 function splitSentence(string) {
-  return string.split(' ')
+  return string.split(' ');
 }
 
 // Desafio 4
 function concatName(strArray) {
-  return strArray[strArray.length - 1] + ', ' + strArray[0]
+  return strArray[strArray.length - 1] + ', ' + strArray[0];
 }
 
 // Desafio 5
 function footballPoints(wins, ties) {
-  wins = wins * 3
-  points = wins + ties
-  return points
+  let points;
+  wins = wins * 3;
+  points = wins + ties;
+  return points;
 }
 
 // Desafio 6
 function highestCount(numbers) {
-  let highestNumber = Math.max(numbers);
-  console.log(highestNumber)
   let count = 0
+  let highestNumber = Math.max(numbers);
   for(let index = 0; index < numbers.length; index += 1) {
     if (numbers[index] = highestNumber) {
       count += 1
@@ -57,11 +59,11 @@ function catAndMouse(mouse, cat1, cat2) {
 function fizzBuzz(numberArray) {
   let arrayFizzBuzz = []
   for(let i = 0; i < numberArray.length; i += 1) {
-    if (numberArray[i] % 3 === 0 && numberArray[i] % 5 === 0) {
+    if (numberArray[i] % 3 == 0 && numberArray[i] % 5 == 0) {
       arrayFizzBuzz.push('fizzbuzz')
-    } else if (numberArray[i] % 5 === 0) {
+    } else if (numberArray[i] % 5 == 0) {
       arrayFizzBuzz.push('buzz')
-    } else if (numberArray[i] % 3 === 0) {
+    } else if (numberArray[i] % 3 == 0) {
       arrayFizzBuzz.push('fizz')
     } else { arrayFizzBuzz.push('bug!') }
   }
@@ -71,32 +73,37 @@ function fizzBuzz(numberArray) {
 // Desafio 9
 function encode(encodeString) {
   encodeString.split('')
-  "a" = 1
-  "e" = 2
-  "i" = 3
-  "o" = 4
-  "u" = 5
-  
+  "a" == 1
+  "e" == 2
+  "i" == 3
+  "o" == 4
+  "u" == 5
+  encodeString.join('')
+  return encodeString
 }
-function decode(decodeString) {
   
-}
 
-console.log(compareTrue(true, false))
+function decode(decodeString) {}
+  
 
-console.log(calcArea(51, 1))
 
-console.log(splitSentence('vamo que vamo'))
+console.log(compareTrue(true, true));
 
-console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']))
+console.log(calcArea(51, 1));
 
-console.log(footballPoints(14, 8))
+console.log(splitSentence('vamo que vamo'));
 
-console.log(highestCount([1, 2, 3, 4]))
+console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
 
-console.log(catAndMouse(9, 1, 10))
+console.log(footballPoints(14, 8));
 
-console.log(fizzBuzz([1, 6, 9]))
+console.log(highestCount(1, 2, 3, 4));
+
+console.log(catAndMouse(9, 1, 10));
+
+console.log(fizzBuzz([1, 6, 9]));
+
+console.log(encode('Eicke'))
 
 
 module.exports = {
