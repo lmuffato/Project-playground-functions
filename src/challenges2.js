cc2ec8afc3a01e5cfc2de6ba8280ed80d48f943b
Marcus C. H. Filho <mchusemann@gmail.com>
2021-03-08 09:53:54 -0300

    :rocket: feat(challenge10): Sorts vector of objects by the value of the tech key

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,39 @@
 // Challenge 10
-function techList(technologiesArray, name) {
-
+function sortArray(arrayOfObjects) {
+  arrayOfObjects.sort(function (a, b) {
+    if (a.tech > b.tech) {
+      return 1;
+    }
+    if (a.tech < b.tech) {
+      return -1;
+    }
+    return 0;
+  });
+  return arrayOfObjects;
 }
+
+function techList(technologiesArray, name) {
+  let arrayOfObjects = [];
+
+  if (technologiesArray.length === 0) {
+    return ('Vazio!');
+  }
+
+  for (let index = 0; index < technologiesArray.length; index += 1) {
+    let object = {
+      tech: technologiesArray[index],
+      name,
+    };
+    arrayOfObjects.push(object);
+  }
+  let sortedArray = sortArray(arrayOfObjects);
+
+  return sortedArray;
+}
+
+let array = ['React', 'Jest', 'HTML', 'CSS', 'JavaScript'];
+let name = 'Lucas';
+console.log(techList(array, name));
 
 // Challenge 11
 function generatePhoneNumber() {
