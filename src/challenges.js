9c978d673ef0f487a2dd7ca538bfaa7fb524f70b
Lucas Muffato <lmiremuffato@gmail.com>
2021-03-08 11:10:56 -0300

    all checks

--- src/challenges.js
+++ src/challenges.js
@@ -1,49 +1,88 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
-}
+function compareTrue (a, b) {
+  if (a === true && b === true) {
+      return true
+  }   else {return false;}
+  }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea (base, height) {
+  return ((base * height)/2)
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence (sentence) {
+  let myArrey = [];
+  myArrey.push = sentence.split(' ');
+  return myArrey
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName (myArrey) {
+  let concat = (myArrey[(myArrey.length - 1)]) + ',' + ' ' + myArrey[0];
+  return concat
 }
+
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints (wins, ties) {
+  return ((wins * 3) + (ties))
 }
+
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
-}
+function highestCount (myArrey) {
+  newArrey = myArrey.sort();
+  for ( index = (myArrey.length-1); ((myArrey[index]) - (myArrey[index-1])) == 0; index -= 1);
+      return (myArrey.length - index)
+  }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse (mouse, cat1, cat2) {
+  mouse = Math.abs(mouse);
+  cat1 = Math.abs(cat1);
+  cat2 = Math.abs(cat2);
+  if (cat1 == cat2) {return 'os gatos trombam e o rato foge';}
+  if ((cat1 - mouse) < (cat2)) { return 'cat1';}
+  if (((cat2 - mouse) < (cat1))) { return 'cat2';}
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(myArray) {
+  for (index = 0; index < myArray.length; index += 1) {
+  if (myArray[index] == 0){
+    myArray[index] = 'bug!';}
+  else if (myArray[index] % 3 == 0 && myArray[index] % 5 == 0){
+    myArray[index] = 'fizzBuzz';}
+  else if ((myArray[index] % 3) == 0) {
+    myArray[index] = 'fizz';}
+  else if ((myArray[index] % 5) == 0) {
+    myArray[index] = 'buzz';}
+  else {
+    myArray[index] = 'bug!';}
+  }     return myArray
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(string) {
+  string = string.split('');
+  let input = ['a', 'e', 'i', 'o', 'u'];
+  let output = [1,2,3,4,5];
+  for (index = 0; index < string.length; index += 1){
+      for (index2 = 0; index2 < input.length; index2 += 1) {
+          if (string[index] == input[index2]) {string[index] = output[index2];}
+      }
+  } return string.join('');
 }
-function decode() {
-  // seu código aqui
+function decode(string) {
+  string = string.split('');
+  let output = ['a', 'e', 'i', 'o', 'u'];
+  let input = [1,2,3,4,5];
+  for (index = 0; index < string.length; index += 1){
+      for (index2 = 0; index2 < input.length; index2 += 1) {
+          if (string[index] == input[index2]) {string[index] = output[index2];}
+      }
+  } return string.join('');
 }
 
 module.exports = {
--- src/challenges2.js
+++ src/challenges2.js
@@ -1,22 +1,58 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(array, names){
+  if (array.length === 0)
+  return 'Vazio!';
+  array = array.sort();
+  let object = {};
+  let lista = [];
+for (index = 0; index < array.length; index += 1) {
+  object[index] = { tech: array[index], name: names }
+  lista[index] = object[index];
+}
+return lista;
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber (){
+  if (array.length != 11) {return 'Arrey com tamanho incorreto'}
+  let count = 0;
+  let phoneNumber;
+  for (index = 0; index < array.length; index += 1) {
+      if (array[index] < 0 || array[index] >= 10) {return 'não é possível gerar um número de telefone com esses valores'}}     
+      for (index1 = 0; index1 < array.length; index1 += 1) {
+          count = 0;
+          for (index2 = 0; index2 < array.length; index2 += 1) {
+              if (array[index1] === array[index2]) {count += 1;}
+          }
+      }              
+      if (count >= 3) {return 'não é possível gerar um número de telefone com esses valores';}
+      phoneNumber = ( '(' + array[0] + array[1] + ')' + ' '
+                              + array[2] + array[3] + array[4] + array[5] + array[6] + '-' +
+                              + array[7] + array[8] + array[9] + array[10]);
+return phoneNumber;
+
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
-}
+function triangleCheck ([lineA, lineB, lineC]) {
+  let array = [ lineA, lineB, lineC, lineA, lineB ];
+      for (index = 1; index < (array.length - 1); index += 1) {     
+      if (array[index] > array[index-1] + array[index+1]) {return false};
+      if (array[index] < (Math.abs(array[index-1]) - Math.abs(array[index+1]))){return false};        
+  }
+  return true}
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
-}
+function hydrate (string){
+  let myString = string;
+  let toFind = /\d+/g;
+  let arrayNumb = (myString).match(toFind);
+  let totalNumb = 0
+  for (index = 0; index < arrayNumb.length; index += 1 ){
+  totalNumb += (arrayNumb[index]*1);
+  }
+  return(totalNumb + ' ' + 'copos de água')
+  }
 
 module.exports = {
   generatePhoneNumber,
