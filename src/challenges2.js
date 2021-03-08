32ac5c46b7ca0174b3717ae3a410931b7701d702
diegoCorreia <diegomethal@gmail.com>
2021-03-08 12:13:59 -0300

    requisito 11 correçao funçoes

--- src/challenges2.js
+++ src/challenges2.js
@@ -31,30 +31,34 @@ function conditionsPhoneNumber(arrayPhoneNumber) {
      if (arrayNumber[index] === arrayNumber[key]){
        count +=1;
       }
-    }
-    
+    }    
   }
-  }
+}
   function repeatNumberPhone(arrayNumber){
   if (arrayNumber[index] < 0 || arrayNumber[index] > 9 || count >= 3){
     return 'não é possível gerar um número de telefone com esses valores';
+  }
 }
-}
-function generatePhoneNumber(arrayPhone){
-  let phone = conditionsPhoneNumber + countPhoneNumber + repeatNumberPhone;
-  for (let index = 0; index < arrayPhone.length; index += 1){
-    if (arrayPhone[index] === arrayPhone[index]){
-      phone = `(${arrayPhone[0]}${arrayPhone[1]})`;
-      phone += `${arrayPhone[2]}${arrayPhone[3]}${arrayPhone[4]}${arrayPhone[5]}${arrayPhone[6]}`;
-      phone += `-${arrayPhone[7]}${arrayPhone[8]}${arrayPhone[9]}${arrayPhone[10]}`;
-      
+function generateNumber(arrayNumber){
+  let arrayPhone = [];
+  for (let index = 0; index < arrayNumber.length; index += 1){
+    if (arrayPhone[index] === arrayNumber[index]){
+      return arrayPhone[index];
     }
   }
-    return phone;
+}
+function generatePhoneNumber(arrayPhone){
+  let conditions = conditionsPhoneNumber + countPhoneNumber ;
+  let createNumber = repeatNumberPhone + generateNumber;
+  let phone = conditions + createNumber;
+  if (phone = generatePhoneNumber){
+    phone = `(${arrayPhone[0]}${arrayPhone[1]})`;
+    phone += `${arrayPhone[2]}${arrayPhone[3]}${arrayPhone[4]}${arrayPhone[5]}${arrayPhone[6]}`;
+    phone += `-${arrayPhone[7]}${arrayPhone[8]}${arrayPhone[9]}${arrayPhone[10]}`;
   }
-
-
-console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
+   return phone;
+}  
+ console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
 function triangleCheck() {
