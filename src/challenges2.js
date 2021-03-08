1a0a59b6745e0927f2644a33db089b4e4dae9409
Gabriel Pereira <g.pereira.a.m@gmail.com>
2021-03-08 11:26:08 -0300

    Requisito 11 - ESlint (3)

--- src/challenges2.js
+++ src/challenges2.js
@@ -25,14 +25,14 @@ function generatePhoneNumber(numbers) {
   // seu código aqui
   let numberFone = [];
 
-    if (numbers.length !== 11) {
-      return 'Array com tamanho incorreto.';
-    } 
-    
-    if (checkingBiggerAndSmaller(numbers) === true || checkingRepetition(numbers) === true) {
-      return 'não é possível gerar um número de telefone com esses valores';
-    }
-  
+  if (numbers.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+
+  if (checkingBiggerAndSmaller(numbers) === true || checkingRepetition(numbers) === true) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+
   numberFone = ('(' + numbers[0] + numbers[1] + ')' + ' ' + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + '-' + numbers[7] + numbers[8] + numbers[9] + numbers[10]);
   return numberFone;
 }
