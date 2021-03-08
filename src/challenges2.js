17da60a9bd93b8ed7c7055ba172bc17f49f0b487
Edmilson Bernardo <ebedmilson48@gmail.com>
2021-03-07 23:45:40 -0300

    Adiciona décima primeira função

--- src/challenges2.js
+++ src/challenges2.js
@@ -6,7 +6,7 @@ function techList(technologies, name) {
   } else {
     technologies = technologies.sort()
     for (let index = 0; index < technologies.length; index++) {
-      technologiesGroup.push( {
+      technologiesGroup.push({
         tech: technologies[index],
         name: name
       });
@@ -16,8 +16,27 @@ function techList(technologies, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(numbersGroup) {
+  if (numbersGroup.length != 11) {
+    return 'Array com tamanho incorreto.'
+  }
+  for (i = 0; i < numbersGroup.length; i++) {
+    let n = numbersGroup[i];
+    let cont = 0;
+    if (n < 0 || n > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+    for (index = 0; index < numbersGroup.length; index++) {
+      if (n == numbersGroup[index]) {
+        cont++;
+      }
+      if (cont >= 3) {
+        return 'não é possível gerar um número de telefone com esses valores';
+      }
+    }
+
+  }
+  return '(' + numbersGroup[0] + numbersGroup[1] + ') ' + numbersGroup[2] + numbersGroup[3] + numbersGroup[4] + numbersGroup[5] + numbersGroup[6] + '-' + numbersGroup[7] + numbersGroup[8] + numbersGroup[9] + numbersGroup[10];
 }
 
 // Desafio 12
