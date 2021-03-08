a1532847831d5deb7e9842d6281d243b8cfaec1c
Lucas Muffato <lmiremuffato@gmail.com>
2021-03-08 20:40:11 -0300

    desafio fone

--- src/challenges2.js
+++ src/challenges2.js
@@ -14,22 +14,29 @@ return lista;
 
 // Desafio 11
 function generatePhoneNumber (array){
-  if (array.length != 11) {return 'Array com tamanho incorreto'}
-  let count = 0;
-  let phoneNumber;
-  for (index = 0; index < array.length; index += 1) {
-      if (array[index] < 0 || array[index] >= 10) {return 'não é possível gerar um número de telefone com esses valores'}}     
-      for (index1 = 0; index1 < array.length; index1 += 1) {
-          count = 0;
-          for (index2 = 0; index2 < array.length; index2 += 1) {
-              if (array[index1] === array[index2]) {count += 1;}
+  let repetido = 0;
+  let analisado = 0;
+  let comparado = 0;
+  let limit = 3;
+  for (let index = 0; index < array.length; index += 1) {
+      analisado = array[index];
+      repetido = 0;
+      if (repetido < limit)  {
+          for (let index2 = 0; index2 < array.length; index2 += 1) {
+              comparado = array[index2];
+              if (analisado === comparado) {repetido += 1;}
           }
-      }              
-      if (count >= 3) {return 'não é possível gerar um número de telefone com esses valores';}
-      phoneNumber = ( '(' + array[0] + array[1] + ')' + ' '
-                              + array[2] + array[3] + array[4] + array[5] + array[6] + '-' +
-                              + array[7] + array[8] + array[9] + array[10]);
-return phoneNumber;}
+      } if (repetido >= limit) {return 'não é possível gerar um número de telefone com esses valores';}
+  } 
+if (array.length != 11) {return 'Array com tamanho incorreto'}
+for (index = 0; index < array.length; index += 1) {
+if (array[index] < 0 || array[index] > 9) {return 'não é possível gerar um número de telefone com esses valores'}}
+
+ let count = 0;
+let phoneNumber;
+phoneNumber = ( '(' + array[0] + array[1] + ')' + ' '
+                      + array[2] + array[3] + array[4] + array[5] + array[6] + '-' +
+                      + array[7] + array[8] + array[9] + array[10]);return phoneNumber;}
 
 // Desafio 12
 function triangleCheck (lineA, lineB, lineC) {
