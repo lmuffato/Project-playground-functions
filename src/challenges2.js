61efe1678e634e0df2a88bc3e4c35ae016bb164b
diegoCorreia <diegomethal@gmail.com>
2021-03-08 13:41:08 -0300

    requisito 11 refatoraçao

--- src/challenges2.js
+++ src/challenges2.js
@@ -20,43 +20,43 @@ console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Diego'));
 // Desafio 11
 
 function conditionsPhoneNumber(arrayPhoneNumber) {
-  if (arrayPhoneNumber.length !== 11){
+  if (arrayPhoneNumber.length !== 11) {
     return 'Array com tamanho incorreto';
-  }else if(arrayNumber[index] < 0 || arrayNumber[index] > 9){
-      return 'não é possível gerar um número de telefone com esses valores';
+  } if (arrayPhoneNumber < 0 || arrayPhoneNumber > 9) {
+    return 'não é possível gerar um número de telefone com esses valores';
   }
 }
-  function countPhoneNumber(arrayNumber){
-  for (let index in arrayNumber){
-      if(arrayNumber[index] === arrayNumber[index]){
-       count +=1;
-      }else if(count >= 3){
-          return 'não é possível gerar um número de telefone com esses valores';
-     }
-    }    
+function countPhoneNumber(arrayNumber) {
+  let count = 0;
+  for (let index in arrayNumber) {
+    if (arrayNumber[index] === arrayNumber) {
+      count += 1;
+      } else if (count >= 3) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
   }
-  function generateNumber(arrayNumber){
+}
+function generateNumber(arrayNumber) {
   let arrayPhone = [];
-  for (let index = 0; index < arrayNumber.length; index += 1){
-    if (arrayPhone[index] === arrayNumber[index]){
+  for (let index = 0; index < arrayNumber.length; index += 1) {
+    if (arrayPhone[index] === arrayNumber[index]) {
       return arrayPhone[index];
     }
   }
 }
-function allFunctionsPhone(conditions,createNumber){
-  conditions = conditionsPhoneNumber + countPhoneNumber ;
-  createNumber = repeatNumberPhone + generateNumber;
+function allFunctionsPhone(conditions, createNumber) {
+  conditions = conditionsPhoneNumber + countPhoneNumber + generateNumber;
 }
-function generatePhoneNumber(arrayPhone){
+function generatePhoneNumber(arrayPhone) {
   let phone = allFunctionsPhone;
-  if (phone = generatePhoneNumber){
+  if (generatePhoneNumber = phone) {
     phone = `(${arrayPhone[0]}${arrayPhone[1]})`;
     phone += `${arrayPhone[2]}${arrayPhone[3]}${arrayPhone[4]}${arrayPhone[5]}${arrayPhone[6]}`;
     phone += `-${arrayPhone[7]}${arrayPhone[8]}${arrayPhone[9]}${arrayPhone[10]}`;
   }
-   return phone;
-}  
- console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
+  return phone;
+}
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
 function triangleCheck() {
