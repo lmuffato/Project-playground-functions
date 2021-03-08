9f48dc16ad25966204f44b14937a04c3e012bada
Renata Andrade <reapminas@gmail.com>
2021-03-08 19:24:51 -0300

    Requisito 12 - Crie uma função de Condição de existência de um triângulo

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,9 @@
 // Desafio 10
 function techList(tech, name) {
-  // seu código aqui
+  let technologies = {
+    tech: tech,
+    name: name
+  }
 }
 
 // Desafio 11
@@ -9,8 +12,20 @@ function generatePhoneNumber() {
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  let somLineAB = lineA + lineB;
+  let somLineAC = lineA + lineC;
+  let somLineBC = lineB + lineC;
+  let difAbsolutAB = Math.abs(lineA - lineB);
+  let difAbsolutAC = Math.abs(lineA - lineC);
+  let difAbsolutBC = Math.abs(lineB - lineC);
+  let check;
+  if ((lineA < somLineBC && lineA > difAbsolutBC) || (lineB < somLineAC && lineB > difAbsolutAC) || (lineC < somLineAB && lineC > difAbsolutAB)) {
+    check = true;
+  } else {
+    check = false;
+  }
+  return check;
 }
 
 // Desafio 13
@@ -24,3 +39,8 @@ module.exports = {
   hydrate,
   triangleCheck,
 };
+
+// Testando a função triangleCheck
+console.log(triangleCheck(10, 14, 8));
+console.log(triangleCheck(1, 6, 8));
+console.log(triangleCheck(14, 4, 8));
