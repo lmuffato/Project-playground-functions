048daba8c2244fe904c41094d444503f9822908c
Luiz Otavio <luiz.otavio273@gmail.com>
2021-03-07 23:01:35 -0300

    Desafio 8 - fizzBuzz e desafio 9 - encode e decode

--- src/challenges.js
+++ src/challenges.js
@@ -20,7 +20,7 @@ function splitSentence(phrase) {
 
 // Desafio 4
 function concatName(names) {
-  let firstLastNames = names[0].concat (', ' + names[names.length - 1]);
+  let firstLastNames = names[0] + ', ' + names[names.length - 1];
   return firstLastNames;
 }
 
@@ -35,7 +35,7 @@ function footballPoints(wins, ties) {
 function highestCount(array) {
   let numberLarger = 0;
   let counter = 0;
-  let amountNumberLarger = 0
+  let amountNumberLarger = 0;
   for (let index = 1; index < array.length; index += 1) {
     if (array[counter] > array[index]) {
       numberLarger = array[counter];
@@ -54,8 +54,8 @@ function highestCount(array) {
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
   let phrases = ['os gatos trombam e o rato foge', 'cat1', 'cat2'];
-  let firstCat = mouse + cat1;
-  let secondCat = mouse + cat2
+  let firstCat = cat1 - mouse;
+  let secondCat = cat2 - mouse;
   if (firstCat < secondCat) {
     return phrases[1];
   } else if (cat1 === cat2){
@@ -66,17 +66,57 @@ function catAndMouse(mouse, cat1, cat2) {
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(arrayNumber) {
+  let arrayResult = [];
+  for (let index = 0; index < arrayNumber.length; index += 1) {
+    if (arrayNumber[index] % 3 === 0 && arrayNumber[index] % 5 != 0){
+      arrayResult.push('fizz');
+    } else if (arrayNumber[index] % 5 === 0 && arrayNumber[index] % 3 != 0) {
+        arrayResult.push('buzz');
+      } else if (arrayNumber[index] % 3 === 0 && arrayNumber[index] % 5 === 0) {
+          arrayResult.push('fizzBuzz');
+        } else {
+            arrayResult.push('bug!')
+          }
+  }
+  return arrayResult
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(stringEncode) {
+  for (let index = 0; index < stringEncode.length; index += 1) {
+    if (stringEncode[index] === 'a') {
+      stringEncode = stringEncode.replace(stringEncode[index], '1');
+    } else if (stringEncode[index] === 'e') {
+        stringEncode = stringEncode.replace(stringEncode[index], '2');
+      } else if (stringEncode[index] === 'i') {
+          stringEncode = stringEncode.replace(stringEncode[index], '3');
+        } else if (stringEncode[index] === 'o') {
+            stringEncode = stringEncode.replace(stringEncode[index], '4');
+          } else if (stringEncode[index] === 'u') {
+              stringEncode = stringEncode.replace(stringEncode[index], '5');
+            }       
+  }  
+  return stringEncode;
 }
-function decode() {
-  // seu código aqui
+
+function decode(stringDecode) {
+  for (let index = 0; index < stringDecode.length; index += 1) {
+    if (stringDecode[index] === '1') {
+      stringDecode = stringDecode.replace(stringDecode[index], 'a');
+    } else if (stringDecode[index] === '2') {
+        stringDecode = stringDecode.replace(stringDecode[index], 'e');
+      } else if (stringDecode[index] === '3') {
+          stringDecode = stringDecode.replace(stringDecode[index], 'i');
+        } else if (stringDecode[index] === '4') {
+            stringDecode = stringDecode.replace(stringDecode[index], 'o');
+          } else if (stringDecode[index] === '5') {
+              stringDecode = stringDecode.replace(stringDecode[index], 'u');
+            }       
+  }  
+  return stringDecode;
 }
+
 
 module.exports = {
   calcArea,
