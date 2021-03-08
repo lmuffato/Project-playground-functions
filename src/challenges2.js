a35a86f5727322fef42c9391d3e6c0b538a38b71
Rodrigo Grande <rogrande0496@gmail.com>
2021-03-08 01:16:40 -0300

    fix requisito 13

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,6 @@
 // Desafio 10
 function techList(techArray, name) {
   let resultArray = [];
-  let objectList;
   for (let index = 0; index < techArray.length; index += 1) {
     let objectList = {
       tech: techArray[index],
@@ -27,8 +26,19 @@ function triangleCheck() {
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(str) {
+  let numDrinks = str.match(/\d+/g);
+  let copos = 0;
+  
+  for (let index = 0; index < numDrinks.length; index += 1) {
+    let drinks = Number(numDrinks[index]);
+    copos += drinks;
+  }
+  if (copos === 1) {
+    return `${copos} copo de água`;
+  } else {
+  return `${copos} copos de água`;
+  }
 }
 
 module.exports = {
