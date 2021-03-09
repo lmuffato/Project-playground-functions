2593d41f754f5fb333782ffafa87ee74be3ec315
Eicke Yumoto <eickeyumoto@gmail.com>
2021-03-08 23:20:47 -0300

    Adiciona requisito 9

--- src/challenges.js
+++ src/challenges.js
@@ -3,7 +3,7 @@ function compareTrue(value1, value2) {
   let result;
   if (value1 === true && value2 === true) {
     result = true;
-  } else { result = false }
+  } else { result = false; }
   return result;
 }
 
@@ -19,84 +19,91 @@ function splitSentence(string) {
 
 // Desafio 4
 function concatName(strArray) {
-  return strArray[strArray.length - 1] + ', ' + strArray[0];
+  return `${strArray[strArray.length - 1]}, ${strArray[0]}`;
 }
 
 // Desafio 5
 function footballPoints(wins, ties) {
   let points;
-  wins = wins * 3;
+  wins *= 3;
   points = wins + ties;
   return points;
 }
 
 // Desafio 6
 function highestCount(numbers) {
-  let count = 0
+  let count = 0;
   let highestNumber = Math.max(...numbers);
-  for(let index = 0; index < numbers.length; index += 1) {
+  for (let index = 0; index < numbers.length; index += 1) {
     if (numbers[index] === highestNumber) {
-      count += 1
+      count += 1;
     }
   }
   return count;
 }
 
-
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  let distMouseCat1 = mouse - cat1
-  let distMouseCat2 = mouse - cat2
-  if(distMouseCat1 < distMouseCat2) {
-    return 'cat1'
-  } else if(distMouseCat1 > distMouseCat2) {
-    return'cat2'
-} else { return 'os gatos trombam e o rato foge' }
+  let distMouseCat1 = mouse - cat1;
+  let distMouseCat2 = mouse - cat2;
+  if (distMouseCat1 < distMouseCat2) {
+    return 'cat1';
+  } if (distMouseCat1 > distMouseCat2) {
+    return 'cat2';
+  } return 'os gatos trombam e o rato foge';
 }
-    
 
 // Desafio 8
 function fizzBuzz(numberArray) {
-  let arrayFizzBuzz = []
-  for(let i = 0; i < numberArray.length; i += 1) {
+  let arrayFizzBuzz = [];
+  for (let i = 0; i < numberArray.length; i += 1) {
     if (numberArray[i] % 3 == 0 && numberArray[i] % 5 == 0) {
-      arrayFizzBuzz.push('fizzbuzz')
+      arrayFizzBuzz.push('fizzbuzz');
     } else if (numberArray[i] % 5 == 0) {
-      arrayFizzBuzz.push('buzz')
+      arrayFizzBuzz.push('buzz');
     } else if (numberArray[i] % 3 == 0) {
-      arrayFizzBuzz.push('fizz')
-    } else { arrayFizzBuzz.push('bug!') }
+      arrayFizzBuzz.push('fizz');
+    } else { arrayFizzBuzz.push('bug!'); }
   }
-  return arrayFizzBuzz
+  return arrayFizzBuzz;
 }
 
 // Desafio 9
 function encode(encodeString) {
-  let splitedString = encodeString.split('')
- for (let index = 0; index < splitedString.length; index += 1) {
-   if (splitedString[index] === 'a') {
-     'a' = 1
-   } if else (splitedString[index] === 'e'); {
-     'e' = 2
-   } if else (splitedString[index] === 'i'); {
-     'i' = 2
-   } if else (splitedString[index] === 'o'); {
-     'o' = 2
-   } else (splitedString[index] === 'u'); {
-     'u' = 2
-   }
+  let splitedString = encodeString.split('');
+  for (let index = 0; index < splitedString.length; index += 1) {
+    if (splitedString[index] === 'a') {
+      splitedString[index] = 1;
+    } else if (splitedString[index] === 'e') {
+      splitedString[index] = 2;
+    } else if (splitedString[index] === 'i') {
+      splitedString[index] = 3;
+    } else if (splitedString[index] === 'o') {
+      splitedString[index] = 4;
+    } else if (splitedString[index] === 'u') {
+      splitedString[index] = 5;
+    } else { }
   }
-   splitedString.join('')
-   return splitedString
+  return splitedString.join('');
 }
- // return encodeString
-//}
-encode('batata')
-  
 
-function decode(decodeString) {}
-  
-
+function decode(decodeString) {
+  let splitedString = decodeString.split('');
+  for (let index = 0; index < splitedString.length; index += 1) {
+    if (splitedString[index] === '1') {
+      splitedString[index] = 'a';
+    } else if (splitedString[index] === '2') {
+      splitedString[index] = 'e';
+    } else if (splitedString[index] === '3') {
+      splitedString[index] = 'i';
+    } else if (splitedString[index] === '4') {
+      splitedString[index] = 'o';
+    } else if (splitedString[index] === '5') {
+      splitedString[index] = 'u';
+    } else { }
+  }
+  return splitedString.join('');
+}
 
 console.log(compareTrue(true, true));
 
@@ -110,13 +117,13 @@ console.log(footballPoints(14, 8));
 
 console.log(highestCount([1, 4, 4, 4]));
 
-console.log(catAndMouse(9, 1, 10));
+console.log(catAndMouse(7, 7, 2));
 
 console.log(fizzBuzz([1, 6, 9]));
 
-console.log(encode('Eicke'))
+console.log(encode('ola para todos'));
 
-console.log()
+console.log(decode('23ck2'));
 
 module.exports = {
   calcArea,
