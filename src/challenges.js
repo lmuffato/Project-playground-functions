d5d4b0591ec70673e366157d89c674a1a6d7441d
Henrique Lima <hnamix@gmail.com>
2021-03-09 14:47:37 -0400

    Refazendo 01

--- src/challenges.js
+++ src/challenges.js
@@ -6,179 +6,6 @@ function compareTrue(number1, number2) {
   return false;
 }
 
-// Desafio 2
-function calcArea(base, height) {
-/*  let area = 0;
-  area = (base * height) / 2;
-  return area;
-  */
-}
-
-// Desafio 3
-function splitSentence(sentence) {
- /* let array = [];
-  array = sentence.split(' ');
- */ return array;
-}
-
-// Desafio 4
-function concatName(phrase) {
- /* let input = '';
-  input = phrase[counter - 1] + ', ' + phrase[0];
- */ return input;
-}
-
-// Desafio 5
-function footballPoints(wins, ties) {
-  /*let points = 0;
-  points = wins * 3 + ties;
-  return points;
-  */
-}
-
-function calcCount(number, arrayCalc) {
-  /*let counter = 0;
-  for (let index = 0; index < arrayCalc.length; index += 1) {
-    if (arrayCalc[index] === number) {
-      counter += 1;
-    }
-  }
-  */return counter;
-}
-
-// Desafio 6
-function highestCount(array) {
-  /*let high = array[0];
-  for (let key in array) {
-    if (array[key] > high) {
-      high = array[key];
-    }
-  }
-  return calcCount(high, array);
-  */
-}
-
-// Desafio 7
-function catAndMouse(mouse, cat1, cat2) {
- /* let positionCat1 = 0;
-  let positionCat2 = 0;
-  positionCat1 = cat1 - mouse;
-  positionCat2 = cat2 - mouse;
-  if (positionCat2 > positionCat1) {
-    return 'cat1';
-  } if (positionCat1 > positionCat2) {
-    return 'cat2';
-  }
-  return 'os gatos trombam e o rato foge';
-  */
-}
-
-function arrayFizzBuzz(array) {
-  /*if (array % 3 === 0 && array % 5 !== 0) {
-    return 'fizz';
-  } if (array % 3 !== 0 && array % 5 !== 0) {
-    return 'bug';
-  }
-  *///return 'buzz';
-}
-
-// Desafio 8
-function fizzBuzz(array) {
-  /*let arrayfb = [];
-  for (let key in array) {
-    if (array[key] % 3 === 0 && array[key] % 5 === 0) {
-      arrayfb.push('fizzBuzz');
-    } else {
-      arrayfb.push(arrayFizzBuzz(array[key]));
-    }
-  }
-  return arrayfb;*/
-}
-
-// Desafio 9
-function decision(phrase){
-/*  let inputPhrase;
-  for (let key in phrase) {
-    if (phrase[key] == 1 || phrase[key] == 2 || phrase[key] == 3 || phrase[key] == 4 || phrase[key] == 5) {
-      inputPhrase = decode(phrase);
-      break;
-    }
-    inputPhrase = encode(phrase);
-    break;
-  }
-  return inputPhrase;*/
-}
-
-function encode(phrase) {
-  /*let inputPhrase = '';
-  for (let key in phrase) {
-    switch(phrase[key]) {
-      case 'a' :
-      inputPhrase += '1';
-      break;
-
-      case 'e' :
-      inputPhrase += '2';
-      break;
-
-      case 'i' :
-      inputPhrase += '3';
-      break;
-
-      case 'o' :
-      inputPhrase += '4';
-      break;
-
-      case 'u' :
-      inputPhrase += '5';
-      break;
-
-      default :
-      inputPhrase += phrase[key];
-
-    }
-  }
-  return inputPhrase;
-
- */ // seu código aqui
-}
-
-function decode(phrase) {
-  /*let inputPhrase = '';
-  for (let key in phrase) {
-    switch(phrase[key]) {
-      case '1':
-      inputPhrase += 'a';
-      break;
-
-      case '2' :
-      inputPhrase += 'e';
-      break;
-
-      case '3' :
-      inputPhrase += 'i';
-      break;
-
-      case '4' :
-      inputPhrase += 'o';
-      break;
-
-      case '5' :
-      inputPhrase += 'u';
-      break;
-
-      default :
-      inputPhrase += phrase[key];
-
-    }
-  }
-  return inputPhrase;
-
-  // seu código aqui*/
-}
-
-let names = ['foguete', 'não', 'tem', 'ré'];
-
 console.log(compareTrue(true, false));
 /*console.log(calcArea(51, 1));
 console.log(splitSentence('go Trybe'));
@@ -190,8 +17,8 @@ console.log(fizzBuzz([2, 15, 7, 9, 45]));
 console.log(decision('hi there'));
 */
 module.exports = {
-  calcArea,
-  catAndMouse,
+  //calcArea,
+ /* catAndMouse,
   compareTrue,
   concatName,
   decode,
@@ -199,5 +26,5 @@ module.exports = {
   fizzBuzz,
   footballPoints,
   highestCount,
-  splitSentence,
+  splitSentence, */
 };
