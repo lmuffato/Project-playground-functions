e56e47bd27f7e354d58bd5d4a50cdb0873c78beb
Luiz Felipe Araujo <thelastnailmusic@gmail.com>
2021-03-09 10:22:29 -0300

    Cat and Mouse

--- src/challenges.js
+++ src/challenges.js
@@ -77,25 +77,14 @@ function highestCount(numeros) {
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-    let dist1 = cat1 - mouse;
-    let dist2 = cat2 - mouse;
-    if ((mouse > cat1) || (mouse > cat2)) {
-      if (dist1 > dist2) {
-        return 'cat1';
-      } else if (dist1 < dist2) {
-        return 'cat2';
-      } else if (dist1 === dist2) {
-        return 'os gatos trombam e o rato foge';
-      }
-    } else {
-      if (dist1 > dist2) {
-        return 'cat2';
-      } else if (dist1 < dist2) {
-        return 'cat1'
-      } else if (dist1 === dist2) {
-        return 'os gatos trombam e o rato foge';
-    }
-  }
+  let dist1 = Math.abs(cat1 - mouse);
+  let dist2 = Math.abs(cat2 - mouse);
+
+  if (dist1 > dist2) {
+    return 'cat2';
+  } if (dist1 < dist2) {
+    return 'cat1';
+  } return 'os gatos trombam e o rato foge';
 }
 
 // Desafio 8
