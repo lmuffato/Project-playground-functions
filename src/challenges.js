8c39109da9d97aa63a71257492310faa2b06ac07
biazidioti <bzferreira.bio@gmail.com>
2021-03-09 08:46:03 -0300

    Start function 12.

--- src/challenges.js
+++ src/challenges.js
@@ -76,25 +76,12 @@ function fizzBuzz(number) {
 }
 
 // Desafio 9
-function encode(encodeArray) {
-  let encodeDone = 0;
-  // for (var index = 0; index < sentenceArray.length; index += 1) {
-  //   if (sentenceArray[index])
-  //   a 1
-  //   e 2
-  //   i 3
-  //   o 4
-  //   u 5
-  // }
+function encode() {
+ 
 }
 
-function decode(decodeArray) {
-  let encodeDone = 0;
-// 1 a
-// 2 e
-// 3 i
-// 4 o
-// 5 u
+function decode() {
+
 }
 
 module.exports = {
--- src/challenges2.js
+++ src/challenges2.js
@@ -9,8 +9,13 @@ function generatePhoneNumber() {
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  if ( lineA < (lineB + lineC) && lineA < Math.abs(lineB + lineC)) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 // Desafio 13
