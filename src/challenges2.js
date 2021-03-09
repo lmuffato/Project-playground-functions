d922b953f3b1ad80cdde254d80063b30f3847d35
Nathalia Zebral <natzebral@gmail.com>
2021-03-09 14:00:03 -0300

    alteracao no desafio 10 e 12 que nao passaram no evaluator

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,4 +1,5 @@
 // Desafio 10
+
 function techList(technologies, studantsname) {
   let arrayOfTechs = [];
   for (let index = 0; index < technologies.sort().length; index += 1) {
@@ -12,23 +13,13 @@ function techList(technologies, studantsname) {
 }
 
 // Desafio 11
-//let phoneNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]
-//function arrayLength(phoneNumber) {
-  //if (phoneNumber.length !== 11){
-  //  return "Array com tamanho incorreto";
- // }
-//} 
-//function numberNotValid(phoneNumber){
-  //for(let index = 0; index < phoneNumber.length; index += 1){
-   //If (phoneNumber[index] < 0 || phoneNumber[index] > 9){
-     //return 'não é possivel gerar um número com esses valores';
-   //} 
-  //}
-//}
-//function generatePhoneNumber() {
-//}
+
+function generatePhoneNumber() {
+  // seu código aqui
+}
 
 // Desafio 12
+
 function triangleCheck(lineA, lineB, lineC) {
   if (lineA < lineB + lineC && lineA > Math.abs(lineB - lineC)) {
     return true;
