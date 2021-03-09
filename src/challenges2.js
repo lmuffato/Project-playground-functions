2c429728be4ed102b5894110dfb31e83c800a473
Eduardo Seije <eduardoseije@gmail.com>
2021-03-08 21:50:11 -0300

    desafio 12 - function triangleCheck

--- src/challenges2.js
+++ src/challenges2.js
@@ -25,17 +25,17 @@ function generatePhoneNumber(array) {
   let repeatCounter = 0;
   for (let key in array) {
     if (array.length < 10 || array.length > 11) {
-      return "Array com tamanho incorreto.";
+      return 'Array com tamanho incorreto.';
     }
     if (array[key] < 0 || array[key] > 9) {
-      return "não é possível gerar um número de telefone com esses valores";
+      return 'não é possível gerar um número de telefone com esses valores';
     }
   }
   for (let key in array) {
     if (array[key] === array[key +=1]) {
-      repeatCounter += 1;
+     let = repeatCounter += 1;
       if (repeatCounter > 3) {
-        return "não é possível gerar um número de telefone com esses valores";
+        return 'não é possível gerar um número de telefone com esses valores';
       }
     }
   }
@@ -44,16 +44,26 @@ function generatePhoneNumber(array) {
   }
 
   for (let key in phoneNumber) {
-    return "(" + phoneNumber[0]+phoneNumber[1]+")" + " " + phoneNumber[2]+phoneNumber[3]+phoneNumber[4]+phoneNumber[5]+phoneNumber[6]+"-"+phoneNumber[7]+phoneNumber[8]+phoneNumber[9]+phoneNumber[10]
+    return `(${phoneNumber[0]}${phoneNumber[1]}) ${phoneNumber[2]}${phoneNumber[3]}${phoneNumber[4]}${phoneNumber[5]}${phoneNumber[6]}-${phoneNumber[7]}${phoneNumber[8]}${phoneNumber[9]}${phoneNumber[10]}`;
   }
   return phoneNumber;
 }
-console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]))
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  if (lineA < lineB + lineC || lineA > Math.abs(lineB - lineC)) {
+    return true;
+  } 
+  if (lineB < lineA + lineC || lineB > Math.abs(lineA - lineC)) {
+      return true;
+  }
+  if (lineC < lineA + lineB || lineC > Math.abs(lineA - lineB)) {
+    return true;
+  } else {
+    false;
+  }
 }
+console.log(triangleCheck(10, 14, 8))
 
 // Desafio 13
 function hydrate() {
