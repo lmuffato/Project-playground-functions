26fb2b7f1ff9150b55b4b37cbab6e44fccb57c19
André Dantas <aluizcfd@gmail.com>
2021-03-09 11:03:55 -0300

    Adição desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -19,8 +19,66 @@ function techList(arrayTechs, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function phoneNumberOutput (arrayPhone) {
+  let firstSector = arrayPhone[0].toString() + arrayPhone[1].toString();
+  let secondSector = '';
+  let thirdSector = '';
+
+  for (let i = 2; i <= 6; i += 1) {
+    secondSector += arrayPhone[i].toString();
+  }
+
+  for (let j = 7; j <= 10; j += 1) {
+    thirdSector += arrayPhone[j].toString();
+  }
+  return `(${firstSector}) ${secondSector}-${thirdSector}`;
+}
+
+function countRepeat(arrayPhone, numberPhone) {
+  let timesNumber = 0;
+  for (let i = 0; i < arrayPhone.length; i += 1) {
+    if (arrayPhone[i] === numberPhone) {
+      timesNumber += 1;
+    }
+  }
+  return timesNumber;
+}
+
+function lessThanZero(numberPhone) {
+  let result = false;
+  for (let i = 0; i < numberPhone.length; i += 1) {
+    if (numberPhone[i] < 0) {
+      result = true;
+    }
+  }
+  return result;
+}
+
+function biggerThanNine(numberPhone) {
+  let result = false;
+  for (let i = 0; i < numberPhone.length; i += 1) {
+    if (numberPhone[i] > 9) {
+      result = true;
+    }
+  }
+  return result;
+}
+
+function generatePhoneNumber(arrayPhone) {
+  if (arrayPhone.length != 11) {
+    return 'Array com tamanho incorreto.';
+  }
+
+  if (biggerThanNine(arrayPhone) || lessThanZero(arrayPhone) === true) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+  
+  for (let i = 0; i < arrayPhone.length; i += 1) {
+    if (countRepeat(arrayPhone, arrayPhone[i]) >= 3) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  return phoneNumberOutput(arrayPhone);
 }
 
 // Desafio 12
