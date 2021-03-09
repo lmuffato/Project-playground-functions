a02049b1ab399d3d06c023d004b972af761286d1
pmaiamateus <pmaiamateus@gmail.com>
2021-03-09 13:58:27 -0300

    conserta a 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -38,7 +38,6 @@ function generatePhoneNumber(numeros){
     return numero
   }
 }
-console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]))
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC){
