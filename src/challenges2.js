9e3ef1305546bcbb38820b6e0f7553d61354daaf
Alan <alan.alb.flopes@gmail.com>
2021-03-09 10:17:44 -0400

    adicionado requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -18,12 +18,68 @@ function techList(array,name) {
   }
 }
 
-
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+
+
+function ceilChecker(array){
+  let counter=0;
+  let repeated=[];
+  let ceilGeted=false;
+  for (let index = 1; index < array.length; index++) {
+      for (let ondex = 0; ondex < index; ondex++) {
+        if(array[ondex]==array[index]){
+          repeated.push(array[ondex]);
+        } 
+      
+    }
+   
+    if(array[index]<0||array[index]>9){
+          ceilGeted=true;
+        }
+      }
+    for (let index = 1; index < repeated.length; index++) {
+       for (let ondex = 0; ondex < index; ondex++) {
+         if(repeated[index]==repeated[ondex]);
+         counter+=1        
+      }
+      
+    }
+  if(counter>=3){
+    ceilGeted=true
+  }
+  return ceilGeted
 }
 
+function generatePhoneNumber(array) {
+let ceil = ceilChecker(array);
+let result = ""
+  if(array.length!=11){
+    result+="Array com tamanho incorreto."
+  }else if(ceil==true){
+    result+="não é possível gerar um número de telefone com esses valores"
+  }else{
+    for (let index = 0; index < array.length; index++) {
+      if(index==0){
+        result+="("+array[index];
+      }else if(index==1){
+        result+=array[index]+")"
+      }
+      else if(index==2){
+        result+=" "+array[index]
+      }
+      else if(index>=3&&index<=6){
+        result+=array[index];
+      }
+      else if(index==7){
+        result+="-"+array[index];
+      }
+      else{
+        result+=array[index];
+      }
+    }
+  }
+  return result;
+}
 // Desafio 12
 function triangleCheck() {
   // seu código aqui
