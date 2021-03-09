b427bc2a2ca817f66f4bcd34e94787ef59dfaeb3
Rafael Bamberg <raafaelbamberg@gmail.com>
2021-03-09 02:55:22 -0300

    desafio 1

---
+++ challenges.js
@@ -0,0 +1,25 @@
+// Desafio 1 //
+let idade = true;
+let nome = true;
+let compareTrue;
+
+if (idade == true && nome == true); {
+    console.log (compareTrue == true)
+}
+else if (idade !== true && nome !== true); {
+    console.log (compareTrue == false);
+}
+else {
+    console.log (compareTrue == false);
+}
+// Desafio 2 //
+let base
+let height
+let calcArea = (base * height / 2);
+    console.log(calcArea);
+
+// Desafio 3 //
+let splitSentence
+let separado = splitSentence.split(" ", 2); {
+    console.log(separado);
+}
