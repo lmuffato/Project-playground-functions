4af0e1d03836ca1e9e727c0b6042aec1adde3b81
Vinicius Rodrigues <V.rodriguesjk@gmail.com>
2021-03-09 11:39:18 -0300

    Ajustes1

--- src/challenges2.js
+++ src/challenges2.js
@@ -3,23 +3,27 @@ function techList(tech, name) {
   // seu código aqui
   let result = [];
 
-    for(let index = 0 ; index < tech.length; index += 1){
-        result.push({tech: tech[index], name: name})
-    }
-
-    result.sort(compare);
+  for (let index = 0; index < tech.length; index += 1) {
+    result.push({ tech: tech[index], name: name });
+  }
+  result.sort(compare);
+  if (result.sort(compare).length > 0) {
     return result;
+  }
+  else {
+    return 'Vazio!';
+  }
 
 }
 
-function compare(a,b){
-    if(a.tech < b.tech){
-        return -1;
-    }
-    if(a.tech > b.tech){
-        return 1;
-    }
-    return 0;
+function compare(a, b) {
+  if (a.tech < b.tech) {
+    return -1;
+  }
+  if (a.tech > b.tech) {
+    return 1;
+  }
+  return 0;
 }
 
 // Desafio 11
