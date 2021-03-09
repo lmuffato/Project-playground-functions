c394c2fd71a4f2d4ef224df536e662617f5a2499
luanlsr <luanfswd@gmail.com>
2021-03-08 22:10:23 -0300

    Corrigido lint

--- src/challenges2.js
+++ src/challenges2.js
@@ -39,7 +39,9 @@ function generatePhoneNumber(phoneNumber) {
       }
     }
   }
-  return `(${phoneNumber[0]}${phoneNumber[1]}) ${phoneNumber[2]}${phoneNumber[3]}${phoneNumber[4]}${phoneNumber[5]}${phoneNumber[6]}-${phoneNumber[7]}${phoneNumber[8]}${phoneNumber[9]}${phoneNumber[10]}`;
+  return `(${phoneNumber[0]}${phoneNumber[1]}) ${phoneNumber[2]}
+  ${phoneNumber[3]}${phoneNumber[4]}${phoneNumber[5]}${phoneNumber[6]}
+  -${phoneNumber[7]}${phoneNumber[8]}${phoneNumber[9]}${phoneNumber[10]}`;
 }
 
 // Desafio 12
