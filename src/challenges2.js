6eec8bfb8f05003d035b0e5ef348153310888ecf
Lucas <lucasml@live.com>
2021-03-10 13:27:00 -0300

    Criterio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -12,14 +12,31 @@ function techList(array, name) {
     };
     arrayTech.push(learningTech);
   }
-return arrayTech.sort();
+  return arrayTech.sort();
 }
 console.log(techList([], 'Lucas'))
 
-// Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+// Desafio 11 <-- Contribuição Felipe Muller e Henrique Lima / Discussões no Zoom-->
+function generatePhoneNumber(arrayNumber) {
+
+  if (arrayNumber.length !== 11){
+    return 'Array com tamanho incorreto.'
+  }
+
+  for (let index = 0; index < arrayNumber.length; index += 1) {
+    let count = 0;
+    for (let indexNumber = 0; indexNumber < arrayNumber.length; indexNumber += 1) {
+      if (arrayNumber[index] === arrayNumber[indexNumber]) {
+        count += 1;
+      } if (arrayNumber[index] < 0 || arrayNumber[index] > 9 || count >=3) {
+        return 'não é possível gerar um número de telefone com esses valores';
+      }
+    }
+
+  }
+    return `(${arrayNumber[0]}${arrayNumber[1]}) ${arrayNumber[2]}${arrayNumber[3]}${arrayNumber[4]}${arrayNumber[5]}${arrayNumber[6]}-${arrayNumber[7]}${arrayNumber[8]}${arrayNumber[9]}${arrayNumber[10]}`
 }
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 3]));
 
 // Desafio 12
 function triangleCheck() {
