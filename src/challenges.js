da397868c158503343cbca28ac8e1d1b701e681b
Rafael Bamberg <raafaelbamberg@gmail.com>
2021-03-19 19:55:40 -0300

    Correcoes gerais 13

--- src/challenges.js
+++ src/challenges.js
@@ -52,8 +52,7 @@ function highestCount(array) {
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
   let cat1Position = Math.abs(cat1 - mouse)
-  let cat2Position = Math.abs(cat2 - mouse)
-  
+  let cat2Position = Math.abs(cat2 - mouse) 
   if (cat1Position > cat2Position) {
     return 'cat2'
   }
