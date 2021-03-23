c73d93b36bddc4e8d5bb92d6cda79d42b85561a3
Rafael Bamberg <raafaelbamberg@gmail.com>
2021-03-23 13:06:13 -0300

    Correcoes 18

--- src/challenges2.js
+++ src/challenges2.js
@@ -17,7 +17,28 @@ function techList(technologiesList, name) {
  
 
 // Desafio 11
-function generatePhoneNumber()
+function wrongRangeCheck(currentValue) {
+  if (currentValue > 9 || currentValue < 0) {
+    return "não é possível gerar um número de telefone com esses valores"
+  }
+}
+function generatePhoneNumber(phoneNumb) {
+  if (phoneNumb.length !== 11) {
+    return "Array com tamanho incorreto."
+  };
+  phoneNumb.some(wrongRangeCheck);
+  for (let index = 0; index < phoneNumb.length; index += 1) {
+    let repeat = 0
+    for (let index2 = index + 1; index2 < phoneNumb.lenght; index2 += 1) {
+      if(phoneNumb[index] === phoneNumb[index2]) {
+        repeat += 1;
+      } if (repeat >= 3) {
+        return "não é possível gerar um número de telefone com esses valores"
+      }
+    }
+  }
+  return `(${arr[0]}${arr[1]}) ${arr[2]}${arr[3]}${arr[4]}${arr[5]}${arr[6]}-${arr[7]}${arr[8]}${arr[9]}${arr[10]}`;
+}
 
 
 // Desafio 12
@@ -26,8 +47,20 @@ function triangleCheck() {
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(string) {
+  let stringCaracteres = string.split(' ');
+  let agua = 0;
+
+  for (let index = 0; index < stringCaracteres.length; index += 1) {
+    if (stringCaracteres[index] == Number) {
+      agua += 1;
+    }
+  }
+  if (agua = 1) {
+    return agua + ' copo de água'ç
+  } else if (agua > 1) {
+    return agua + ' copos de água'
+  }
 }
 
 module.exports = {
