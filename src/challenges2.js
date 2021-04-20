ceaa12f7b9ca216732683147ba969de8a8b06ee9
Beatriz Estebanez <bia.estebanez@gmail.com>
2021-04-20 18:51:50 -0300

    Desafio 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -51,8 +51,13 @@ function generatePhoneNumber(phoneNumber) {
 }
 
 // Desafio 12
-function triangleCheck() {
+function triangleCheck(lineA, lineB, lineC) {
   // seu código aqui
+  if ((lineA > lineB + lineC) && (lineA < Math.abs(lineB - lineC))) return false;
+  if ((lineB > lineA + lineC) && (lineB < Math.abs(lineA - lineC))) return false;
+  if ((lineC > lineB + lineA) && (lineC < Math.abs(lineB - lineA))) return false;
+
+  return true;
 }
 
 // Desafio 13
