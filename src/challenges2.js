b14e0c3c02086835fe34cc22555dc9b407dea10d
Beatriz Estebanez <bia.estebanez@gmail.com>
2021-04-20 18:40:14 -0300

    Desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -15,53 +15,8 @@ function techList(techNames, name) {
   }
 }
 
-// Desafio 11
-function generatePhoneNumber(phoneNumber) {
-  let erro = false;
-  if (phoneNumber.length !== 11) {
-    return 'Array com tamanho incorreto.';
-    erro = true;
-  } else {
-    for (let index in phoneNumber) {
-      if (phoneNumber[index] < 0 || phoneNumber[index] > 9) {
-        return 'não é possível gerar um número de telefone com esses valores';
-        erro = true;
-      }
-    }
-  }
-  // console.log(erro);
-  /*VerifNumRep (phoneNumber);
-   console.log(VerifNumRep(phoneNumber));
-   if (VerifNumRep (phoneNumber) !== '') {
-     erro = true;
-   }*/
-  // console.log(VerifNumRep (phoneNumber));
-  if (erro === false) {
-    return correctNumber(phoneNumber);
-  }
-  // console.log(numeroCerto);   
-}
-
-/*function VerifNumRep (numTelefone) {
-  while (erro = false) {
-  for (let index = 0; index <= numTelefone.length; index += 1) {
-    let repeat = 1;
-    for (indexNumber = index + 1; indexNumber < numTelefone.length; indexNumber +=1) {
-      if (numTelefone[index] === numTelefone[indexNumber]) {
-        repeat += 1;
-        console.log(repeat);
-      }
-    }
-    if (repeat >= 3) {
-      return 'não é possível gerar um número de telefone com esses valores';
-      erro = true;
-    }
-  }
-}
-}*/
-//let numero =  [1, 2, 3, 4, 9, 9, 7, 8, 9, 0, 1];
 function correctNumber(numero) {
-  let finalNumber = '(' + numero[0] + numero[1] + ') ';
+  let finalNumber = `(${numero[0]}${numero[1]}) `;
   for (let index = 2; index < 7; index += 1) {
     finalNumber += numero[index];
   }
@@ -72,7 +27,28 @@ function correctNumber(numero) {
   return finalNumber;
 }
 
-console.log(generatePhoneNumber([1, 9, 0, 4, 2, 6, 7, 8, 9, 0, 1]))
+const checkSize = (phoneNumber) => {
+  for (let index in phoneNumber) {
+    if (phoneNumber[index] < 0 || phoneNumber[index] > 9) return 'error';
+  }
+};
+
+const checkRepited = (phoneNumber) => {
+  for (let index in phoneNumber) {
+    const repited = phoneNumber.filter((num) => num === phoneNumber[index]);
+    if (repited.length > 2) return 'error';
+  };
+};
+
+// Desafio 11
+function generatePhoneNumber(phoneNumber) {
+  if (phoneNumber.length !== 11) return 'Array com tamanho incorreto.';
+
+  if (checkSize(phoneNumber) === 'error' || checkRepited(phoneNumber) === 'error') {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+  return correctNumber(phoneNumber);
+}
 
 // Desafio 12
 function triangleCheck() {
