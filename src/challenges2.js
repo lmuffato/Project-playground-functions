f28a42a9aad265efa62d842ccbc4d4003f1c3e35
ScriptCamilo <rodrigo.camilo228@gmail.com>
2021-04-28 08:02:02 -0300

    console.log removed

--- src/challenges2.js
+++ src/challenges2.js
@@ -114,7 +114,6 @@ function hydrate(drinks) {
   }
   return `${sum} copos de água`;
 }
-console.log(hydrate('1 cachaça, 5 cervejas e 1 copo de vinho'));
 
 module.exports = {
   generatePhoneNumber,
